da18b634f3f7d3a4644bcff5cc48ef94fa0901fb
Ilan Mitnikov <ilan@quantum-machines.co>
2020-09-16 19:27:10 +0300

    Added support for chaining inputs and outputs of QuaProgramNode s
    Added some helper functions as well

--- quaPrograms/quaPrograms.py
+++ quaPrograms/quaPrograms.py
@@ -21,6 +21,9 @@ class QuaProgramNode:
         self.output_params = output_params
         self.result = None
 
+    def update_input_values(self, updated_inputs_values):
+        self.input_params.update(updated_inputs_values)
+
     def load_input_values(self, input_params=None):
         """
         Loads the specified variable values to the qua program
@@ -29,8 +32,10 @@ class QuaProgramNode:
         :return: a executable qua program
         """
         if input_params is None:
-            input_params = self.input_params
-        return self.qua_prog(**input_params)
+            return self.qua_prog(**self.input_params)
+        else:
+            self.input_params = input_params
+        return self.qua_prog(**self.input_params)
 
     def get_output_values(self, output_params=None):
         """
@@ -43,7 +48,10 @@ class QuaProgramNode:
         if output_params is None:
             output_params = self.output_params
         for param in output_params:
-            output_values[param] = getattr(self.result, param).fetch_all()['value']
+            try:
+                output_values[param] = getattr(self.result, param).fetch_all()['value']
+            except AttributeError:
+                print("The result of '{}' does not contain the variable '{}'".format(self.name, param))
         return output_values
 
 
@@ -59,23 +67,40 @@ class QuaGraphExecutor:
         """
 
         self.executor = executor
-        self.qua_graph = qua_graph
+        self.graph = qua_graph
 
     def execute(self):
         """
         Execute the qua programs in the graph in a topological order, and save the results in the nodes
         """
-        program_queue = nx.topological_sort(self.qua_graph)
+        program_queue = nx.topological_sort(self.graph)
 
         for prog_name in program_queue:
-            curr_prog = self.qua_graph.nodes[prog_name]['prog']
-            
-            job = self.executor(curr_prog.load_input_values())
-            curr_prog.result = job.result_handles
+            curr_prog_node = self.get_prog_node(prog_name)
+
+            for pred_name in self.graph.predecessors(prog_name):
+                '''
+                Update the current program inputs using the predecessors outputs
+                '''
+                curr_prog_node = self.get_prog_node(pred_name)
+                updated_input_params = curr_prog_node.get_output_values(curr_prog_node.input_params)
+
+                curr_prog_node.update_input_values(updated_input_params)
+
+            job = self.executor(curr_prog_node.load_input_values())
+            curr_prog_node.result = job.result_handles
 
     def plot(self):
         """
         Visualize the graph structure
         """
         plt.tight_layout()
-        nx.draw_networkx(self.qua_graph, arrows=True)
+        nx.draw_networkx(self.graph, arrows=True)
+
+    def get_prog_node(self, name):
+        """
+        Returns a qua program given the name
+        :param name: the name of the qua program node
+        :return: QuaProgramNode
+        """
+        return self.graph.nodes[name]['prog']
