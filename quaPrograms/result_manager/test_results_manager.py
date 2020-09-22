cb36e2ed54249ee7e3df5d924c7c34aab6f8e635
galW <gal@quantum-machines.co>
2020-09-22 19:47:58 +0300

    Auto stash before rebase of "origin/master"

--- quaPrograms/result_manager/test_results_manager.py
+++ quaPrograms/result_manager/test_results_manager.py
@@ -4,6 +4,7 @@ from base_configuration.vanilla_config import *
 from base_configuration.hello_qua import *
 from qm.QuantumMachinesManager import QuantumMachinesManager
 from qm import SimulationConfig
+import shutil
 
 
 def test_results_manager():
@@ -12,3 +13,5 @@ def test_results_manager():
     job = qm1.simulate(hello_qua(), SimulationConfig(1000))
     res = job.result_handles
     res.save_to_store()
+    assert(1)
+    shutil.rmtree('res')
