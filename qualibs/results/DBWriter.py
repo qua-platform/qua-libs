267513405ede3625b9d95f99404cccde05cb180e
galW <gal@quantum-machines.co>
2020-10-01 21:17:48 +0300

    added  a sqlite class and read and write methods

--- qualibs/results/DBWriter.py
+++ qualibs/results/DBWriter.py
@@ -1,6 +1,8 @@
+import os
 from abc import ABC, abstractmethod
 import sqlite3 as sl
 from typing import Tuple
+from time import time_ns
 
 
 class GraphStore(ABC):
@@ -9,33 +11,41 @@ class GraphStore(ABC):
         pass
 
     @abstractmethod
-    def write_res(self):
+    def write_res(self, result: Tuple[int, int, int, str, int, int, str, str]):
         raise NotImplementedError()
 
     @abstractmethod
-    def read_res(self):
+    def get_res_by_id(self,id):
         raise NotImplementedError()
 
-    def write_nodes(self):
-        raise NotImplementedError()
-
-    @abstractmethod
-    def read_nodes(self):
+    def write_node(self, node: Tuple[int, int]):
         raise NotImplementedError()
 
     @abstractmethod
-    def write_metadata(self):
+    def read_node_by_id(self,id):
         raise NotImplementedError()
 
     @abstractmethod
-    def read_metadata(self):
+    def write_metadata(self,data:Tuple[int,int,int,str,str]):
         raise NotImplementedError()
+
+    # @abstractmethod
+    # def read_metadata(self):
+    #     raise NotImplementedError()
+
+    @abstractmethod
+    def write_graph(self, graph: Tuple[int, str]):
+        raise NotImplementedError()
+
+    # @abstractmethod
+    # def read_graph(self):
+    #     raise NotImplementedError()
 
 
 class GraphStoreSqlite(GraphStore):
 
     def __init__(self, db_path):
-        super.__init__()
+        # super.__init__(db_path)
         self.con = sl.connect(db_path)
 
     def init_db(self):
@@ -94,17 +104,71 @@ class GraphStoreSqlite(GraphStore):
                            VALUES (?,?,?,?,?,?,?,?); 
                            """, result)
 
-    def read_res(self):
-        pass
+    def get_res_by_id(self,id):
+        with self.con as con:
+            cur=con.cursor()
+            cur.execute(""" 
+                           SELECT * FROM Results where result_id=(?)
+                           """,(id,))
+            rows = cur.fetchall()
+            return rows[0]
 
-    def write_nodes(self):
-        pass
 
-    def read_nodes(self):
-        pass
 
-    def write_metadata(self):
-        pass
+    def write_node(self, node: Tuple[int, int]):
+        with self.con as con:
+            con.execute(""" 
+                            INSERT INTO Nodes (graph_id,node_id) 
+                            VALUES (?,?); 
+                        """, node)
+
+
+    def read_node_by_id(self,id):
+        with self.con as con:
+            cur = con.cursor()
+            cur.execute(""" 
+                                  SELECT * FROM Nodes where node_id=(?)
+                                  """, (id,))
+            rows = cur.fetchall()
+            return rows[0]
+
+    def write_metadata(self,data:Tuple[int,int,int,str,str]):
+        with self.con as con:
+            con.execute(f""" 
+                               INSERT INTO Metadata (graph_id,node_id,data_id,name,val) 
+                               VALUES (?,?,?,?,?) 
+                               """, data)
 
     def read_metadata(self):
         pass
+
+    def write_graph(self, graph: Tuple[int, str]):
+        with self.con as con:
+            con.execute("""  
+                            INSERT INTO Graphs (graph_id,graph_script) 
+                            VALUES (?,?); 
+                            """, graph)
+
+
+if __name__ == '__main__':
+    g_id = time_ns()
+    n_id = time_ns()
+    r_id = time_ns()
+    try:
+        os.remove('my_db.db')
+    except:
+        print('no file')
+    a = GraphStoreSqlite('my_db.db')
+    a.init_db()
+    for g_id in range(10):
+        a.write_graph((g_id, 'a'))
+        for n_id in range(10):
+            a.write_node((g_id, n_id))
+            for r_id in range(2):
+                a.write_res((g_id, n_id, r_id, 'a', 1, 1, 'a', 'a'))
+                # for d_id in range(3):
+                #     a.write_metadata((g_id,n_id,d_id,'a','a'))
+    print(a.get_res_by_id(1))
+    print(a.read_node_by_id(1))
+
+
