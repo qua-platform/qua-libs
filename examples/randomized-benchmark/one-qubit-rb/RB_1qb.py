b5c43ec9b434612d57bbde3e88f2fc8944e6b055
galW <gal@quantum-machines.co>
2020-11-25 22:02:44 +0200

    RB refactor

--- examples/randomized-benchmark/one-qubit-rb/RB_1qb.py
+++ examples/randomized-benchmark/one-qubit-rb/RB_1qb.py
@@ -5,54 +5,10 @@ from qm.qua import *
 from qm import SimulationConfig
 import numpy as np
 from configuration import config
+from rb_lib import *
 
 # Open communication with the server.
 QMm = QuantumMachinesManager()
-
-cliffords = [['I'], ['X'], ['Y'], ['Y', 'X'],
-             ['X/2', 'Y/2'], ['X/2', '-Y/2'], ['-X/2', 'Y/2'], ['-X/2', '-Y/2'], ['Y/2', 'X/2'], ['Y/2', '-X/2'],
-             ['-Y/2', 'X/2'], ['-Y/2', '-X/2'],
-             ['X/2'], ['-X/2'], ['Y/2'], ['-Y/2'], ['-X/2', 'Y/2', 'X/2'], ['-X/2', '-Y/2', 'X/2'],
-             ['X', 'Y/2'], ['X', '-Y/2'], ['Y', 'X/2'], ['Y', '-X/2'], ['X/2', 'Y/2', 'X/2'], ['-X/2', 'Y/2', '-X/2']]
-
-
-def recovery_clifford(state):
-    # operations = {'x': ['I'], '-x': ['Y'], 'y': ['X/2', '-Y/2'], '-y': ['-X/2', '-Y/2'], 'z': ['-Y/2'], '-z': ['Y/2']}
-    operations = {'z': ['I'], '-x': ['Y/2'], 'y': ['X/2'], '-y': ['-X/2'], 'x': ['-Y/2'], '-z': ['X']}
-    return operations[state]
-
-
-def transform_state(input_state: str, transformation: str):
-    transformations = {'x': {'I': 'x', 'X/2': 'x', 'X': 'x', '-X/2': 'x', 'Y/2': 'z', 'Y': '-x', '-Y/2': '-z'},
-                       '-x': {'I': '-x', 'X/2': '-x', 'X': '-x', '-X/2': '-x', 'Y/2': '-z', 'Y': 'x', '-Y/2': 'z'},
-                       'y': {'I': 'y', 'X/2': 'z', 'X': '-y', '-X/2': '-z', 'Y/2': 'y', 'Y': 'y', '-Y/2': 'y'},
-                       '-y': {'I': '-y', 'X/2': '-z', 'X': 'y', '-X/2': 'z', 'Y/2': '-y', 'Y': '-y', '-Y/2': '-y'},
-                       'z': {'I': 'z', 'X/2': '-y', 'X': '-z', '-X/2': 'y', 'Y/2': '-x', 'Y': '-z', '-Y/2': 'x'},
-                       '-z': {'I': '-z', 'X/2': 'y', 'X': 'z', '-X/2': '-y', 'Y/2': 'x', 'Y': 'z', '-Y/2': '-x'}}
-
-    return transformations[input_state][transformation]
-
-
-def play_clifford(clifford, state: str):
-    for op in clifford:
-        state = transform_state(state, op)
-        if op != 'I':
-            play(op, 'qe1')
-    return state
-
-
-def randomize_and_play_circuit(n_gates: int, init_state: str = 'x'):
-    state = init_state
-    for ind in range(n_gates):
-        state = play_clifford(cliffords[np.random.randint(0, len(cliffords))], state)
-    return state
-
-#   This measurement function is a typical SC qubit measurement (via a dispersive readout)
-def measure_state(state):
-  th = 0
-  measure('readout', 'rr', None, integration.full('integW1', I))
-            assign(state,I>th)
-
 QM1 = QMm.open_qm(config)
 
 N_avg = 10
@@ -72,7 +28,7 @@ with program() as RBprog:
             final_state = randomize_and_play_circuit(depth)
             play_clifford(recovery_clifford(final_state), final_state)
             align('rr', 'qe1')
-            measure_state(state)
+            measure_state(state,I)
             save(state, out_str)
             wait(10 * t1, 'qe1')
 
@@ -81,8 +37,8 @@ with program() as RBprog:
 
 job = QM1.simulate(RBprog,
                    SimulationConfig(int(100000)))
-res=job.result_handles
-avg_state=res.out_stream.fetch_all()
+res = job.result_handles
+avg_state = res.out_stream.fetch_all()
 
 samples = job.get_simulated_samples()
 
