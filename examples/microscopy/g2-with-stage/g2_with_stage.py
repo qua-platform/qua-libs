0c7b3c951b56611fe9d82d0916dedc74556053f2
Yoav Romach <yomach@gmail.com>
2021-01-11 19:05:22 +0200

    fixes for NV

--- examples/microscopy/g2-with-stage/g2_with_stage.py
+++ examples/microscopy/g2-with-stage/g2_with_stage.py
@@ -30,13 +30,17 @@ meas_len = 2000  # 2us
 N_avg = 1e6  # total measurement time will be: meas_len*N_avg
 result_array = []
 
+
+###############
+# confocal g2 #
+###############
 with program() as confocal_g2:
+
     i = declare(int)
     n = declare(int)
     n_avg = declare(int)
     k = declare(int)
     p = declare(int)
-    g2 = declare(int, size=int(2 * correlation_width))
     diff = declare(int)
     diff_index = declare(int)
     resultLen1 = declare(int)
@@ -45,12 +49,25 @@ with program() as confocal_g2:
     # This can be increased if needed.
     result1 = declare(int, size=int(meas_len / 500))
     result2 = declare(int, size=int(meas_len / 500))
+    g2 = declare(int, size=int(2 * correlation_width))
     g2_stream = declare_stream()
+
+    #########################
+    # looping over position #
+    #########################
     with for_(i, 0, i < Nx * Ny, i+1):
+
         pause()
+
+        ########################
+        # initialize g2 vector #
+        ########################
         with for_(p, 0, p < g2.length(), p + 1):
             assign(g2[p], 0)
 
+        ##########################
+        # perform g2 measurement #
+        ##########################
         with for_(n_avg, 0, n_avg <= N_avg, n_avg + 1):
             play('readout', 'AOM')
             measure("readout", "spcm1", None,
@@ -58,29 +75,48 @@ with program() as confocal_g2:
             measure("readout", "spcm2", None,
                     time_tagging.raw(result2, meas_len, resultLen2))
             assign(n, 0)
-            with for_(k, 0, k < resultLen2, k + 1):
-                with if_(n < resultLen1):
+            assign(k, 0)
+            # with for_(k, 0, k < resultLen2, k + 1):
+            #     with if_(n < resultLen1):
+            with while_(k < resultLen2):
+                with while_(n < resultLen1):
                     assign(diff, result1[n] - result2[k])
 
                     with if_((diff < correlation_width) & (diff > -correlation_width)):
                         assign(diff_index, diff + correlation_width)
                         assign(g2[diff_index], g2[diff_index] + 1)
+                        assign(n, n + 1)
+                        assign(k, k + 1)
 
-                    with if_(diff > 0):
+                    with if_(diff >= correlation_width):
+                        pass
+
+                    with if_(diff <= correlation_width):
                         assign(n, n + 1)
 
+                assign(k, k + 1)
+
+
+        ####################
+        # stream g2 vector #
+        ####################
         with for_(p, 0, p < g2.length(), p + 1):
             save(g2[p], g2_stream)
 
     with stream_processing():
         g2_stream.buffer(2 * correlation_width).save_all('g2')  # Take g2 vector per position.
 
-job = QM1.execute(confocal_g2)
 
+job = QM1.execute(confocal_g2)
 res = job.result_handles
 x_vec = np.arange(x_start, x_end, x_step)
 y_vec = np.arange(y_start, y_end, y_step)
 g2_data = np.zeros([len(x_vec), len(y_vec)])
+
+
+##############
+# move stage #
+##############
 for x_i in range(len(x_vec)):
     for y_i in range(len(x_vec)):
         stage.go_to(pos=(x_vec[x_i], y_vec[y_i]))
