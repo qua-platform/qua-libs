bb1b5e3eb536f380647f085e8eaa9a4ae71c951b
Satya Bade <satya@quantum-machines.co>
2021-01-18 14:27:06 +0100

    Using waveform under optimization for the recovery operation

--- examples/leakage-reduction/configuration.py
+++ examples/leakage-reduction/configuration.py
@@ -2,11 +2,13 @@ import numpy as np
 
 gauss_len = 4
 
+
 def IQ_imbalance(g, phi):
     c = np.cos(phi)
     s = np.sin(phi)
-    N = 1 / ((1-g**2)*(2*c**2-1))
-    return [float(N * x) for x in [(1-g)*c, (1+g)*s, (1-g)*s, (1+g)*c]]
+    N = 1 / ((1 - g ** 2) * (2 * c ** 2 - 1))
+    return [float(N * x) for x in [(1 - g) * c, (1 + g) * s, (1 - g) * s, (1 + g) * c]]
+
 
 readout_len = 400
 qubit_IF = 100e6
@@ -15,156 +17,123 @@ qubit_LO = 5117.22e6
 rr_LO = 5117.22e6
 
 config = {
-
-    'version': 1,
-
-    'controllers': {
-
+    "version": 1,
+    "controllers": {
         "con1": {
-            'type': 'opx1',
-            'analog_outputs': {
-                1: {'offset': +0.0},  # qubit 1-I
-                2: {'offset': +0.0},  # qubit 1-Q
-                3: {'offset': +0.0},  # Readout resonator
-                4: {'offset': +0.0},  # Readout resonator
+            "type": "opx1",
+            "analog_outputs": {
+                1: {"offset": +0.0},  # qubit 1-I
+                2: {"offset": +0.0},  # qubit 1-Q
+                3: {"offset": +0.0},  # Readout resonator
+                4: {"offset": +0.0},  # Readout resonator
             },
-            'digital_outputs': {
+            "digital_outputs": {
                 1: {},
             },
-            'analog_inputs': {
-                1: {'offset': +0.0},
-                2: {'offset': +0.0},
-            }
+            "analog_inputs": {
+                1: {"offset": +0.0},
+                2: {"offset": +0.0},
+            },
         }
     },
-
-    'elements': {
-
+    "elements": {
         "qubit": {
             "mixInputs": {
                 "I": ("con1", 1),
                 "Q": ("con1", 2),
-                'lo_frequency': qubit_LO,
-                'mixer': 'mixer_qubit'
+                "lo_frequency": qubit_LO,
+                "mixer": "mixer_qubit",
             },
-            'intermediate_frequency': qubit_IF,
-            'operations': {
-                'X/2': "DRAG_PULSE",
-                '-X/2': "DRAG_PULSE",
-                'Y/2': "DRAG_PULSE",
-                '-Y/2': "DRAG_PULSE",
-                'X': "DRAG_PULSE",
-                'Y': "DRAG_PULSE",
-                'random_clifford_seq':"random_sequence"
+            "intermediate_frequency": qubit_IF,
+            "operations": {
+                "X/2": "DRAG_PULSE",
+                "-X/2": "DRAG_PULSE",
+                "Y/2": "DRAG_PULSE",
+                "-Y/2": "DRAG_PULSE",
+                "X": "DRAG_PULSE",
+                "Y": "DRAG_PULSE",
+                "random_clifford_seq": "random_sequence",
             },
         },
-        'rr': {
-            'mixInputs': {
-                'I': ('con1', 3),
-                'Q': ('con1', 4),
-                'lo_frequency': rr_LO,
-                'mixer': 'mixer_RR'
+        "rr": {
+            "mixInputs": {
+                "I": ("con1", 3),
+                "Q": ("con1", 4),
+                "lo_frequency": rr_LO,
+                "mixer": "mixer_RR",
             },
-            'intermediate_frequency': rr_IF,
-            'operations': {
-                'readout': 'readout_pulse',
+            "intermediate_frequency": rr_IF,
+            "operations": {
+                "readout": "readout_pulse",
             },
-            "outputs": {
-                'out1': ('con1', 1)
-            },
-            'time_of_flight': 28,
-            'smearing': 0
+            "outputs": {"out1": ("con1", 1)},
+            "time_of_flight": 28,
+            "smearing": 0,
         },
     },
-
     "pulses": {
         "DRAG_PULSE": {
-            'operation': 'control',
-            'length': None,
-            'waveforms': {
-                'I': 'DRAG_gauss_wf',
-                'Q': 'DRAG_gauss_der_wf'
-            },
+            "operation": "control",
+            "length": None,
+            "waveforms": {"I": "DRAG_gauss_wf", "Q": "DRAG_gauss_der_wf"},
         },
         "random_sequence": {
-            'operation': 'control',
-            'length': None,
-            'waveforms': {
-                'I': 'random_I',
-                'Q': 'random_Q'
-            },
+            "operation": "control",
+            "length": None,
+            "waveforms": {"I": "random_I", "Q": "random_Q"},
         },
-        'readout_pulse': {
-            'operation': 'measurement',
-            'length': readout_len/4,
-            'waveforms': {
-                'I': 'gauss_wf',
-                'Q': 'zero_wf'
+        "readout_pulse": {
+            "operation": "measurement",
+            "length": readout_len,
+            "waveforms": {"I": "readout_wf", "Q": "zero_wf"},
+            "integration_weights": {
+                "integW1": "integW1",
+                "integW2": "integW2",
             },
-            'integration_weights': {
-                'integW1': 'integW1',
-                'integW2': 'integW2',
-            },
-            'digital_marker': 'ON'
+            "digital_marker": "ON",
         },
     },
-
     "waveforms": {
-        'zero_wf': {
-            'type': 'constant',
-            'sample': 0.0
+        "zero_wf": {"type": "constant", "sample": 0.0},
+        "DRAG_gauss_wf": {"type": "arbitrary", "samples": []},
+        "DRAG_gauss_der_wf": {"type": "arbitrary", "samples": []},
+        "readout_wf": {"type": "arbitrary", "samples": []},
+        "random_I": {
+            "type": "arbitrary",
+            "samples": [],
         },
-        'gauss_wf': {
-            'type': 'arbitrary',
-            'samples': []
-        },
-        'DRAG_gauss_wf': {
-            'type': 'arbitrary',
-            'samples': []
-        },
-        'DRAG_gauss_der_wf': {
-            'type': 'arbitrary',
-            'samples': []
-        },
-        'readout_wf':{
-            'type': 'constant',
-            'sample': 0.3
-        },
-        'random_I':{
-            'type': 'arbitrary',
-            'samples': [],
-        },
-        'random_Q':{
-            'type': 'arbitrary',
-            'samples': [],
+        "random_Q": {
+            "type": "arbitrary",
+            "samples": [],
         },
     },
-    'digital_waveforms': {
-        'ON': {
-            'samples': [(1, 0)]
+    "digital_waveforms": {
+        "ON": {"samples": [(1, 0)]},
+    },
+    "integration_weights": {
+        "integW1": {
+            "cosine": [1.0] * readout_len,
+            "sine": [0.0] * readout_len,
+        },
+        "integW2": {
+            "cosine": [0.0] * readout_len,
+            "sine": [1.0] * readout_len,
         },
     },
-    'integration_weights': {
-
-        'integW1': {
-            'cosine': [1.0] * int(readout_len / 4),
-            'sine': [0.0] * int(readout_len / 4),
-        },
-
-        'integW2': {
-            'cosine': [0.0] * int(readout_len / 4),
-            'sine': [1.0] * int(readout_len / 4),
-        },
-
-    },
-    'mixers': {
-        'mixer_qubit': [
-            {'intermediate_frequency': qubit_IF, 'lo_frequency': qubit_LO,
-             'correction': IQ_imbalance(0.0, 0.0)}
+    "mixers": {
+        "mixer_qubit": [
+            {
+                "intermediate_frequency": qubit_IF,
+                "lo_frequency": qubit_LO,
+                "correction": IQ_imbalance(0.0, 0.0),
+            }
         ],
-        'mixer_RR': [
-            {'intermediate_frequency': rr_IF, 'lo_frequency': rr_LO,
-             'correction': IQ_imbalance(0.0, 0.0)}
+        "mixer_RR": [
+            {
+                "intermediate_frequency": rr_IF,
+                "lo_frequency": rr_LO,
+                "correction": IQ_imbalance(0.0, 0.0),
+            }
         ],
-    }
+    },
 }
--- examples/leakage-reduction/lr_lib.py
+++ examples/leakage-reduction/lr_lib.py
@@ -8,7 +8,7 @@ from scipy.interpolate import interp1d
 
 
 def get_program(config, params, t, N_avg, d):
-    '''
+    """
     A function to generate the QUA program
     :param config:
     :param params:
@@ -16,167 +16,236 @@ def get_program(config, params, t, N_avg, d):
     :param N_avg:
     :param d:
     :return:
-    '''
+    """
 
     state, op_list = update_randomized_waveform(params, d, config, t)
     with program() as drag_RB_prog:
-         N = declare(int)
-         I = declare(fixed)
-         out_str = declare_stream()
-         F = declare(fixed)
-         F_str=declare_stream()
-         update_frequency("qubit", params[2])
-         with for_(N, 0, N<N_avg, N+1):
-            play('random_clifford_seq', 'qubit')
+        N = declare(int)
+        I = declare(fixed)
+        out_str = declare_stream()
+        F = declare(fixed)
+        F_str = declare_stream()
+        update_frequency("qubit", params[2])
+        with for_(N, 0, N < N_avg, N + 1):
+            play("random_clifford_seq", "qubit")
             ## compute the recovery operation
             recovery_op = recovery_clifford(state)[0]
-            if recovery_op == 'I':
-                wait(gauss_len, 'qubit')
+            if recovery_op == "I":
+                wait(gauss_len, "qubit")
             else:
-                play(recovery_op, 'qubit')
+                play(recovery_op, "qubit")
             assign(F, get_simulated_fidelity(op_list, err=e))
             save(F, F_str)
-            align('rr', 'qubit')
-            measure('readout', 'rr', None, integration.full('integW1', I))
+            align("rr", "qubit")
+            measure("readout", "rr", None, integration.full("integW1", I))
             save(I, out_str)
-            wait(500, 'qubit')
-         with stream_processing():
-             out_str.save_all('out_stream')
-             F_str.save_all('F_stream')
+            wait(500, "qubit")
+        with stream_processing():
+            out_str.save_all("out_stream")
+            F_str.save_all("F_stream")
     return drag_RB_prog
 
+
 def get_result(prog, duration):
-    '''
+    """
     Upload the waveforms to the configuration and re-open the QM
 
     :param prog:
     :param duration:
     :return:
-    '''
+    """
     # host = '3.122.60.129'
-    QMm = QuantumMachinesManager()
+    QMm = QuantumMachinesManager(host="3.122.60.129")
     QM = QMm.open_qm(config)
-    job = QM.simulate(prog,
-                      SimulationConfig(duration))
+    job = QM.simulate(prog, SimulationConfig(duration))
     res = job.result_handles
-    F = res.F_stream.fetch_all()['value']
+    F = res.F_stream.fetch_all()["value"]
     F_avg = F.mean(axis=0)
-    err=1-F_avg
+    err = 1 - F_avg
     return err
 
+
 def cost_DRAG(params):
-    '''
+    """
     Get the cost of an unmodified DRAG pulse
     :param params:
     :return:
-    '''
+    """
 
-    _params = list(params) + [0]*n_params
+    _params = list(params) + [0] * n_params
     prog = get_program(config, _params, pulse_duration, N_avg, depth)
     return get_result(prog, 1000)
 
+
 def cost_optimal_pulse(params):
-    '''
+    """
     Get the cost of the modified DRAG pulse
     :param params:
     :return:
-    '''
+    """
     prog = get_program(config, params, pulse_duration, N_avg, depth)
     return get_result(prog, 1000)
 
+
 def DRAG_I(A, mu, sigma):
-    '''
+    """
     Generate a Gaussian waveform
     :param A:
     :param mu:
     :param sigma:
     :return:
-    '''
+    """
+
     def f(t):
-        return A*np.exp(-(t-mu) ** 2/( 2.0 * sigma) ** 2)
+        return A * np.exp(-((t - mu) ** 2) / (2.0 * sigma) ** 2)
+
     return f
 
+
 def DRAG_Q(B, mu, sigma):
-    '''
+    """
     Generate a Gaussian derivative waveform
     :param B:
     :param mu:
     :param sigma:
     :return:
-    '''
+    """
+
     def f(t):
-        return B * (-2.0*(t-mu))*np.exp(-((t - mu) ** 2) / (2.0 * sigma ** 2))
+        return B * (-2.0 * (t - mu)) * np.exp(-((t - mu) ** 2) / (2.0 * sigma ** 2))
+
     return f
 
+
 def get_DRAG_pulse(gate: str, params: list, t: float):
-    '''
+    """
     Generate a modified DRAG pulse based on the params structure
     :param gate:
     :param params:
     :param t:
     :return:
-    '''
+    """
 
     ## params [A, B, freq, a0, a1, a2, .... an-1, b0, b1 .....bn-1]
-    _n_params = (len(params) - 3)//2
+    _n_params = (len(params) - 3) // 2
     _ts = np.linspace(0.0, t, _n_params)
     if np.sum(params[3:]) == 0.0:
-        an_func = lambda x:0
-        bn_func = lambda x:0
+        an_func = lambda x: 0
+        bn_func = lambda x: 0
     else:
-        an_func = interp1d(params[3:_n_params+3], _ts, fill_value="extrapolate")
-        bn_func = interp1d(params[_n_params+3:], _ts, fill_value="extrapolate")
+        an_func = interp1d(params[3 : _n_params + 3], _ts, fill_value="extrapolate")
+        bn_func = interp1d(params[_n_params + 3 :], _ts, fill_value="extrapolate")
     ns = int(t)
     ts = np.linspace(0.0, t, ns)
     ts[-1] -= 0.01
     ts[0] += 0.01
-    I_t = DRAG_I(params[0], t/2, t)
-    Q_t = DRAG_Q(params[1], t/2, t)
+    I_t = DRAG_I(params[0], t / 2, t)
+    Q_t = DRAG_Q(params[1], t / 2, t)
     I = [(I_t(_t) + an_func(_t)) for _t in ts]
     Q = [(Q_t(_t) + bn_func(_t)) for _t in ts]
     return (I, Q)
 
+
 def recovery_clifford(state):
-    '''
+    """
     Returns the recovery clifford operation matching the final state (after the RB circuit)
     :param state:
     :return:
-    '''
+    """
 
-    operations = {'z': ['I'], '-x': ['-Y/2'], 'y': ['X/2'], '-y': ['-X/2'], 'x': ['Y/2'], '-z': ['X']}
+    operations = {
+        "z": ["I"],
+        "-x": ["-Y/2"],
+        "y": ["X/2"],
+        "-y": ["-X/2"],
+        "x": ["Y/2"],
+        "-z": ["X"],
+    }
     return operations[state]
 
+
 def transform_state(input_state: str, transformation: str):
-    '''
+    """
     A function to update the state after each clifford operation
     :param input_state:
     :param transformation:
     :return:
-    '''
-    transformations = {'x': {'I': 'x', 'X/2': 'x', 'X': 'x', '-X/2': 'x', 'Y/2': 'z', 'Y': '-x', '-Y/2': '-z'},
-                       '-x': {'I': '-x', 'X/2': '-x', 'X': '-x', '-X/2': '-x', 'Y/2': '-z', 'Y': 'x', '-Y/2': 'z'},
-                       'y': {'I': 'y', 'X/2': 'z', 'X': '-y', '-X/2': '-z', 'Y/2': 'y', 'Y': 'y', '-Y/2': 'y'},
-                       '-y': {'I': '-y', 'X/2': '-z', 'X': 'y', '-X/2': 'z', 'Y/2': '-y', 'Y': '-y', '-Y/2': '-y'},
-                       'z': {'I': 'z', 'X/2': '-y', 'X': '-z', '-X/2': 'y', 'Y/2': '-x', 'Y': '-z', '-Y/2': 'x'},
-                       '-z': {'I': '-z', 'X/2': 'y', 'X': 'z', '-X/2': '-y', 'Y/2': 'x', 'Y': 'z', '-Y/2': '-x'}}
+    """
+    transformations = {
+        "x": {
+            "I": "x",
+            "X/2": "x",
+            "X": "x",
+            "-X/2": "x",
+            "Y/2": "z",
+            "Y": "-x",
+            "-Y/2": "-z",
+        },
+        "-x": {
+            "I": "-x",
+            "X/2": "-x",
+            "X": "-x",
+            "-X/2": "-x",
+            "Y/2": "-z",
+            "Y": "x",
+            "-Y/2": "z",
+        },
+        "y": {
+            "I": "y",
+            "X/2": "z",
+            "X": "-y",
+            "-X/2": "-z",
+            "Y/2": "y",
+            "Y": "y",
+            "-Y/2": "y",
+        },
+        "-y": {
+            "I": "-y",
+            "X/2": "-z",
+            "X": "y",
+            "-X/2": "z",
+            "Y/2": "-y",
+            "Y": "-y",
+            "-Y/2": "-y",
+        },
+        "z": {
+            "I": "z",
+            "X/2": "-y",
+            "X": "-z",
+            "-X/2": "y",
+            "Y/2": "-x",
+            "Y": "-z",
+            "-Y/2": "x",
+        },
+        "-z": {
+            "I": "-z",
+            "X/2": "y",
+            "X": "z",
+            "-X/2": "-y",
+            "Y/2": "x",
+            "Y": "z",
+            "-Y/2": "-x",
+        },
+    }
     return transformations[input_state][transformation]
 
+
 def update_randomized_waveform(params: list, d: int, config: dict, t: float):
-    '''
+    """
     Randomize the circuit and update configuration with the waveforms
     :param params:
     :param d:
     :param config:
     :param t:
     :return:
-    '''
-    state = '-z' #initial state
-    cliffords = ["X/2", "-X/2", "Y/2", "-Y/2"] #generating clifford operations
+    """
+    state = "-z"  # initial state
+    cliffords = ["X/2", "-X/2", "Y/2", "-Y/2"]  # generating clifford operations
     op_list = []
     I, Q = [], []
     config["pulses"]["random_sequence"]["length"] = 0
     for _ in range(d):
-        c=cliffords[np.random.randint(4)]
+        c = cliffords[np.random.randint(4)]
         op_list.append(c)
         state = transform_state(state, c)
         I_Q = get_DRAG_pulse(c, params, t)
@@ -185,43 +254,62 @@ def update_randomized_waveform(params: list, d: int, config: dict, t: float):
         config["pulses"]["random_sequence"]["length"] += len(I_Q[0])
     config["waveforms"]["random_I"]["samples"] = I
     config["waveforms"]["random_Q"]["samples"] = Q
+    I = np.zeros(16)
+    Q = np.zeros(16)  # 16ns is the minimum duration of a play statement
+    Ir, Qr = get_DRAG_pulse("", params, t)
+    I[: len(Ir)] = Ir
+    Q[: len(Qr)] = Qr
+    config["pulses"]["DRAG_PULSE"]["length"] = 16
+    config["waveforms"]["DRAG_gauss_wf"]["samples"] = I
+    config["waveforms"]["DRAG_gauss_der_wf"]["samples"] = Q
     return state, op_list
 
 
-
-def get_error_dep_fidelity(err,op):
-    '''
+def get_error_dep_fidelity(err, op):
+    """
     Function to emulate increasing performance with decreasing optimization cost
     (For a single clifford operation)
     :param err:
     :param op:
     :return:
-    '''
-    return clifford_fidelity[op]*np.exp(-err/10)
+    """
+    return clifford_fidelity[op] * np.exp(-err / 10)
 
-def get_simulated_fidelity(ops_list,err=0):
-    '''
+
+def get_simulated_fidelity(ops_list, err=0):
+    """
     Function to emulate increasing performance with decreasing optimization cost
     (For a single a full circuit)
     :param ops_list:
     :param err:
     :return:
-    '''
-    fidelity=1
+    """
+    fidelity = 1
     for op in ops_list:
-        fidelity=fidelity*get_error_dep_fidelity(err,op)
+        fidelity = fidelity * get_error_dep_fidelity(err, op)
     return fidelity
 
-#globals
 
-clifford_fidelity = {'I': 1, 'X/2': 0.99, 'X': 0.99, '-X/2': 0.99, 'Y/2': 0.99, 'Y': 0.99, '-Y/2': 0.99}
+# globals
+
+clifford_fidelity = {
+    "I": 1,
+    "X/2": 0.99,
+    "X": 0.99,
+    "-X/2": 0.99,
+    "Y/2": 0.99,
+    "Y": 0.99,
+    "-Y/2": 0.99,
+}
 pulse_duration = 4.19
 n_params = 20
 N_avg = 10
 depth = 20
 
-x_the=np.array([1, 2.3, 100e6])
-x_0=np.array([1.2, 2.2, 110e6])
+x_the = np.array([1, 2.3, 100e6])
+x_0 = np.array([1.2, 2.2, 110e6])
 e = np.sqrt(np.sum((x_0 - x_the) ** 2))
-gauss_len = 100
-
+ts = np.linspace(0.0, readout_len, readout_len)
+config["waveforms"]["readout_wf"]["samples"] = [
+    DRAG_I(1.0, 0.5 * readout_len, readout_len)(t) for t in ts
+]
