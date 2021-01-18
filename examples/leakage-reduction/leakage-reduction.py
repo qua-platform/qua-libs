94169c6616868843e9a83f6b01c4d89640f9ecdb
Satya Bade <satya@quantum-machines.co>
2021-01-18 14:27:06 +0100

    Using waveform under optimization for the recovery operation

--- examples/leakage-reduction/leakage-reduction.py
+++ examples/leakage-reduction/leakage-reduction.py
@@ -2,13 +2,6 @@ from lr_lib import *
 import numpy as np
 import matplotlib.pyplot as plt
 
-##we start with a standard DRAG pulse
-ts = np.linspace(0.0, 4.0*gauss_len, gauss_len)
-config["pulses"]["DRAG_PULSE"]["length"] = gauss_len
-config["waveforms"]["gauss_wf"]["samples"] = [DRAG_I(1.0, 2.0*gauss_len, 4.0*gauss_len)(t) for t in ts]
-config["waveforms"]["DRAG_gauss_wf"]["samples"] = [DRAG_I(1.0, 2.0*gauss_len, 4.0*gauss_len)(t) for t in ts]
-config["waveforms"]["DRAG_gauss_der_wf"]["samples"] = [DRAG_Q(1.0, 2.0*gauss_len, 4.0*gauss_len)(t) for t in ts]
-
 ##we then optimize a regular DRAG pulse
 np.random.seed(3)
 es1 = cma.CMAEvolutionStrategy(np.random.rand(3), 0.5)
@@ -22,7 +15,7 @@ es2 = cma.CMAEvolutionStrategy(init, 0.5)
 es2.optimize(cost_optimal_pulse)
 es2.result_pretty()
 
-#We can now draw the optimal pulse
-opt_pulse=np.array(get_DRAG_pulse('x',es2.result.xbest,40))
-plt.plot(opt_pulse[0,:])
-plt.plot(opt_pulse[1,:])
+# We can now draw the optimal pulse
+opt_pulse = np.array(get_DRAG_pulse("x", es2.result.xbest, 40))
+plt.plot(opt_pulse[0, :])
+plt.plot(opt_pulse[1, :])
