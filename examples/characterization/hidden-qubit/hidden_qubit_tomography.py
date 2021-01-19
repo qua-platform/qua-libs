39b3cf41545e20b4b725cd2428735fb47b368ef6
galW <gal@quantum-machines.co>
2021-01-19 11:10:22 +0200

    High time resolution rabi sweep
    Added hidden_qubit script + corrections on characterization scripts
    
    Changed order of loops in Rabi sweeps and T1/T2, adjusted the plots+ Added script for hidden qubit 
    Removed OPX IP address in scripts
    
    IP used for testing was still in the script, now it's removed
    Auto stash before rebase of "origin/main"
    
    Hidden qubit script update, include stream processing 
    changed waveform weights and added align
    
    following suggestion: https://quantum-machines.atlassian.net/browse/SW-355?focusedCommentId=14695
    WIP
    
    
    added readme.md for hidden qubit
    
    
    Corrected tomography operations in config
    
    Also : removed redundancy of readout operation in the main script
    Reformated file of Qubit process_tomography

---
+++ examples/characterization/hidden-qubit/hidden_qubit_tomography.py
@@ -0,0 +1,138 @@
+"""hidden_qubit_tomography.py: Performing process tomography with one control and one hidden qubit
+Author: Arthur Strauss - Quantum Machines
+Created: 08/01/2021
+QUA version used : 0.8.439
+"""
+
+# QM imports
+from configuration import *
+from qm.QuantumMachinesManager import QuantumMachinesManager
+from qm import QuantumMachine
+from qm import LoopbackInterface
+from qm import SimulationConfig
+
+
+qm1 = QuantumMachinesManager()
+QM = qm1.open_qm(config)
+
+N_shots = 1
+
+nb_processes = len(list(processes.keys()))
+nb_basis_states = len(list(state_prep.keys()))
+nb_Pauli_op = len(list(tomography_set.keys()))
+
+with program() as hidden_qubit_tomography:
+    N = declare(fixed)
+    I = declare(fixed)
+    Q = declare(fixed)
+    state_c = declare(bool)
+    state_h = declare(bool)
+    stream_c = declare_stream()
+    stream_h = declare_stream()
+    with for_(var=N, init=0, cond=N < N_shots, update=N + 1):
+        for process in processes.keys():  # 1/4
+            for input_state in state_prep.keys():  # 16
+                for readout_operator in tomography_set.keys():  # 15
+                    align("RR", "control", "TC")
+                    for pulse in state_prep[input_state]:
+                        play_pulse(pulse)
+                    for pulse in processes[process]:
+                        play_pulse(pulse)
+
+                    for op in tomography_set[readout_operator]:
+                        play_pulse(op)
+
+                    align("RR", "control", "TC")
+                    measure_and_reset_state("RR", I, Q, state_c, state_h, stream_c, stream_h)
+
+    with stream_processing():
+        stream_c.boolean_to_int().buffer(nb_processes, nb_basis_states, nb_Pauli_op).average().save("state_c")
+        stream_h.boolean_to_int().buffer(nb_processes, nb_basis_states, nb_Pauli_op).average().save("state_h")
+
+job = qm1.simulate(config, hidden_qubit_tomography,
+                   SimulationConfig(int(50000), simulation_interface=LoopbackInterface(
+                       [("con1", 1, "con1", 1)])))  # Use LoopbackInterface to simulate the response of the qubit
+
+results = job.result_handles
+
+control = results.state_c.fetch_all()
+hidden = results.state_h.fetch_all()
+
+counts = {}
+frequencies = {}
+expectation_values = {}
+ρ = {}
+for process in processes.keys():  # 4 #Initialize all the dictionaries
+    counts[process] = {}
+    frequencies[process] = {}
+    expectation_values[process] = {}
+    ρ[process] = {}
+    for input_state in state_prep.keys():
+        counts[process][input_state] = {}  # 16
+        frequencies[process][input_state] = {}
+        expectation_values[process][input_state] = {}
+        ρ[process][input_state] = {}
+        for readout_operator in tomography_set.keys():
+            counts[process][input_state][readout_operator] = {"00": 0,
+                                                              "01": 0,
+                                                              "10": 0,
+                                                              "11": 0}
+            frequencies[process][input_state][readout_operator] = {"00": 0,
+                                                                   "01": 0,
+                                                                   "10": 0,
+                                                                   "11": 0}
+            expectation_values[process][input_state][readout_operator] = 0
+i = 0
+j = 0
+k = 0
+
+#Add here in appropriate dictionaries results retrieved from stream_processing
+for process in processes.keys():
+    for input_state in state_prep.keys():
+        for readout_operator in tomography_set.keys():
+            state = str(control[i][j][k]) + str(hidden[i][j][k])
+            counts[process][input_state][readout_operator][state] += 1
+            #frequencies[process][input_state][readout_operator][state] += 1. / N_shots
+            if (state == "00") or (state == "01"):
+                expectation_values[process][input_state][readout_operator] += 1./N_shots
+            else:
+                expectation_values[process][input_state][readout_operator] -= 1. / N_shots
+            k += 1
+        j += 1
+    i += 1
+
+ID = np.array([[1, 0],
+               [0, 1]])
+σ_x = np.array([[0, 1],
+                [1, 0]])
+σ_y = np.array([[0, -1j],
+                [1j, 0]])
+σ_z = np.array([[1, 0],
+                [0, -1]])
+Pauli_matrices = [ID, σ_x, σ_y, σ_z]
+
+Pauli_basis = {
+    #"ID__ID": np.kron(ID, ID),
+    "ID__σ_x": np.kron(ID, σ_x),
+    "ID__σ_y": np.kron(ID, σ_y),
+    "ID__σ_z": np.kron(ID, σ_z),
+
+    "σ_z__ID": np.kron(σ_z, ID),
+    "σ_z__σ_x": np.kron(σ_z, σ_x),
+    "σ_z__σ_y": np.kron(σ_z, σ_y),
+    "σ_z__σ_z": np.kron(σ_z, σ_z),
+
+    "σ_y__ID": np.kron(σ_y, ID),
+    "σ_y__σ_x": np.kron(σ_y, σ_x),
+    "σ_y__σ_y": np.kron(σ_y, σ_y),
+    "σ_y__σ_z": np.kron(σ_y, σ_z),
+
+    "σ_x__ID": np.kron(σ_x, ID),
+    "σ_x__σ_x": np.kron(σ_x, σ_x),
+    "σ_x__σ_y": np.kron(σ_x, σ_y),
+    "σ_x__σ_z": np.kron(σ_x, σ_z),
+}
+
+#Choose here the state tomography method to be used, then the process tomography scheme
+#:
+
