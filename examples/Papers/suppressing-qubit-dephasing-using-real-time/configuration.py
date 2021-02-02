3ebc5aafc22a49b422a800dbf19af2d21625a72d
galW <gal@quantum-machines.co>
2021-02-02 21:23:16 +0200

    WIP

---
+++ examples/Papers/suppressing-qubit-dephasing-using-real-time/configuration.py
@@ -0,0 +1,169 @@
+import numpy as np
+
+gauss_pulse_len = 20  # ns
+const_pulse_len = 100
+Amp = 0.2  # Pulse Amplitude
+gauss_arg = np.linspace(-3, 3, gauss_pulse_len)
+gauss_wf = np.exp(-(gauss_arg ** 2) / 2)
+gauss_wf = Amp * gauss_wf / np.max(gauss_wf)
+readout_pulse_len = 20
+omega_10 = 4.958e9
+omega_d = omega_10/3
+
+config = {
+    "version": 1,
+    "controllers": {
+        "con1": {
+            "type": "opx1",
+            "analog_outputs": {
+                1: {"offset": +0.0},
+                2: {"offset": +0.0},
+                3: {"offset": +0.0},
+                4: {"offset": +0.0},
+                5: {"offset": +0.0},
+            },
+            "analog_inputs": {
+                1: {"offset": +0.0},
+            },
+        },
+    },
+    "elements": {
+        "qubit_control": {
+            "mixInputs": {
+                "I": ("con1", 1),
+                "Q": ("con1", 2),
+                "lo_frequency": 5.10e9,
+                "mixer": "mixer_qubit",
+            },
+            "intermediate_frequency": 0,  # ω_d,
+            "operations": {
+                "gauss_pulse": "gauss_pulse_in",  # to a pulse
+                "pi_pulse": "pi_pulse_in"
+            },
+        },
+        "SFQ_trigger": {
+            "mixInputs": {
+                "I": ("con1", 1),
+                "Q": ("con1", 2),
+                "lo_frequency": 5.10e9,
+                "mixer": "mixer_qubit",
+            },
+            "intermediate_frequency": 0,  # ω_d,
+            "operations": {
+                "const_pulse": "const_pulse_in",  # to a pulse
+                "pi_pulse": "pi_pulse_in"
+            },
+        },
+        "qubit_flux_bias": {
+            "singleInput": {"port": ("con1", 5)},
+            "digitalInputs": {
+                "digital_input1": {
+                    "port": ("con1", 1),
+                    "delay": 0,
+                    "buffer": 0,
+                },
+            },
+            "intermediate_frequency": 5e6,
+            "operations": {
+                "playOp": "constPulse",
+            },
+        },
+
+        "RR": {
+            "mixInputs": {
+                "I": ("con1", 3),
+                "Q": ("con1", 4),
+                "lo_frequency": 6.00e9,
+                "mixer": "mixer_res",
+            },
+            "intermediate_frequency": 50e6,  # 6.15e9,
+            "operations": {
+                "meas_pulse": "meas_pulse_in",
+            },
+            "time_of_flight": 180,  # Measurement parameters
+            "smearing": 0,
+            "outputs": {"out1": ("con1", 1)},
+        },
+        "SFQ_bias": {
+            "singleInput": {"port": ("con1", 5)},
+            "digitalInputs": {
+                "digital_input1": {
+                    "port": ("con1", 1),
+                    "delay": 0,
+                    "buffer": 0,
+                },
+            },
+            "intermediate_frequency": 5e6,
+            "operations": {
+                "playOp": "constPulse",
+            },
+        }
+    },
+    "pulses": {
+        "meas_pulse_in": {  # Readout pulse
+            "operation": "measurement",
+            "length": readout_pulse_len,
+            "waveforms": {
+                "I": "exc_wf",  # Decide what pulse to apply for each component
+                "Q": "zero_wf",
+            },
+            "integration_weights": {
+                "integW1": "integW1",
+                "integW2": "integW2",
+            },
+        },
+        "constPulse": {
+            "operation": "control",
+            "length": const_pulse_len,  # in ns
+            "waveforms": {"single": "const_wf"}
+        },
+        "gauss_pulse_in": {
+            "operation": "control",
+            "length": gauss_pulse_len,
+            "waveforms": {"I": "gauss_wf", "Q": "zero_wf"},
+        },
+        "pi_pulse_in": {  # Assumed to be calibrated
+            "operation": "control",
+            "length": const_pulse_len,
+            "waveforms": {"I": "const_wf", "Q": "zero_wf"},
+        },
+        "const_pulse_in": {  # Assumed to be calibrated
+            "operation": "control",
+            "length": const_pulse_len,
+            "waveforms": {"I": "const_wf", "Q": "zero_wf"},
+        },
+    },
+    "waveforms": {
+        "const_wf": {"type": "constant", "sample": 0.2},
+        "zero_wf": {"type": "constant", "sample": 0.0},
+        "gauss_wf": {"type": "arbitrary", "samples": gauss_wf.tolist()},
+        "exc_wf": {"type": "constant", "sample": 0.479},
+    },
+    "integration_weights": {  # Define integration weights for measurement demodulation
+        "integW1": {
+            "cosine": [4.0] * readout_pulse_len,
+
+            "sine": [0.0] * readout_pulse_len
+        },
+        "integW2": {
+            "cosine": [0.0] * readout_pulse_len,
+            "sine": [4.0] * readout_pulse_len
+        },
+    },
+    "mixers": {  # Potential corrections to be brought related to the IQ mixing scheme
+        "mixer_res": [
+            {
+                "intermediate_frequency": 50e6,  # 6.15e9,
+                "lo_frequency": 6.00e9,
+                "correction": [1.0, 0.0, 0.0, 1.0],
+            }
+        ],
+        "mixer_qubit": [
+            {
+                "intermediate_frequency": 0,  # ω_d,
+                "lo_frequency": 5.10e9,
+                "correction": [1.0, 0.0, 0.0, 1.0],
+            }
+        ],
+    },
+}
---
+++ examples/Papers/suppressing-qubit-dephasing-using-real-time/suppressing-dephasing-real-time.py
@@ -0,0 +1,91 @@
+"""
+suppressing-dephasing-real-time.py: suppressing dephasing using real-time Hamiltonian estimation
+Author: Gal Winer - Quantum Machines
+Created: 02/02/2021
+Created on QUA version: 0.8.439
+"""
+
+# Importing the necessary from qm
+from qm.QuantumMachinesManager import QuantumMachinesManager
+from qm.qua import *
+from qm import SimulationConfig
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+from configuration import *
+
+
+N_max = 3
+t_max = int(500 / 4)
+dt = 1
+N_t = t_max // dt
+f_max = 1.658e9
+f_min = 1.648e9
+df = 1.e8
+N_f = int((f_max-f_min)/df)
+
+qmManager = QuantumMachinesManager()
+QM = qmManager.open_qm(config)  # Generate a Quantum Machine based on the configuration described above
+
+with program() as bias_current_sweeping:  #
+    I = declare(fixed)  # QUA variables declaration
+    Q = declare(fixed)
+    state = declare(bool)
+    th = declare(fixed, value=2.0)  # Threshold assumed to have been calibrated by state discrimination exp
+    t = declare(int)
+    f = declare(fixed)
+    Nrep = declare(int)  # Variable looping for repetitions of experiments
+    tau = declare(fixed, value=1.0)
+    f_stream = declare_stream()
+    state_stream = declare_stream()
+    t_stream = declare_stream()
+    I_stream = declare_stream()
+    Q_stream = declare_stream()
+
+    with for_(Nrep, 0, Nrep < N_max, Nrep + 1):
+        with for_(f, f_min, f < f_max, f + df):  # Sweep from 0 to V_c the bias voltage
+            with for_(t, 0.00, t < t_max, t + dt):
+                update_frequency("qubit", f)
+                play("playOp", "SFQ_bias")
+                play("pi_pulse" * amp(0.5), 'SFQ_trigger', duration=t)  # π/2 pulse
+                wait(t, "SFQ_trigger")
+                play("pi_pulse" * amp(0.5), 'SFQ_trigger', duration=t)  # π/2 pulse
+                align("qubit", "RR", "SFQ_trigger")
+                measure("meas_pulse", "RR", "samples", ("integW1", I), ("integW2", Q))
+                assign(state, I > th)
+                save(I, I_stream)
+                save(Q, Q_stream)
+                with while_(I > th):  # Active reset
+                    play("pi_pulse", 'SFQ_trigger', condition=I > th)
+                    measure("meas_pulse", "RR", "samples", ("integW1", I), ("integW2", Q))
+                save(state, state_stream)
+                save(t, t_stream)
+
+            save(f, f_stream)
+    with stream_processing():
+        I_stream.save_all("I")
+        Q_stream.save_all("Q")
+        f_stream.buffer(N_f).save("f")
+        t_stream.buffer(N_t).save('t')
+        state_stream.boolean_to_int().buffer(N_f, N_t).average().save("state")
+
+job = qmManager.simulate(config, bias_current_sweeping,
+                         SimulationConfig(int(50000)))  # Use LoopbackInterface to simulate the response of the qubit
+time.sleep(1.0)
+
+# Retrieving results of the experiments
+results = job.result_handles
+f = results.f.fetch_all()
+t = results.t.fetch_all()
+state = results.state.fetch_all()
+I = results.I.fetch_all()["value"]
+Q = results.Q.fetch_all()["value"]
+
+fig = plt.figure()
+
+# Plot the surface.
+plt.pcolormesh(f, t, state, shading="nearest")
+plt.xlabel("ω_d/2π [GHz]")
+plt.ylabel("wait time [ns]")
+plt.colorbar()
+plt.title("Ramsey experiment")
