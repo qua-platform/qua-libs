14ad9d5053b75a2dcea7772f5078bf2fbce4e8e4
Arthur Strauss <arthur@quantum-machines.co>
2021-03-16 08:56:27 +0100

    Added code comments and doc

--- bakary/Ramsey_Gauss_baking.py
+++ bakary/Ramsey_Gauss_baking.py
@@ -12,14 +12,13 @@ from Configuration_Maker_RamseyGauss import *
 from time import sleep
 from matplotlib import pyplot as plt
 
-
 Resonator_TOF = 332
 Readout_pulse_length = 100
 Fastload_length = 60
 Drive_pulse_length = 200
 
-FreqCW = 6.6069 #meas freq
-freq = 9.283 #drive freq
+FreqCW = 6.6069  # meas freq
+freq = 9.283  # drive freq
 
 Tpihalf = 16
 wait_time_cc = 100
@@ -27,29 +26,28 @@ npts = 64
 
 amplitude_pihalf = 0.1
 angle = np.pi / 4 + 0.65
-dephasing0 = 0 #phase at the origin of the 2nd Tpihalf gauss pulse
-#dephasingStep = np.pi/7 #beware, pulses with weird shapes for some values of dephasingStep
+dephasing0 = 0  # phase at the origin of the 2nd Tpihalf gauss pulse
+# dephasingStep = np.pi/7 #beware, pulses with weird shapes for some values of dephasingStep
 dephasingStep = 0
 Drive_gauss_pulse_length = Tpihalf
 Drive_Amp = 0.1
 gauss_drive_amp = 0.1
 gauss_drive_mu = 0
-gauss_drive_sigma = Drive_gauss_pulse_length/6
+gauss_drive_sigma = Drive_gauss_pulse_length / 6
 Inverse_Readout_pulse_length = 1e9 / Readout_pulse_length
-drive_cc = int(Tpihalf/4) + 4 #12cc = 48ns for Tpihalf=32
+drive_cc = int(Tpihalf / 4) + 4  # 12cc = 48ns for Tpihalf=32
 
+configRamseyGauss = configMakerRamseyGauss(Resonator_TOF=Resonator_TOF, Readout_pulse_length=Readout_pulse_length,
+                                           Load_pulse_length=Fastload_length, Resonator_freq=FreqCW * 1e9,
+                                           Drive_freq=freq * 1e9, Tpihalf=Tpihalf)
 
-configRamseyGauss = configMakerRamseyGauss(Resonator_TOF = Resonator_TOF, Readout_pulse_length = Readout_pulse_length,
-                                      Load_pulse_length = Fastload_length, Resonator_freq = FreqCW*1e9,
-                                      Drive_freq = freq*1e9, Tpihalf = Tpihalf)
-
-dmax = int(npts/4)
+dmax = int(npts / 4)
 period_IF = 31.25e6
 
-baking_list = [] #Stores the baking objects
-for i in range(16):
+baking_list = []  # Stores the baking objects
+for i in range(16): # Create 16 different baked sequences
     with baking(configRamseyGauss, padding_method="left") as b:
-        init_delay = 19   # Put initial delay to ensure synchronicity between
+        init_delay = 19  # Put initial delay to ensure synchronicity between
         # the end of the pulse and the trigger of the resonator
 
         # Create and add gaussian sample to be added for Ramsey experiment
@@ -62,7 +60,7 @@ for i in range(16):
 
         # Play uploads the sample in the original config file
         b.play("gauss_drive", 'Drive', amp=1)  # duration Tpihalf+16
-        b.play_at('gauss_drive', 'Drive', init_delay-i)  # duration Tpihalf #Add error for negative values
+        b.play_at('gauss_drive', 'Drive', init_delay - i)  # duration Tpihalf #Add error for negative values
 
     # Append the baking object in the list to call it from the QUA program
     baking_list.append(b)
@@ -110,7 +108,8 @@ with program() as RamseyGauss:  # to measure Rabi flops every 1ns starting from 
                         demod.full("integW_sin", Q1, "out1"),
                         demod.full("integW_cos", I2, "out2"),
                         demod.full("integW_sin", Q2, "out2"))
-                assign(I, I1 + Q2)  # summing over all the items in the vectors before assigning to the final I and Q variables
+                assign(I,
+                       I1 + Q2)  # summing over all the items in the vectors before assigning to the final I and Q variables
                 assign(Q, I2 - Q1)
                 assign(dephasing, dephasing + dephasingStep)
                 assign(pw, 4 * d + j)  # delay in ns between the 2 pihalf gauss pulses
@@ -128,9 +127,7 @@ with program() as RamseyGauss:  # to measure Rabi flops every 1ns starting from 
         param_stream.buffer(npts).average().save('param')
         phase_stream.buffer(npts).average().save('dephasing')
 
-
 simulate = True
-
 
 qmm = QuantumMachinesManager("3.122.60.129")
 qm = qmm.open_qm(configRamseyGauss)
