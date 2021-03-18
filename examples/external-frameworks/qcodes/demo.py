75aed7ea1caa0187c0b400c608ea6f4ed165f894
gal <gal@quantum-machines.co>
2021-03-18 15:11:44 +0200

    WIP Qcodes driver

---
+++ examples/external-frameworks/qcodes/demo.py
@@ -0,0 +1,81 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import qcodes as qc
+from qcodes import (
+    Measurement,
+    experiments,
+    initialise_database,
+    initialise_or_create_database_at,
+    load_by_guid,
+    load_by_run_spec,
+    load_experiment,
+    load_last_experiment,
+    load_or_create_experiment,
+    new_experiment,
+)
+from qcodes.dataset.plotting import plot_dataset
+from qcodes.logger.logger import start_all_logging
+# from qcodes.tests.instrument_mocks import DummyInstrument, DummyInstrumentWithMeasurement
+
+from OPX_driver import *
+
+
+config = {
+    "version": 1,
+    "controllers": {
+        "con1": {
+            "type": "opx1",
+            "analog_outputs": {
+                1: {"offset": +0.0},
+            },
+        }
+    },
+    "elements": {
+        "qe1": {
+            "singleInput": {"port": ("con1", 1)},
+            "intermediate_frequency": 5e6,
+            "operations": {
+                "playOp": "constPulse",
+            },
+        },
+    },
+    "pulses": {
+        "constPulse": {
+            "operation": "control",
+            "length": 1000,  # in ns
+            "waveforms": {"single": "const_wf"},
+        },
+    },
+    "waveforms": {
+        "const_wf": {"type": "constant", "sample": 0.2},
+    },
+}
+
+
+with program() as prog:
+    r = Random()
+    a= declare(fixed)
+    assign(a,r.rand_fixed())
+    result_str = declare_stream()
+    play("playOp", "qe1")
+    save(a,result_str)
+    with stream_processing():
+        result_str.save_all('result')
+
+
+opx = OPX(config)
+station = qc.Station()
+station.add_component(opx)
+exp = load_or_create_experiment(experiment_name='dataset_context_manager',
+                                sample_name="no sample1")
+
+meas = Measurement(exp=exp, station=station)
+idp=Parameter(name='idp',set_cmd=None)
+dp=Parameter(name='dp',get_cmd=None)
+meas.register_parameter(idp)  # register the first independent parameter
+meas.register_parameter(dp)  # now register the dependent oone
+with meas.run() as datasaver:
+    for n in range(3):
+        opx.simulate_prog(prog)
+        datasaver.add_result((dp,opx.get_res()),(idp,n))
+    dataset = datasaver.dataset
