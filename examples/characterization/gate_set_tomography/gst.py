0e9341a116bf4b1a5a0180ffcd158a4ec7d91f53
Ilan Mitnikov <ilan@quantum-machines.co>
2021-07-12 17:30:48 +0300

    class init

--- examples/characterization/gate_set_tomography/gst.py
+++ examples/characterization/gate_set_tomography/gst.py
@@ -1,5 +1,15 @@
 import numpy as np
 from qm.qua import *
 from pygsti.construction import make_lsgst_experiment_list
-from pygsti.modelpacks import smq1Q_XYI
+from pygsti.modelpacks import GSTModelPack
 import pygsti
+
+
+class QuaGST:
+    def __init__(self, model: GSTModelPack, *gate_macros):
+
+        self.pygsti_model = model
+        self.gates = gate_macros
+
+        assert len(self.pygsti_model.gates) == len(self.gates)
+
