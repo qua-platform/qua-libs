968c0250a4ffd051aa0332f803e2ec47a56bc4ea
Ilan Mitnikov <ilan@quantum-machines.co>
2021-07-27 09:56:12 +0300

    small fix

--- examples/characterization/gate_set_tomography/example.py
+++ examples/characterization/gate_set_tomography/example.py
@@ -20,7 +20,7 @@ def id_gate():
     wait(1000, "qe1")
 
 
-def post(out_st):
+def post_circuit(out_st):
     I = declare(fixed)
     measure("readoutOp", "qe1", None, I)
     save(I, out_st)
@@ -28,6 +28,9 @@ def post(out_st):
 
 GST_sequence_file = 'Circuits_before_results.txt'
 gate_macros = {"xpi2:0": x_pi2, "ypi2:0": y_pi2, "[]": id_gate}
-gst = QuaGST(GST_sequence_file, model=smq1Q_XYI, basic_gates_macros=gate_macros, N_shots=1, post_circuit=post,
+gst = QuaGST(GST_sequence_file, model=smq1Q_XYI, basic_gates_macros=gate_macros, N_shots=1, post_circuit=post_circuit,
              config=config, quantum_machines_manager=qmm, simulate=SimulationConfig(int(1e4)))
 gst.run(300)
+# gst.run_IO()
+
+results = gst.results
