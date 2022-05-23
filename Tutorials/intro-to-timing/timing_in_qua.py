90d7ee2705bc7d55ff6f5fe9d3fd529b1e3ee257
Michal Goldenshtein <michal@quantum-machines.co>
2022-05-23 14:55:26 +0300

    black

--- Tutorials/intro-to-timing/timing_in_qua.py
+++ Tutorials/intro-to-timing/timing_in_qua.py
@@ -40,7 +40,7 @@ plt.subplot(2, 1, 1)
 samples.con1.plot("qe1")
 plt.xlabel("Time[ns]")
 plt.ylabel("qe1")
-plt.title('example 1 - Two pulses from different elements')
+plt.title("example 1 - Two pulses from different elements")
 plt.subplot(2, 1, 2)
 samples.con1.plot("qe2")
 plt.xlabel("Time[ns]")
@@ -49,9 +49,8 @@ plt.ylabel("qe2")
 plt.subplots_adjust(left=0.1, bottom=0.1, right=0.9, top=0.9, wspace=0.4, hspace=0.4)
 
 
-
 # Simulate and plot 2nd example
-job = QMm.simulate(config, example2, SimulationConfig(int(1000)))  # in clock cycles, 4 ns
+job = qmm.simulate(config, example2, SimulationConfig(int(1000)))  # in clock cycles, 4 ns
 samples = job.get_simulated_samples()
 
 plt.figure(2)
@@ -60,7 +59,7 @@ plt.subplot(2, 1, 1)
 samples.con1.plot("qe1")
 plt.xlabel("Time[ns]")
 plt.ylabel("qe1")
-plt.title('example 2 - Two pulses from different elements with wait command')
+plt.title("example 2 - Two pulses from different elements with wait command")
 plt.subplot(2, 1, 2)
 samples.con1.plot("qe2")
 plt.xlabel("Time[ns]")
@@ -69,25 +68,25 @@ plt.ylabel("qe2")
 plt.subplots_adjust(left=0.1, bottom=0.1, right=0.9, top=0.9, wspace=0.4, hspace=0.4)
 
 # Simulate and plot 3rd example
-job = QMm.simulate(config, example3, SimulationConfig(int(1000)))  # in clock cycles, 4 ns
+job = qmm.simulate(config, example3, SimulationConfig(int(1000)))  # in clock cycles, 4 ns
 samples = job.get_simulated_samples()
 
 plt.figure(3)
 samples.con1.plot()
-plt.title('example 3 - Two pulses from different elements \n with align command (deterministic case)')
+plt.title("example 3 - Two pulses from different elements \n with align command (deterministic case)")
 
 # Simulate and plot 4th example
-job = QMm.simulate(config, example4, SimulationConfig(int(1000)))  # in clock cycles, 4 ns
+job = qmm.simulate(config, example4, SimulationConfig(int(1000)))  # in clock cycles, 4 ns
 samples = job.get_simulated_samples()
 
 plt.figure(4)
 samples.con1.plot()
-plt.title('example 4 - from different elements \n with align command (non-deterministic case)')
+plt.title("example 4 - from different elements \n with align command (non-deterministic case)")
 
 # Simulate and plot 5th example
-job = QMm.simulate(config, example5, SimulationConfig(int(1000)))  # in clock cycles, 4 ns
+job = qmm.simulate(config, example5, SimulationConfig(int(1000)))  # in clock cycles, 4 ns
 samples = job.get_simulated_samples()
 
 plt.figure(5)
 samples.con1.plot()
-plt.title('example 5 - Two pulses from the same element')
+plt.title("example 5 - Two pulses from the same element")
