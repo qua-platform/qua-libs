03d60e40faeb18e5eab68de442bb833404bc9fa4
Yoav Romach <yomach@gmail.com>
2022-06-01 13:34:07 +0300

    added comments about simulation/execution in hello_qua

--- Quantum-Control-Applications/Superconducting/Single Fixed Transmon/hello_qua.py
+++ Quantum-Control-Applications/Superconducting/Single Fixed Transmon/hello_qua.py
@@ -18,8 +18,11 @@ qmm = QuantumMachinesManager(qop_ip)
 qm = qmm.open_qm(config)
 simulation_duration = 400  # clock cycle units - 4ns
 job_sim = qm.simulate(hello_QUA, SimulationConfig(simulation_duration))
+# Simulate blocks python until the simulation is done
 job_sim.get_simulated_samples().con1.plot()
 
 job = qm.execute(hello_QUA)
+# Execute does not block python! As this is an infinite loop, the job would run forever. In this case, we've put a 10
+# seconds sleep and then halted the job.
 time.sleep(10)
 job.halt()
