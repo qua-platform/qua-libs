def1ecb54c128668e818c84f12d50cd61fee6fd8
Kevin Villegas <kevin@quantum-machines.co>
2022-06-15 16:50:47 -0400

    Ramsey with frame rotation
    addded the modifications so as to Ramsey without update frequency

--- Quantum-Control-Applications/Superconducting/Single Fixed Transmon/ramsey_phase_rotation.py
+++ Quantum-Control-Applications/Superconducting/Single Fixed Transmon/ramsey_phase_rotation.py
@@ -1,6 +1,3 @@
-"""
-Measures T2*
-"""
 from qm.qua import *
 from qm.QuantumMachinesManager import QuantumMachinesManager
 from configuration import *
@@ -30,14 +27,15 @@ with program() as ramsey:
     Q = declare(fixed)
     Q_st = declare_stream()
     tau = declare(int)
-
-    update_frequency('qubit', detuning)
+    phase = declare(fixed)
 
     with for_(n, 0, n < n_avg, n + 1):
         # Notice it's <= to include t_max (This is only for integers!)
         with for_(tau, tau_min, tau <= tau_max, tau + dtau):
             play("pi_half", "qubit")
+            assign(phase, Cast.mul_fixed_by_int(detuning*1e-9, 4*tau))
             wait(tau, "qubit")
+            frame_rotation_2pi(phase)
             play("pi_half", "qubit")
             align("qubit", "resonator")
             measure(
@@ -50,6 +48,7 @@ with program() as ramsey:
             save(I, I_st)
             save(Q, Q_st)
             wait(cooldown_time, "resonator")
+            reset_frame('qubit')
         save(n, n_st)
 
     with stream_processing():
@@ -60,7 +59,7 @@ with program() as ramsey:
 #####################################
 #  Open Communication with the QOP  #
 #####################################
-qmm = QuantumMachinesManager(qop_ip)
+qmm = QuantumMachinesManager(host=qop_ip, port=qop_port)
 
 #######################
 # Simulate or execute #
@@ -108,7 +107,7 @@ else:
         plt.plot(taus, I, ".", label="I")
         plt.plot(taus, Q, ".", label="Q")
         plt.xlabel('Time in the equator')
-        plt.title('Ramsey freq detuning')
+        plt.title('Ramsey with frame rotation')
 
         plt.legend()
         plt.pause(0.1)
@@ -121,5 +120,5 @@ else:
     plt.plot(taus, I, ".", label="I")
     plt.plot(taus, Q, ".", label="Q")
     plt.xlabel('Time in the equator')
-    plt.title('Ramsey freq detuning')
+    plt.title('Ramsey with frame rotation')
     plt.legend()
