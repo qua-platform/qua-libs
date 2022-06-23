5e51ba3a54246fb2bd1676a077d85e70d378080e
Yoav Romach <yomach@gmail.com>
2022-06-23 09:10:35 +0300

    rb without ssr

---
+++ Quantum-Control-Applications/Superconducting/Single Fixed Transmon/rb_without_singleshot_readout.py
@@ -0,0 +1,255 @@
+"""
+Performs a 1 qubit randomized benchmarking to measure the 1 qubit gate fidelity. This version is using directly the I
+& Q data and should be used when there is no single-shot readout
+"""
+from qm.qua import *
+from qm.QuantumMachinesManager import QuantumMachinesManager
+from scipy.optimize import curve_fit
+from configuration import *
+import matplotlib.pyplot as plt
+import numpy as np
+from qualang_tools.bakery.randomized_benchmark_c1 import c1_table
+
+inv_gates = [int(np.where(c1_table[i, :] == 0)[0][0]) for i in range(24)]
+max_circuit_depth = int(3 * qubit_T1 / x180_len)
+delta_depth = 1
+num_of_sequences = 50
+n_avg = 20000
+seed = 345324
+cooldown_time = 5 * qubit_T1 // 4
+
+qmm = QuantumMachinesManager(qop_ip)
+
+
+def generate_sequence():
+    cayley = declare(int, value=c1_table.flatten().tolist())
+    inv_list = declare(int, value=inv_gates)
+    current_state = declare(int)
+    step = declare(int)
+    sequence = declare(int, size=max_circuit_depth + 1)
+    inv_gate = declare(int, size=max_circuit_depth + 1)
+    i = declare(int)
+    rand = Random(seed=seed)
+
+    assign(current_state, 0)
+    with for_(i, 0, i < max_circuit_depth, i + 1):
+        assign(step, rand.rand_int(24))
+        assign(current_state, cayley[current_state * 24 + step])
+        assign(sequence[i], step)
+        assign(inv_gate[i], inv_list[current_state])
+
+    return sequence, inv_gate
+
+
+def play_sequence(sequence_list, depth):
+    i = declare(int)
+    with for_(i, 0, i <= depth, i + 1):
+        with switch_(sequence_list[i], unsafe=True):
+            with case_(0):
+                wait(x180_len // 4, "qubit")
+            with case_(1):
+                play("x180", "qubit")
+            with case_(2):
+                play("y180", "qubit")
+            with case_(3):
+                play("y180", "qubit")
+                play("x180", "qubit")
+            with case_(4):
+                play("x90", "qubit")
+                play("y90", "qubit")
+            with case_(5):
+                play("x90", "qubit")
+                play("y270", "qubit")
+            with case_(6):
+                play("x270", "qubit")
+                play("y90", "qubit")
+            with case_(7):
+                play("x270", "qubit")
+                play("y270", "qubit")
+            with case_(8):
+                play("y90", "qubit")
+                play("x90", "qubit")
+            with case_(9):
+                play("y90", "qubit")
+                play("x270", "qubit")
+            with case_(10):
+                play("y270", "qubit")
+                play("x90", "qubit")
+            with case_(11):
+                play("y270", "qubit")
+                play("x270", "qubit")
+            with case_(12):
+                play("x90", "qubit")
+            with case_(13):
+                play("x270", "qubit")
+            with case_(14):
+                play("y90", "qubit")
+            with case_(15):
+                play("y270", "qubit")
+            with case_(16):
+                play("x270", "qubit")
+                play("y90", "qubit")
+                play("x90", "qubit")
+            with case_(17):
+                play("x270", "qubit")
+                play("y270", "qubit")
+                play("x90", "qubit")
+            with case_(18):
+                play("x180", "qubit")
+                play("y90", "qubit")
+            with case_(19):
+                play("x180", "qubit")
+                play("y270", "qubit")
+            with case_(20):
+                play("y180", "qubit")
+                play("x90", "qubit")
+            with case_(21):
+                play("y180", "qubit")
+                play("x270", "qubit")
+            with case_(22):
+                play("x90", "qubit")
+                play("y90", "qubit")
+                play("x90", "qubit")
+            with case_(23):
+                play("x270", "qubit")
+                play("y90", "qubit")
+                play("x270", "qubit")
+
+
+with program() as rb:
+    depth = declare(int)
+    saved_gate = declare(int)
+    m = declare(int)
+    n = declare(int)
+    n_st = declare_stream()
+    I = declare(fixed)
+    Q = declare(fixed)
+    I_st = declare_stream()
+    Q_st = declare_stream()
+
+    with for_(n, 0, n < n_avg, n + 1):
+        with for_(m, 0, m < num_of_sequences, m + 1):
+            sequence_list, inv_gate_list = generate_sequence()
+
+            with for_(depth, 1, depth <= max_circuit_depth, depth + delta_depth):
+                # Replacing the last gate in the sequence with the sequence's inverse gate
+                # The original gate is saved in 'saved_gate' and is being restored at the end
+                assign(saved_gate, sequence_list[depth])
+                assign(sequence_list[depth], inv_gate_list[depth - 1])
+
+                wait(cooldown_time, "resonator")
+
+                align("resonator", "qubit")
+
+                play_sequence(sequence_list, depth)
+                align("qubit", "resonator")
+                measure("readout", "resonator", None,
+                    dual_demod.full("cos", "out1", "sin", "out2", I),
+                    dual_demod.full("minus_sin", "out1", "cos", "out2", Q),
+                )
+
+                save(I, I_st)
+                save(Q, Q_st)
+
+                assign(sequence_list[depth], saved_gate)
+        save(n, n_st)
+
+    with stream_processing():
+        I_st.buffer(max_circuit_depth).buffer(num_of_sequences).average().save("I")
+        Q_st.buffer(max_circuit_depth).buffer(num_of_sequences).average().save("Q")
+
+qm = qmm.open_qm(config)
+
+job = qm.execute(rb)
+res_handles = job.result_handles
+
+I_handle = res_handles.get("I")
+Q_handle = res_handles.get("Q")
+iteration_handle = res_handles.get("iteration")
+I_handle.wait_for_values(1)
+Q_handle.wait_for_values(1)
+iteration_handle.wait_for_values(1)
+next_percent = 0.1  # First time print 10%
+
+x = np.linspace(1, max_circuit_depth, max_circuit_depth)
+
+
+def on_close(event):
+    event.canvas.stop_event_loop()
+    job.halt()
+
+
+f = plt.figure()
+f.canvas.mpl_connect("close_event", on_close)
+print("Progress =", end=" ")
+
+while res_handles.is_processing():
+    plt.cla()
+    I = np.average(I_handle.fetch_all(), axis=0)
+    Q = np.average(Q_handle.fetch_all(), axis=0)
+    iteration = iteration_handle.fetch_all()
+    if iteration / n_avg > next_percent:
+        percent = 10 * round(iteration / n_avg * 10)  # Round to nearest 10%
+        print(f"{percent}%", end=" ")
+        next_percent = percent / 100 + 0.1  # Print every 10%
+
+    plt.plot(x, I, ".", label="I")
+    plt.plot(x, Q, ".", label="Q")
+    plt.xlabel("Number of cliffords")
+
+    plt.legend()
+    plt.pause(0.1)
+
+plt.cla()
+I = np.average(I_handle.fetch_all(), axis=0)
+Q = np.average(Q_handle.fetch_all(), axis=0)
+iteration = iteration_handle.fetch_all()
+print(f"{round(iteration/n_avg * 100)}%")
+plt.plot(x, I, ".", label="I")
+plt.plot(x, Q, ".", label="Q")
+
+plt.legend()
+
+value = I  # Can change to Q
+
+
+def power_law(m, a, b, p):
+    return a * (p**m) + b
+
+
+pars, cov = curve_fit(
+    f=power_law,
+    xdata=x,
+    ydata=value,
+    p0=[0.5, 0.5, 0.9],
+    bounds=(-np.inf, np.inf),
+    maxfev=2000,
+)
+
+plt.plot(x, power_law(x, *pars), linestyle="--", linewidth=2)
+
+stdevs = np.sqrt(np.diag(cov))
+
+print("#########################")
+print("### Fitted Parameters ###")
+print("#########################")
+print(f"A = {pars[0]:.3} ({stdevs[0]:.1}), B = {pars[1]:.3} ({stdevs[1]:.1}), p = {pars[2]:.3} ({stdevs[2]:.1})")
+print("Covariance Matrix")
+print(cov)
+
+one_minus_p = 1 - pars[2]
+r_c = one_minus_p * (1 - 1 / 2**1)
+r_g = r_c / 1.875
+r_c_std = stdevs[2] * (1 - 1 / 2**1)
+r_g_std = r_c_std / 1.875
+
+print("#########################")
+print("### Useful Parameters ###")
+print("#########################")
+print(
+    f"1-p = {np.format_float_scientific(one_minus_p, precision=2)} ({stdevs[2]:.1}), "
+    f"r_c = {np.format_float_scientific(r_c, precision=2)} ({r_c_std:.1}), "
+    f"r_g = {np.format_float_scientific(r_g, precision=2)}  ({r_g_std:.1})"
+)
+
+np.savez("rb_values", value)
