0c31af0955029d353d33c58c6ba4fb701d46afd4
Tomer Feld <tomerf@quantum-machines.co>
2022-06-26 17:18:32 +0300

    Simulation tutorial

---
+++ Tutorials/intro-to-simulation/intro-to-simulation.py
@@ -0,0 +1,187 @@
+"""
+intro_to_simulation.py: Demonstrate usage of the simulation feature
+Author: Tomer Feld - Quantum Machines
+Created: 26/06/2022
+
+"""
+
+# readout_len = 200
+# const_len = 200
+# const_amp = 0.2
+#
+# config = {
+#     "version": 1,
+#     "controllers": {
+#         "con1": {
+#             "analog_outputs": {
+#                 1: {"offset": +0.0},
+#             },
+#             "analog_inputs": {
+#                 1: {"offset": +0.0},
+#             },
+#         }
+#     },
+#     "elements": {
+#         "qe1": {
+#             "singleInput": {"port": ("con1", 1)},
+#             "outputs": {"output1": ("con1", 1)},
+#             "intermediate_frequency": 100e6,
+#             "operations": {
+#                 'const': 'const_pulse',
+#                 "readout": "readout_pulse",
+#                 "long_readout": "long_readout_pulse",
+#             },
+#             "time_of_flight": 36,
+#             "smearing": 0,
+#         },
+#     },
+#     "pulses": {
+#         "readout_pulse": {
+#             "operation": "measure",
+#             "length": readout_len,
+#             "waveforms": {"single": "const_wf"},
+#             "digital_marker": "ON",
+#             "integration_weights": {"cos": "cosine_weights", "sin": "sine_weights"},
+#         },
+#         "const_pulse": {
+#             "operation": "control",
+#             "length": const_len,
+#             "waveforms": {"single": "const_wf"},
+#         },
+#     },
+#     "waveforms": {
+#         "const_wf": {"type": "constant", "sample": const_amp},
+#     },
+#     "digital_waveforms": {
+#         "ON": {"samples": [(1, 0)]},
+#     },
+#     "integration_weights": {
+#         "cosine_weights": {
+#             "cosine": [(1.0, readout_len)],
+#             "sine": [(0.0, readout_len)],
+#         },
+#         "sine_weights": {
+#             "cosine": [(0.0, readout_len)],
+#             "sine": [(1.0, readout_len)],
+#         },
+#     },
+# }
+
+import numpy as np
+from qm.QuantumMachinesManager import QuantumMachinesManager
+from qm import SimulationConfig
+from qm import LoopbackInterface
+from qm.qua import *
+import matplotlib.pyplot as plt
+from configuration import *
+
+qmm = QuantumMachinesManager('127.0.0.1', 9510)
+
+##
+# ## Example 1 - Basic simulation, No inputs
+
+with program() as prog1:
+    a = declare(fixed)
+    c = declare(fixed, value=0.4)
+
+    with for_(a, 0.2, a < 0.9, a + 0.1):
+        play("const" * amp(a), "qe1")
+
+    save(c, "c")
+
+# simulate program
+simulated_job = qmm.simulate(config, prog1, SimulationConfig(
+    duration=500,  # duration of simulation in units of 4ns
+))
+
+# get DAC and digital samples
+samples = simulated_job.get_simulated_samples()
+
+# plot all ports:
+samples.con1.plot()
+
+# get simulated results
+res = simulated_job.result_handles
+c = res.c.fetch_all()
+print(c)  # prints a tuple of the value and timestamp
+
+
+##
+# ## Example 2:
+
+with program() as prog2:
+    d = declare(int)
+
+    with for_(d, 10, d <= 100, d + 10):
+        play("const", "qe1")
+        play("const", "qe2", duration=d)
+        wait(50)
+
+# simulate program
+simulated_job = qmm.simulate(config, prog2, SimulationConfig(
+    duration=1700,  # duration of simulation in units of 4ns
+))
+
+# get DAC and digital samples
+samples = simulated_job.get_simulated_samples()
+
+# plot analog ports 1 and 3:
+samples.con1.plot(analog_ports={'1', '3'}, digital_ports={})
+
+# another way:
+# plt.figure()
+# plt.plot(samples.con1.analog["1"], "-")
+# plt.plot(samples.con1.analog["3"], "--")
+# plt.legend(("analog 1", "analog 3"))
+# plt.xlabel("Time [ns]")
+# plt.ylabel("Signal [V]")
+
+##
+## Example 3 - Loopback inputs
+
+len_a = len(np.arange(0.1, 0.45, 0.1))  # for stream processing buffer size
+len_f = len(np.arange(50e6, 60e6, 1e6))  # for stream processing buffer size
+
+with program() as prog2:
+    I = declare(fixed)
+    Q = declare(fixed)
+    I_stream = declare_stream()
+    Q_stream = declare_stream()
+    adc_stream = declare_stream(adc_trace=True)
+    f = declare(int)
+    a = declare(fixed)
+
+    with for_(a, 0.1, a < 0.45, a + 0.1):
+        with for_(f, 50e6, f < 61e6, f + 1e6):
+            update_frequency("qe1", f)
+            measure("readout" * amp(a), "qe1", adc_stream, demod.full('cos', I), demod.full('sin', Q))
+            save(I, I_stream)
+            save(Q, Q_stream)
+            wait(100)
+
+    with stream_processing():
+        I_stream.buffer(len_f).buffer(len_a).save('I')
+        Q_stream.buffer(len_f).buffer(len_a).save('Q')
+        adc_stream.input1().save_all('adc')
+#
+# simulate program
+simulated_job = qmm.simulate(config, prog2, SimulationConfig(
+    duration=40000,
+    simulation_interface=LoopbackInterface([("con1", 1, "con1", 1)])  # loopback from output 1 to input 1
+))
+
+# get results
+res = simulated_job.result_handles
+adc = res.get('adc').fetch_all()['value'][0]  # fetch first measurement
+I = res.get('I').fetch_all()
+Q = res.get('Q').fetch_all()
+
+plt.figure()
+plt.plot(adc)
+plt.xlabel("Time [ns]")
+plt.ylabel("ADC")
+
+plt.figure()
+plt.plot(I, Q, '.')
+plt.xlabel("I")
+plt.ylabel("Q")
---
+++ Tutorials/intro-to-simulation/readme.md
@@ -0,0 +1,38 @@
+---
+title: Introduction to Simulation
+sidebar_label: Intro to Simulation
+slug: ./
+id: index
+---
+
+This example shows usage of the hardware simulator. The simulator mimics the output of the hardware, 
+with its exact timing and voltage level, following the compilation of QUA to the FPGA's low-level. 
+It is a useful tool for predicting and debugging the outcome of a QUA program. 
+Read more on the simulator, and it's capabilities in the [QUA docs](https://qm-docs.qualang.io/guides/simulator).
+
+The Examples
+============
+
+The following script gives three examples for the usage of the simulator in the QOP.
+
+First and second exampled
+-------------
+
+The first and second demonstrates basic usage od the simulator. Note that the simulation is done via the QuantumMachinesManager (qmm) object. 
+The simulation function is given the configuration, the program and the `SimulationConfig` instance. The last sets the duration for the 
+simulation. i.e. how many clock cycles should the simulator simulate. The outcome of the simulator is a `job` object. 
+
+The examples also demonstrate how to obtain and plot the simulated output of the hardware. Finally, the first example also demonstrates
+how the simulator can simulate saving variables, as would occur in the real hardware. 
+
+Third Example
+==============
+
+The third example demonstrates a slightly more advanced usage. It shows how a loop-back connection can be defined, 
+to simulate acquisition and demodulation of ADC signals. In the example, a connection from analog output 1 of controller 1
+is connected to analog input 1 of controller 1. The example also shows that the demodulation and adc input can be simulated 
+and saved to the stream processing, which later can be fetched and analyzed. It is important to note that the data will only
+be available if the simulation duration was long enough to simulate it. FIn the current example, to "fill" the buffer in the stream processing, 
+the simulation must simulate the entire program with all the loop's iterations.  
+
+[download script](intro-to-simulation.py)
