95c7d58d61ff77f352f6d6522bc0c2d2bc6a7883
TheoQM <theo@quantum-machines.co>
2022-07-20 16:22:33 +0200

    All XY tentative tested

--- Quantum-Control-Applications/Superconducting/Single Fixed Transmon/allxy.py
+++ Quantum-Control-Applications/Superconducting/Single Fixed Transmon/allxy.py
@@ -9,9 +9,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 from qm import SimulationConfig
 
-###################
-# The QUA program #
-###################
+
+##############################
+# Program-specific variables #
+##############################
+n_points = 1e2
+cooldown_time = 5 * qubit_T1 // 4
+
+# All XY sequences. The sequence names must match corresponding operation in the config
 sequence = [  # based on https://rsl.yale.edu/sites/default/files/physreva.82.pdf-optimized_driving_0.pdf
     ("I", "I"),
     ("x180", "x180"),
@@ -35,333 +40,44 @@ sequence = [  # based on https://rsl.yale.edu/sites/default/files/physreva.82.pd
     ("x90", "x90"),
     ("y90", "y90"),
 ]
+# All XY macro generating the pulse sequences from a python list.
+def allXY(pulses):
+    """
+    Generate a QUA sequence based on the two operations written in pulses. Used to generate the all XY program.
+    **Example:** I, Q = allXY(['I', 'y90']])
 
-n_points = 1e6
-
-cooldown_time = 5 * qubit_T1 // 4
-
-with program() as ALLXY:
-    n = declare(int)
-    r = Random()
-    r_ = declare(int)
-    I = declare(fixed)
-    Q = declare(fixed)
-    I_st = [declare_stream() for _ in range(21)]
-    Q_st = [declare_stream() for _ in range(21)]
-
-    with for_(n, 0, n < n_points, n + 1):
-        assign(r_, r.rand_int(21))
-        # Can replace by active reset
-        wait(cooldown_time, "qubit")
-
-        with switch_(r_):
-            with case_(0):
-                wait(x180_len // 4, "qubit")
-                wait(x180_len // 4, "qubit")
-                align("qubit", "resonator")
-                measure(
-                    "readout",
-                    "resonator",
-                    None,
-                    dual_demod.full("cos", "out1", "sin", "out2", I),
-                    dual_demod.full("minus_sin", "out1", "cos", "out2", Q),
-                )
-                save(I, I_st[0])
-                save(Q, Q_st[0])
-            with case_(1):
-                play("x180", "qubit")
-                play("x180", "qubit")
-                align("qubit", "resonator")
-                measure(
-                    "readout",
-                    "resonator",
-                    None,
-                    dual_demod.full("cos", "out1", "sin", "out2", I),
-                    dual_demod.full("minus_sin", "out1", "cos", "out2", Q),
-                )
-                save(I, I_st[1])
-                save(Q, Q_st[1])
-            with case_(2):
-                play("y180", "qubit")
-                play("y180", "qubit")
-                align("qubit", "resonator")
-                measure(
-                    "readout",
-                    "resonator",
-                    None,
-                    dual_demod.full("cos", "out1", "sin", "out2", I),
-                    dual_demod.full("minus_sin", "out1", "cos", "out2", Q),
-                )
-                save(I, I_st[2])
-                save(Q, Q_st[2])
-            with case_(3):
-                play("x180", "qubit")
-                play("y180", "qubit")
-                align("qubit", "resonator")
-                measure(
-                    "readout",
-                    "resonator",
-                    None,
-                    dual_demod.full("cos", "out1", "sin", "out2", I),
-                    dual_demod.full("minus_sin", "out1", "cos", "out2", Q),
-                )
-                save(I, I_st[3])
-                save(Q, Q_st[3])
-            with case_(4):
-                play("y180", "qubit")
-                play("x180", "qubit")
-                align("qubit", "resonator")
-                measure(
-                    "readout",
-                    "resonator",
-                    None,
-                    dual_demod.full("cos", "out1", "sin", "out2", I),
-                    dual_demod.full("minus_sin", "out1", "cos", "out2", Q),
-                )
-                save(I, I_st[4])
-                save(Q, Q_st[4])
-            with case_(5):
-                play("x90", "qubit")
-                wait(x180_len // 4, "qubit")
-                align("qubit", "resonator")
-                measure(
-                    "readout",
-                    "resonator",
-                    None,
-                    dual_demod.full("cos", "out1", "sin", "out2", I),
-                    dual_demod.full("minus_sin", "out1", "cos", "out2", Q),
-                )
-                save(I, I_st[5])
-                save(Q, Q_st[5])
-            with case_(6):
-                play("y90", "qubit")
-                wait(x180_len // 4, "qubit")
-                align("qubit", "resonator")
-                measure(
-                    "readout",
-                    "resonator",
-                    None,
-                    dual_demod.full("cos", "out1", "sin", "out2", I),
-                    dual_demod.full("minus_sin", "out1", "cos", "out2", Q),
-                )
-                save(I, I_st[6])
-                save(Q, Q_st[6])
-            with case_(7):
-                play("x90", "qubit")
-                play("y90", "qubit")
-                align("qubit", "resonator")
-                measure(
-                    "readout",
-                    "resonator",
-                    None,
-                    dual_demod.full("cos", "out1", "sin", "out2", I),
-                    dual_demod.full("minus_sin", "out1", "cos", "out2", Q),
-                )
-                save(I, I_st[7])
-                save(Q, Q_st[7])
-            with case_(8):
-                play("y90", "qubit")
-                play("x90", "qubit")
-                align("qubit", "resonator")
-                measure(
-                    "readout",
-                    "resonator",
-                    None,
-                    dual_demod.full("cos", "out1", "sin", "out2", I),
-                    dual_demod.full("minus_sin", "out1", "cos", "out2", Q),
-                )
-                save(I, I_st[8])
-                save(Q, Q_st[8])
-            with case_(9):
-                play("x90", "qubit")
-                play("y180", "qubit")
-                align("qubit", "resonator")
-                measure(
-                    "readout",
-                    "resonator",
-                    None,
-                    dual_demod.full("cos", "out1", "sin", "out2", I),
-                    dual_demod.full("minus_sin", "out1", "cos", "out2", Q),
-                )
-                save(I, I_st[9])
-                save(Q, Q_st[9])
-            with case_(10):
-                play("y90", "qubit")
-                play("x180", "qubit")
-                align("qubit", "resonator")
-                measure(
-                    "readout",
-                    "resonator",
-                    None,
-                    dual_demod.full("cos", "out1", "sin", "out2", I),
-                    dual_demod.full("minus_sin", "out1", "cos", "out2", Q),
-                )
-                save(I, I_st[10])
-                save(Q, Q_st[10])
-            with case_(11):
-                play("x180", "qubit")
-                play("y90", "qubit")
-                align("qubit", "resonator")
-                measure(
-                    "readout",
-                    "resonator",
-                    None,
-                    dual_demod.full("cos", "out1", "sin", "out2", I),
-                    dual_demod.full("minus_sin", "out1", "cos", "out2", Q),
-                )
-                save(I, I_st[11])
-                save(Q, Q_st[11])
-            with case_(12):
-                play("y180", "qubit")
-                play("x90", "qubit")
-                align("qubit", "resonator")
-                measure(
-                    "readout",
-                    "resonator",
-                    None,
-                    dual_demod.full("cos", "out1", "sin", "out2", I),
-                    dual_demod.full("minus_sin", "out1", "cos", "out2", Q),
-                )
-                save(I, I_st[12])
-                save(Q, Q_st[12])
-            with case_(13):
-                play("x90", "qubit")
-                play("x180", "qubit")
-                align("qubit", "resonator")
-                measure(
-                    "readout",
-                    "resonator",
-                    None,
-                    dual_demod.full("cos", "out1", "sin", "out2", I),
-                    dual_demod.full("minus_sin", "out1", "cos", "out2", Q),
-                )
-                save(I, I_st[13])
-                save(Q, Q_st[13])
-            with case_(14):
-                play("x180", "qubit")
-                play("x90", "qubit")
-                align("qubit", "resonator")
-                measure(
-                    "readout",
-                    "resonator",
-                    None,
-                    dual_demod.full("cos", "out1", "sin", "out2", I),
-                    dual_demod.full("minus_sin", "out1", "cos", "out2", Q),
-                )
-                save(I, I_st[14])
-                save(Q, Q_st[14])
-            with case_(15):
-                play("y90", "qubit")
-                play("y180", "qubit")
-                align("qubit", "resonator")
-                measure(
-                    "readout",
-                    "resonator",
-                    None,
-                    dual_demod.full("cos", "out1", "sin", "out2", I),
-                    dual_demod.full("minus_sin", "out1", "cos", "out2", Q),
-                )
-                save(I, I_st[15])
-                save(Q, Q_st[15])
-            with case_(16):
-                play("y180", "qubit")
-                play("y90", "qubit")
-                align("qubit", "resonator")
-                measure(
-                    "readout",
-                    "resonator",
-                    None,
-                    dual_demod.full("cos", "out1", "sin", "out2", I),
-                    dual_demod.full("minus_sin", "out1", "cos", "out2", Q),
-                )
-                save(I, I_st[16])
-                save(Q, Q_st[16])
-            with case_(17):
-                play("x180", "qubit")
-                wait(x180_len // 4, "qubit")
-                align("qubit", "resonator")
-                measure(
-                    "readout",
-                    "resonator",
-                    None,
-                    dual_demod.full("cos", "out1", "sin", "out2", I),
-                    dual_demod.full("minus_sin", "out1", "cos", "out2", Q),
-                )
-                save(I, I_st[17])
-                save(Q, Q_st[17])
-            with case_(18):
-                play("y180", "qubit")
-                wait(x180_len // 4, "qubit")
-                align("qubit", "resonator")
-                measure(
-                    "readout",
-                    "resonator",
-                    None,
-                    dual_demod.full("cos", "out1", "sin", "out2", I),
-                    dual_demod.full("minus_sin", "out1", "cos", "out2", Q),
-                )
-                save(I, I_st[18])
-                save(Q, Q_st[18])
-            with case_(19):
-                play("x90", "qubit")
-                play("x90", "qubit")
-                align("qubit", "resonator")
-                measure(
-                    "readout",
-                    "resonator",
-                    None,
-                    dual_demod.full("cos", "out1", "sin", "out2", I),
-                    dual_demod.full("minus_sin", "out1", "cos", "out2", Q),
-                )
-                save(I, I_st[19])
-                save(Q, Q_st[19])
-            with case_(20):
-                play("y90", "qubit")
-                play("y90", "qubit")
-                align("qubit", "resonator")
-                measure(
-                    "readout",
-                    "resonator",
-                    None,
-                    dual_demod.full("cos", "out1", "sin", "out2", I),
-                    dual_demod.full("minus_sin", "out1", "cos", "out2", Q),
-                )
-                save(I, I_st[20])
-                save(Q, Q_st[20])
-
-    with stream_processing():
-        for i in range(21):
-            I_st[i].average().save(f"I{i}")
-            Q_st[i].average().save(f"Q{i}")
-
-
-def allXY(pulses, index):
-    if pulses[0] is not "I":
-        play(pulses[0], "qubit")
+    :param pulses: tuple containing a particular set of operations to play. The pulse names must match corresponding
+        operations in the config except for the identity operation that must be called 'I'.
+    :return: two QUA variables for the I and Q quadratures measured after the sequence.
+    """
+    I_xy = declare(fixed)
+    Q_xy = declare(fixed)
+    if pulses[0] != "I":
+        play(pulses[0], "qubit")  # Either play the sequence
     else:
-        wait(x180_len // 4, "qubit")
-    if pulses[1] is not "I":
-        play(pulses[1], "qubit")
+        wait(x180_len // 4, "qubit")  # or wait if sequence is identity
+    if pulses[1] != "I":
+        play(pulses[1], "qubit")  # Either play the sequence
     else:
-        wait(x180_len // 4, "qubit")
+        wait(x180_len // 4, "qubit")  # or wait if sequence is identity
     align("qubit", "resonator")
     measure(
         "readout",
         "resonator",
         None,
-        dual_demod.full("cos", "out1", "sin", "out2", I),
-        dual_demod.full("minus_sin", "out1", "cos", "out2", Q),
+        dual_demod.full("cos", "out1", "sin", "out2", I_xy),
+        dual_demod.full("minus_sin", "out1", "cos", "out2", Q_xy),
     )
-    save(I, I_st[index])
-    save(Q, Q_st[index])
+    return I_xy, Q_xy
 
 
+###################
+# The QUA program #
+###################
 with program() as ALLXY:
     n = declare(int)
     r = Random()
     r_ = declare(int)
-    I = declare(fixed)
-    Q = declare(fixed)
     I_st = [declare_stream() for _ in range(21)]
     Q_st = [declare_stream() for _ in range(21)]
 
@@ -369,55 +85,13 @@ with program() as ALLXY:
         assign(r_, r.rand_int(21))
         # Can replace by active reset
         wait(cooldown_time, "qubit")
-
-        # with switch_(r_):
-        #     for i in range(len(sequence)):
-        #         with case_(i):
-        #             allXY(sequence[i], i)
-
+        # Plays a random XY sequence
         with switch_(r_):
-            with case_(0):
-                allXY(sequence[0], 0)
-            with case_(1):
-                allXY(sequence[1], 1)
-            with case_(2):
-                allXY(sequence[2], 2)
-            with case_(3):
-                allXY(sequence[3], 3)
-            with case_(4):
-                allXY(sequence[4], 4)
-            with case_(5):
-                allXY(sequence[5], 5)
-            with case_(6):
-                allXY(sequence[6], 6)
-            with case_(7):
-                allXY(sequence[7], 7)
-            with case_(8):
-                allXY(sequence[8], 8)
-            with case_(9):
-                allXY(sequence[9], 9)
-            with case_(10):
-                allXY(sequence[10], 10)
-            with case_(11):
-                allXY(sequence[11], 11)
-            with case_(12):
-                allXY(sequence[12], 12)
-            with case_(13):
-                allXY(sequence[13], 13)
-            with case_(14):
-                allXY(sequence[14], 14)
-            with case_(15):
-                allXY(sequence[15], 15)
-            with case_(16):
-                allXY(sequence[16], 16)
-            with case_(17):
-                allXY(sequence[17], 17)
-            with case_(18):
-                allXY(sequence[18], 18)
-            with case_(19):
-                allXY(sequence[19], 19)
-            with case_(20):
-                allXY(sequence[20], 20)
+            for i in range(len(sequence)):
+                with case_(i):
+                    I, Q = allXY(sequence[i])
+                    save(I, I_st[i])
+                    save(Q, Q_st[i])
 
     with stream_processing():
         for i in range(21):
@@ -429,10 +103,10 @@ with program() as ALLXY:
 #####################################
 qmm = QuantumMachinesManager(qop_ip)
 
-simulate = True
+simulate = False
 
 if simulate:
-    simulation_config = SimulationConfig(duration=1000)  # in clock cycles
+    simulation_config = SimulationConfig(duration=100000)  # in clock cycles
     job = qmm.simulate(config, ALLXY, simulation_config)
     job.get_simulated_samples().con1.plot()
 
@@ -454,8 +128,6 @@ else:
 
     plt.figure()
     plt.plot(I)
-
-    plt.figure()
     plt.plot(Q)
 
     plt.xticks(ticks=range(21), labels=[str(el) for el in sequence], rotation=90)
