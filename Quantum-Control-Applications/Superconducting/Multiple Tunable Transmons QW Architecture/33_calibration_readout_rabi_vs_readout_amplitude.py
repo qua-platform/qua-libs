e0a2e8d967b339eb56ef690e0b2bccde8b4f7a22
Kevin Villegas <kevin@quantum-machines.co>
2022-12-08 15:06:03 -0500

    33_cal checked

--- Quantum-Control-Applications/Superconducting/Multiple Tunable Transmons QW Architecture/33_calibration_readout_rabi_vs_readout_amplitude.py
+++ Quantum-Control-Applications/Superconducting/Multiple Tunable Transmons QW Architecture/33_calibration_readout_rabi_vs_readout_amplitude.py
@@ -11,6 +11,7 @@ from qualang_tools.plot import interrupt_on_close, plot_demodulated_data_2d
 from qualang_tools.results import progress_counter, fetching_tool
 from qualang_tools.loops import from_array
 from macros import *
+from config import NUMBER_OF_QUBITS_W_CHARGE
 
 ##################
 # State and QuAM #
@@ -18,20 +19,22 @@ from macros import *
 experiment = "Rabi_vs_readout_amplitude"
 debug = True
 simulate = False
-fit_data = True
-qubit_list = [1, 0]
-digital = []
+qubit_w_charge_list = [0, 1]
+qubit_wo_charge_list = [2, 3, 4, 5]
+qubit_list = [0, 5]  # you can shuffle the order at which you perform the experiment
+injector_list = [0, 1]
+digital = [1, 9]
 machine = QuAM("latest_quam.json")
 gate_shape = "drag_cosine"
 
 gate_length = []
-for q in qubit_list:
+for i, q in enumerate(qubit_list):
     gate_length.append(machine.get_qubit_gate(q, gate_shape).length)
     machine.get_qubit_gate(q, gate_shape).length = 16e-9
 
-config = machine.build_config(digital, qubit_list, gate_shape)
-for q in qubit_list:
-    machine.get_qubit_gate(q, gate_shape).length = gate_length[q]
+config = machine.build_config(digital, qubit_w_charge_list, qubit_wo_charge_list, injector_list, gate_shape)
+for i, q in enumerate(qubit_list):
+    machine.get_qubit_gate(q, gate_shape).length = gate_length[i]
 
 ###################
 # The QUA program #
@@ -58,25 +61,30 @@ with program() as readout_opt:
     t = declare(int)
 
     for i, q in enumerate(qubit_list):
-        # bring other qubits to zero frequency
-        machine.nullify_other_qubits(qubit_list, q)
-        set_dc_offset(
-            machine.qubits[q].name + "_flux", "single", machine.get_flux_bias_point(q, "zero_frequency_point").value
-        )
+        if q in qubit_w_charge_list:
+            set_dc_offset(
+                machine.qubits[q].name + "_charge", "single", machine.get_charge_bias_point(q, "working_point").value
+            )
 
         with for_(n[i], 0, n[i] < n_avg, n[i] + 1):
             with for_(*from_array(t, lengths)):
                 with for_(*from_array(a, amps)):
-                    play("x180", machine.qubits[q].name, duration=t)
+                    if q in qubit_w_charge_list:
+                        play("x180", machine.qubits[q].name, duration=t)
+                    else:
+                        play("x180", machine.qubits_wo_charge[q - NUMBER_OF_QUBITS_W_CHARGE].name, duration=t)
                     align()
                     measure(
                         "readout" * amp(a),
                         machine.readout_resonators[q].name,
                         None,
-                        dual_demod.full("cos", "out1", "sin", "out2", I[i]),
-                        dual_demod.full("minus_sin", "out1", "cos", "out2", Q[i]),
+                        demod.full("cos", I[i], "out1"),
+                        demod.full("sin", Q[i], "out1"),
                     )
-                    wait_cooldown_time(5 * machine.qubits[q].t1, simulate)
+                    if q in qubit_w_charge_list:
+                        wait_cooldown_time(5 * machine.qubits[q].t1, simulate)
+                    else:
+                        wait_cooldown_time(5 * machine.qubits_wo_charge[q - NUMBER_OF_QUBITS_W_CHARGE].t1, simulate)
                     save(I[i], I_st[i])
                     save(Q[i], Q_st[i])
             save(n[i], n_st[i])
