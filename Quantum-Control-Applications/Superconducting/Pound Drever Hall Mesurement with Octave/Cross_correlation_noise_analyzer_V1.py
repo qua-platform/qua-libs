ccb16be95de482795c713fb37de84fe1b66623df
yuvalqm <yuval@quantum-machines.co>
2023-04-18 19:15:18 +0300

    Added the PDH initial files

---
+++ Quantum-Control-Applications/Superconducting/Pound Drever Hall Mesurement with Octave/Cross_correlation_noise_analyzer_V1.py
@@ -0,0 +1,269 @@
+from qm.QuantumMachinesManager import QuantumMachinesManager
+from qm.qua import *
+import numpy as np
+from PDH_config_octave import *
+import matplotlib.pyplot as plt
+from qm import SimulationConfig
+from qm.octave import *
+from scipy.fft import fft, fftfreq
+import os
+import csv
+
+# opx_ip = '172.16.2.123'
+# opx_port = 80
+
+
+octave_config = QmOctaveConfig()
+octave_config.add_device_info('octave1', octave_ip, octave_port)
+octave_config.set_opx_octave_mapping([("con1", "octave1")])
+# octave_config.add_opx_connections(portmap)
+octave_config.set_calibration_db(os.getcwd())
+# cdb = calibration_db.CalibrationDB(os.getcwd())
+qmm = QuantumMachinesManager(host=opx_ip, port=opx_port, octave=octave_config)
+
+# qmm.octave_manager.set_clock("octave1", ClockType.External, ClockFrequency.MHZ_10)
+qmm.octave_manager.set_clock("octave1", ClockType.External, ClockFrequency.MHZ_1000)
+
+qm = qmm.open_qm(config)
+
+element = "RR"
+qm.octave.set_lo_frequency(element, rr_LO)
+qm.octave.set_lo_source(element, OctaveLOSource.Internal)
+qm.octave.set_rf_output_gain(element, -4)
+qm.octave.set_rf_output_mode(element, RFOutputMode.on)
+
+qm.octave.set_qua_element_octave_rf_in_port(element, "octave1", 2)
+qm.octave.set_downconversion(element, lo_source=RFInputLOSource.Dmd2LO)
+
+qm.octave._set_downconversion_if_mode(element, if_mode_i=IFMode.envelope, if_mode_q=IFMode.envelope)
+
+qm.octave.calibrate_element(element, [(rr_LO, rr_IF)])
+
+qm = qmm.open_qm(config)
+
+
+pound_samples_per_chunk = 1*int(2*250e6/pound_modulation_freq) #in clocks = 100ns to catch 2 periods of the PDH signal
+pound_vector_size = int(pound_pulse_length/4/pound_samples_per_chunk)
+correlation_length = 4096
+averaging_length = 8
+fft_averaging_length = 10
+with program() as noise_analyzer:
+    adc_st = declare_stream(adc_trace=True)
+    adc_st2 = declare_stream(adc_trace=True)
+
+    pound_signalI = declare(fixed)
+    pound_signalQ = declare(fixed)
+    dt = declare(int)
+    pound_vector_I1 = declare(fixed, value=np.zeros(pound_vector_size))
+    pound_vector_Q1 = declare(fixed, value=np.zeros(pound_vector_size))
+    pound_vector_I2 = declare(fixed, value=np.zeros(pound_vector_size))
+    pound_vector_Q2 = declare(fixed, value=np.zeros(pound_vector_size))
+
+    pound_vector_I1_st = declare_stream()
+    pound_vector_Q1_st = declare_stream()
+    pound_vector_I2_st = declare_stream()
+    pound_vector_Q2_st = declare_stream()
+
+
+    pound_index = declare(int)
+    index = declare(int)
+    #MAC_I = declare(fixed, value=0.0)
+    #MAC_Q = declare(fixed, value=0.0)
+    j = declare(int)
+    j2 = declare(int)
+    avg = declare(int)
+    fft_avg = declare(int)
+
+    corr_I = declare(fixed, value=np.zeros(correlation_length))
+    corr_Q = declare(fixed, value=np.zeros(correlation_length))
+    corr_I_st = declare_stream()
+    corr_Q_st = declare_stream()
+
+    align()
+
+    play('pound_pulse', 'RR')
+    align()
+    with for_(pound_index, 0, pound_index<5*averaging_length*correlation_length*fft_averaging_length, pound_index+1):
+        play('pound_pulse', 'RR')
+        #frame_rotation_2pi(0.5, 'Pound_simulator')
+
+
+
+    #measure('pound_demod_pulse', 'Pound_demod', adc_st, demod.sliced('integ_pound',pound_vector_I1, pound_samples_per_chunk, 'out1'), demod.sliced('integ_pound',pound_vector_Q1, pound_samples_per_chunk, 'out2'))
+    #measure('pound_demod_pulse', 'Pound_demod_2', adc_st2, demod.sliced('integ_pound',pound_vector_I2, pound_samples_per_chunk, 'out1'), demod.sliced('integ_pound',pound_vector_Q2, pound_samples_per_chunk, 'out2'))
+    #with for_(dt, pound_samples_per_chunk, dt<pound_samples_per_chunk*1000, dt+pound_samples_per_chunk):
+    #align('Pound_demod', 'Pound_demod_2')
+    #measure('pound_demod_pulse', 'Pound_demod', adc_st, demod.full('integ_pound', pound_signalI, 'out1'), demod.sliced('integ_pound',pound_vector_I, pound_samples_per_chunk, 'out1'), demod.full('integ_pound', pound_signalQ, 'out2'))
+
+    measure('pound_demod_pulse', 'Pound_demod', adc_st,
+            demod.sliced('integ_pound', pound_vector_I1, pound_samples_per_chunk, 'out1'),
+            demod.sliced('integ_pound', pound_vector_Q1, pound_samples_per_chunk, 'out2'))
+
+    measure('pound_demod_pulse', 'Pound_demod_2', adc_st2,
+            demod.sliced('integ_pound', pound_vector_I2, pound_samples_per_chunk, 'out1'),
+            demod.sliced('integ_pound', pound_vector_Q2, pound_samples_per_chunk, 'out2'))
+
+    with for_(fft_avg, 0, fft_avg < fft_averaging_length, fft_avg+1):
+        with for_(avg, 0, avg < averaging_length, avg+1):
+            with for_(j, 0, j < correlation_length, j+1):
+                measure('pound_demod_pulse', 'Pound_demod', None,
+                    demod.sliced('integ_pound', pound_vector_I1, pound_samples_per_chunk, 'out1'),
+                    demod.sliced('integ_pound', pound_vector_Q1, pound_samples_per_chunk, 'out2'))
+                wait(pound_samples_per_chunk*(j), 'Pound_demod_2')
+                measure('pound_demod_pulse', 'Pound_demod_2', None,
+                    demod.sliced('integ_pound', pound_vector_I2, pound_samples_per_chunk, 'out1'),
+                    demod.sliced('integ_pound', pound_vector_Q2, pound_samples_per_chunk, 'out2'))
+
+                assign(corr_I[j], corr_I[j]+Math.dot(pound_vector_I1,pound_vector_I2))
+                assign(corr_Q[j], corr_Q[j]+Math.dot(pound_vector_Q1, pound_vector_Q2))
+
+
+        with for_(j2, 0, j2 < correlation_length, j2 + 1):
+            assign(corr_I[j2], corr_I[j2]>>int(np.log2(averaging_length)))
+            assign(corr_Q[j2], corr_Q[j2]>>int(np.log2(averaging_length)))
+            save(corr_I[j2], corr_I_st)
+            save(corr_Q[j2], corr_Q_st)
+            assign(corr_I[j2], 0.0)
+            assign(corr_Q[j2], 0.0)
+
+    with for_(index, 0, index < pound_vector_size, index + 1):
+        save(pound_vector_I1[index], pound_vector_I1_st)
+        save(pound_vector_Q1[index], pound_vector_Q1_st)
+        save(pound_vector_I2[index], pound_vector_I2_st)
+        save(pound_vector_Q2[index], pound_vector_Q2_st)
+
+    with stream_processing():
+        adc_st.input1().save('adcI1') # see the raw data
+        adc_st.input2().save('adcQ1')
+        adc_st2.input1().save('adcI2') # see the raw data
+        adc_st2.input2().save('adcQ2')
+        pound_vector_I1_st.buffer(pound_vector_size).save_all('pound_vector_I1')
+        pound_vector_Q1_st.buffer(pound_vector_size).save_all('pound_vector_Q1')
+        pound_vector_I2_st.buffer(pound_vector_size).save_all('pound_vector_I2')
+        pound_vector_Q2_st.buffer(pound_vector_size).save_all('pound_vector_Q2')
+        corr_I_st.buffer(correlation_length).save_all('corr_I')
+        corr_Q_st.buffer(correlation_length).save_all('corr_Q')
+        corr_I_st.buffer(correlation_length).fft('abs').average().save('PSD_I')
+
+
+job = qm.execute(noise_analyzer)
+job.result_handles.wait_for_all_values()
+res = job.result_handles
+adcI1 = res.get("adcI1").fetch_all()
+adcQ1 = res.get("adcQ1").fetch_all()
+adcI2 = res.get("adcI2").fetch_all()
+adcQ2 = res.get("adcQ2").fetch_all()
+pound_vector_I1 = res.get('pound_vector_I1').fetch_all()[0][0]
+pound_vector_Q1 = res.get('pound_vector_Q1').fetch_all()[0][0]
+pound_vector_I2 = res.get('pound_vector_I2').fetch_all()[0][0]
+pound_vector_Q2 = res.get('pound_vector_Q2').fetch_all()[0][0]
+corr_I = res.get('corr_I').fetch_all()['value'][0]
+corr_Q = res.get('corr_Q').fetch_all()['value'][0]
+PSD=res.get('PSD_I').fetch_all()
+
+# Number of sample points
+N = corr_I.__len__()
+# sample spacing
+T = pound_samples_per_chunk*4e-9
+xI = np.linspace(0.0, N*T, N, endpoint=False)
+yI = corr_I
+yfI = fft(yI)
+xfI = fftfreq(N, T)[:N//2]
+
+xQ = np.linspace(0.0, N*T, N, endpoint=False)
+yQ = corr_Q
+yfQ = fft(yQ) #PSD
+xfQ = fftfreq(N, T)[:N//2]
+
+plt.figure(figsize=(16,12))
+plt.loglog(xfI, 2.0/N * np.abs(yfI[0:N//2]),linewidth=5, markersize=10)
+plt.loglog(xfQ, 2.0/N * np.abs(yfQ[0:N//2]),linewidth=5, markersize=10)
+plt.title('FFT of autocorrelation', fontsize=30)
+plt.xlabel('Freq (Hz)', fontsize=30)
+plt.ylabel('FFT Amplitude |X(freq)|', fontsize=30)
+plt.xticks(fontsize= 20)
+plt.yticks(fontsize= 20)
+plt.grid()
+
+plt.figure(figsize=(16,12))
+plt.loglog(xfI,2.0/N * np.abs(PSD[0:N//2]),linewidth=5, markersize=10)
+plt.title('FFT from stream processing', fontsize=30)
+plt.xlabel('Freq (Hz)', fontsize=30)
+plt.ylabel('FFT Amplitude |X(freq)|', fontsize=30)
+plt.xticks(fontsize= 20)
+plt.yticks(fontsize= 20)
+plt.grid()
+
+# plt.subplot(122)
+# plt.plot(t, ifft(X), 'r')
+# plt.xlabel('Time (s)')
+# plt.ylabel('Amplitude')
+# plt.tight_layout()
+# plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
+# job = qmm.simulate(config, noise_analyzer, SimulationConfig(2000))
+# samps = job.get_simulated_samples()
+# pound_simulated_signal9 = samps.con1.analog['9']
+# pound_simulated_signal10 = samps.con1.analog['10']
+#
+#
+# plt.figure(figsize=(16,12))
+# plt.plot(pound_vector_I1,linewidth=5, markersize=10)
+# plt.plot(pound_vector_Q1,linewidth=5, markersize=10)
+# plt.plot(pound_vector_I2,linewidth=5, markersize=10)
+# plt.plot(pound_vector_Q2,linewidth=5, markersize=10)
+# plt.title('pound demod vectors', fontsize=30)
+# plt.xlabel('Shot no.', fontsize=30)
+# plt.ylabel('I/Q values', fontsize=30)
+# plt.xticks(fontsize= 20)
+# plt.yticks(fontsize= 20)
+# plt.grid()
+#
+#
+#
+# plt.figure(figsize=(16,12))
+# plt.plot([(x+1)*pound_samples_per_chunk*4 for x in range(corr_I.__len__())], corr_I,linewidth=5, markersize=10)
+# plt.plot([(x+1)*pound_samples_per_chunk*4 for x in range(corr_Q.__len__())], corr_Q,linewidth=5, markersize=10)
+# plt.title('autocorrelation I/Q', fontsize=30)
+# plt.xlabel('dt', fontsize=30)
+# plt.ylabel('Raw ADC data', fontsize=30)
+# plt.xticks(fontsize= 20)
+# plt.yticks(fontsize= 20)
+# plt.grid()
+#
+# plt.figure(figsize=(16,12))
+# plt.plot(adcI1,linewidth=5, markersize=10)
+# plt.plot(adcQ1,linewidth=5, markersize=10)
+# plt.plot(adcI2,linewidth=5, markersize=10)
+# plt.plot(adcQ2,linewidth=5, markersize=10)
+# plt.title('Raw ADC data', fontsize=30)
+# plt.xlabel('Shot no.', fontsize=30)
+# plt.ylabel('Raw ADC data', fontsize=30)
+# plt.xticks(fontsize= 20)
+# plt.yticks(fontsize= 20)
+# plt.grid()
+#
+# plt.figure(figsize=(16,12))
+# plt.plot(pound_simulated_signal9,linewidth=5, markersize=10)
+# plt.plot(pound_simulated_signal10,linewidth=5, markersize=10)
+# plt.title('Simulation output', fontsize=30)
+# plt.xlabel('Shot no.', fontsize=30)
+# plt.ylabel('Volt', fontsize=30)
+# plt.xticks(fontsize= 20)
+# plt.yticks(fontsize= 20)
+# plt.grid()
---
+++ Quantum-Control-Applications/Superconducting/Pound Drever Hall Mesurement with Octave/PDH_PIDv3_octave.py
@@ -0,0 +1,349 @@
+from qm.QuantumMachinesManager import QuantumMachinesManager
+from qm.qua import *
+import numpy as np
+from PDH_config_octave import *
+import matplotlib.pyplot as plt
+from qm import SimulationConfig
+from qm.octave import *
+import os
+import csv
+
+#
+# opx_ip = '172.16.2.107'
+# opx_port = 80
+# octave_ip = '172.16.2.107'
+# octave_port = 50
+
+
+octave_config = QmOctaveConfig()
+octave_config.add_device_info('octave1', octave_ip, octave_port)
+octave_config.set_opx_octave_mapping([("con1", "octave1")])
+# octave_config.add_opx_connections(portmap)
+octave_config.set_calibration_db(os.getcwd())
+# cdb = calibration_db.CalibrationDB(os.getcwd())
+qmm = QuantumMachinesManager(host=opx_ip, port=opx_port, octave=octave_config)
+
+# qmm.octave_manager.set_clock("octave1", ClockType.External, ClockFrequency.MHZ_10)
+qmm.octave_manager.set_clock("octave1", ClockType.External, ClockFrequency.MHZ_1000)
+
+qm = qmm.open_qm(config)
+
+element = "RR"
+qm.octave.set_lo_frequency(element, rr_LO)
+qm.octave.set_lo_source(element, OctaveLOSource.Internal)
+qm.octave.set_rf_output_gain(element, -4)
+qm.octave.set_rf_output_mode(element, RFOutputMode.on)
+
+qm.octave.set_qua_element_octave_rf_in_port(element, "octave1", 2)
+qm.octave.set_downconversion(element, lo_source=RFInputLOSource.Dmd2LO)
+
+qm.octave._set_downconversion_if_mode(element, if_mode_i=IFMode.envelope, if_mode_q=IFMode.envelope)
+
+qm.octave.calibrate_element(element, [(rr_LO, rr_IF)])
+
+qm = qmm.open_qm(config)
+
+# PDH spectroscopy code
+# Output channels are 1 and 2 to go to the IQ mixer, all PDH parameters are in the config file
+# It would be best to calibrate the mixer first to have clean single sideband upconversion and not have
+# to worry about mixing in with some LO leakage.
+# Also make sure of course that the carrier IF frequency is larger than the sideband
+
+
+f_init = int(32e6)
+f_final = int(195e6)
+df = int(10e3)
+freq_array_size = int((f_final - f_init) / df)
+N_averaging = 1
+
+gain_P = 0.1
+gain_I = 0.1
+gain_D = 0.0
+alpha = 0.1
+PID_length = int(1e5)  # how many shots to run the PID loop
+averaging_length_for_plotting = int(1)  # averaging every N shots to not transfer the whole vector of size PID_length
+initial_frequency = int(-41.737e6)
+error_scaling = -1e6
+
+with program() as PDH_PID:
+    adc_st = declare_stream(adc_trace=True)
+    pound_signal = declare(fixed)
+    pound_signal_st = declare_stream()
+    f = declare(int, value=initial_frequency)  # initial resonator frequency (LO=9.8 GHz)
+    scan_freq = declare(int)
+    f_st = declare_stream()
+    n = declare(int)
+    ind = declare(int)
+    error = declare(int)
+
+    error_st = declare_stream()
+    old_error = declare(int, value=0)
+    integrator_error = declare(int, value=0)
+    derivative_error = declare(int)
+
+    # get raw ADC data first
+    update_frequency('RR', f)
+    reset_phase('RR')
+    reset_phase('Pound_demod')
+    play('pound_pulse', 'RR')
+    play('pound_pulse', 'RR')
+    align()
+    play('pound_pulse', 'RR')
+    measure('pound_demod_pulse', 'Pound_demod', adc_st, demod.full('integ_pound', pound_signal, 'out1'))
+    play('pound_pulse', 'RR')
+
+    # outside loop is for averaging many spectra
+    with for_(n, 0, n < N_averaging, n + 1):
+        # loop over the frequency
+        # with for_(f, f_init, f<f_final, f+df ):
+
+        with for_(ind, 0, ind < PID_length, ind + 1):
+            update_frequency('RR', f)
+            save(f, f_st)
+            reset_phase('RR')
+            reset_phase('Pound_demod')
+            play('pound_pulse', 'RR')
+            play('pound_pulse', 'RR')
+            align()
+            play('pound_pulse', 'RR')
+            measure('pound_demod_pulse', 'Pound_demod', None, demod.full('integ_pound', pound_signal, 'out1'))
+            play('pound_pulse', 'RR')
+            save(pound_signal, pound_signal_st)
+            assign(error, Cast.mul_int_by_fixed(error_scaling, pound_signal))
+
+            assign(integrator_error, Cast.mul_int_by_fixed(integrator_error, 1 - alpha) +
+                   Cast.mul_int_by_fixed(error, alpha))
+            assign(derivative_error, old_error - error)
+            assign(f, f + Cast.mul_int_by_fixed(error, gain_P) +
+                   Cast.mul_int_by_fixed(integrator_error, gain_I) +
+                   Cast.mul_int_by_fixed(derivative_error, gain_D))
+
+            assign(old_error, error)
+            save(error, 'error_test')
+            save(error, error_st)
+
+        ind2 = declare(int)
+        scan_steps = 100
+        scan_step_size = 100
+        averaging = 1024
+
+        error_calib = declare(int, value=np.zeros(scan_steps, dtype=int).tolist())
+        scan_freq_list = declare(int, value=np.zeros(scan_steps, dtype=int).tolist())
+        with for_(ind2, 0, ind2 < averaging, ind2 + 1):
+
+            with for_(ind, 0, ind < scan_steps, ind + 1):
+                assign(scan_freq, f - scan_step_size * scan_steps / 2 + scan_step_size * ind)
+                update_frequency('RR', scan_freq)
+                assign(scan_freq_list[ind], scan_freq)
+                #assign(error, 0)
+
+                reset_phase('RR')
+                reset_phase('Pound_demod')
+                play('pound_pulse', 'RR')
+                align()
+                play('pound_pulse', 'RR')
+                measure('pound_demod_pulse', 'Pound_demod', None, demod.full('integ_pound', pound_signal, 'out1'))
+                play('pound_pulse', 'RR')
+                assign(error_calib[ind], error_calib[ind] + Cast.mul_int_by_fixed(error_scaling, pound_signal))
+
+                #assign(error, error >> int(np.log2(averaging)))
+                #save(error, 'scan_PDH_error')
+        with for_(ind, 0, ind < scan_steps, ind+1):
+            assign(error_calib[ind], error_calib[ind] >> int(np.log2(averaging)))
+            save(error_calib[ind], 'scan_PDH_error')
+            save(scan_freq_list[ind], 'scan_freq')
+        # with for_(ind, 0, ind < scan_steps, ind + 1):
+        #     assign(scan_freq, f - scan_step_size * scan_steps / 2 + scan_step_size * ind)
+        #     update_frequency('RR', scan_freq)
+        #     save(scan_freq, 'scan_freq')
+        #     assign(error, 0)
+        #
+        #     with for_(ind2, 0, ind2 < averaging, ind2 + 1):
+        #         reset_phase('RR')
+        #         reset_phase('Pound_demod')
+        #         play('pound_pulse', 'RR')
+        #         align()
+        #         play('pound_pulse', 'RR')
+        #         measure('pound_demod_pulse', 'Pound_demod', None, demod.full('integ_pound', pound_signal, 'out1'))
+        #         play('pound_pulse', 'RR')
+        #         assign(error, error + Cast.mul_int_by_fixed(error_scaling, pound_signal))
+        #
+        #     assign(error, error >> int(np.log2(averaging)))
+        #     save(error, 'scan_PDH_error')
+
+
+            # # one last time to get raw ADC data
+            # update_frequency('RR', f+1000000)
+            # reset_phase('RR')
+            # reset_phase('Pound_demod')
+            # play('pound_pulse', 'RR')
+            # align()
+            # play('pound_pulse', 'RR')
+            # measure('pound_demod_pulse', 'Pound_demod', adc_st, demod.full('integ_pound', pound_signal, 'out1'))
+            # play('pound_pulse', 'RR')
+
+    # stream processing handling
+    with stream_processing():
+        adc_st.input1().save('adc1')  # see the raw data
+        # pound_signal_st.buffer(100).average().save('pound_signal') # buffer it into vectors the length of the frequency scan and average over the averaging loop
+        error_st.buffer(averaging_length_for_plotting).average().save_all('error')
+        pound_signal_st.buffer(averaging_length_for_plotting).average().save(
+            'pound_signal')  # buffer it into vectors the length of the frequency scan and average over the averaging loop
+        f_st.buffer(averaging_length_for_plotting).average().save_all('f')
+
+job = qm.execute(PDH_PID)
+job.result_handles.wait_for_all_values()
+res = job.result_handles
+res.wait_for_all_values()
+adc1 = res.get("adc1").fetch_all()
+pound_signal = res.get('pound_signal').fetch_all()
+error = res.get('error').fetch_all()
+scan_PDH_error = res.get('scan_PDH_error').fetch_all()['value'].tolist()
+scan_freq = res.get('scan_freq').fetch_all()['value'].tolist()
+f = res.get('f').fetch_all()
+freq = [x / 1e6 for x in np.linspace(f_init, f_final, int((f_final - f_init) / df))]
+
+error_flat = [er[0] for er in error['value']]
+f_flat = [ff[0] for ff in f['value']]
+mean_f = np.mean(f_flat)
+resonator_freq = rr_LO + mean_f
+frac_f = [(freq - mean_f) / (resonator_freq) for freq in f_flat]
+
+from scipy.optimize import curve_fit
+
+
+def func(x, a, b):
+    return [a * xind + b for xind in x]
+
+
+popt, pcov = curve_fit(func, scan_freq, scan_PDH_error)
+error_to_Hz = popt[0]
+
+error_Hz = [(er * error_to_Hz) for er in error_flat]
+error_frac = [(er * error_to_Hz) / resonator_freq for er in error_flat]
+
+with open('error_frac_freq_withlock2.csv', 'w', newline='') as file_error_freq:
+    writer = csv.writer(file_error_freq)
+    for index in range(error_frac.__len__()):
+        writer.writerow([error_frac[index]])
+
+plt.figure(figsize=(16, 12))
+plt.plot(error_Hz, linewidth=5, markersize=10)
+plt.title('PDH error signal in Hz', fontsize=30)
+plt.xlabel('Shot no.', fontsize=30)
+plt.ylabel('PDH Error signal [Hz]', fontsize=30)
+plt.xticks(fontsize=20)
+plt.yticks(fontsize=20)
+plt.grid()
+
+plt.figure(figsize=(16, 12))
+plt.plot(scan_freq, scan_PDH_error, linewidth=5, markersize=10)
+plt.title('PDH/freq slope fit: a=%5.3f, b=%5.3f' % tuple(popt), fontsize=30)
+plt.xlabel('Resonator frequency', fontsize=30)
+plt.ylabel('PDH error signal', fontsize=30)
+plt.xticks(fontsize=20)
+plt.yticks(fontsize=20)
+plt.grid()
+plt.plot(scan_freq, func(scan_freq, popt[0], popt[1]), 'r-', label='fit: a=%5.3f, b=%5.3f' % tuple(popt))
+
+plt.figure(figsize=(16, 12))
+plt.plot(error, linewidth=5, markersize=10)
+plt.title('PDH error signal', fontsize=30)
+plt.xlabel('Shot no.', fontsize=30)
+plt.ylabel('PDH Error signal', fontsize=30)
+plt.xticks(fontsize=20)
+plt.yticks(fontsize=20)
+plt.grid()
+
+plt.figure(figsize=(16, 12))
+plt.plot(f, linewidth=5, markersize=10)
+plt.title('Resonator frequency', fontsize=30)
+plt.xlabel('Shot no.', fontsize=30)
+plt.ylabel('Resonator_frequency', fontsize=30)
+plt.xticks(fontsize=20)
+plt.yticks(fontsize=20)
+plt.grid()
+
+# plt.figure(figsize=(16,12))
+# plt.plot(pound_signal,linewidth=5, markersize=10)
+# plt.title('PDH on resonance', fontsize=30)
+# plt.xlabel('Shot no.', fontsize=30)
+# plt.ylabel('PDH signal', fontsize=30)
+# plt.xticks(fontsize= 20)
+# plt.yticks(fontsize= 20)
+# plt.grid()
+
+# j=0
+# error_flat = np.zeros(100000*1000)
+# error_flat = error_flat.tolist()
+# for index1 in range(100000):
+#     for index2 in range(1000):
+#         error_flat[j] = error[index1][0][index2]
+#         j = j+1
+
+
+#
+plt.figure(figsize=(16, 12))
+plt.plot(adc1)
+plt.title('Raw acquired data (for the first shot)', fontsize=30)
+plt.xlabel('Time [ns]', fontsize=30)
+plt.ylabel('Raw ADC sample', fontsize=30)
+plt.xticks(fontsize=20)
+plt.yticks(fontsize=20)
+plt.grid()
+
+# sampling rate
+sr = 1e9
+# sampling interval
+ts = 1.0/sr
+t = np.arange(0,1,ts)
+
+X = np.abs(np.fft.fft(adc1))
+N = len(adc1)
+n = np.arange(N)
+T = N/sr
+#freq = n/T
+
+plt.figure(figsize=(16, 12))
+freq = np.fft.fftfreq(len(adc1), d=1)
+#fft = np.abs(np.fft.fft(adc1))
+plt.plot(freq * 1e3, X, lw=5)
+plt.title('FFT of raw acquired data (for the first shot)', fontsize=30)
+plt.xlabel('Frequency [MHz]', fontsize=30)
+plt.ylabel('FT of signal [V.s]', fontsize=30)
+plt.yscale("log")
+plt.grid()
+
+# simulate the signal to see that everything is phase coherent etc.. simulate for 20000 clock cycles or 80us
+# job = qmm.simulate(config, PDH_PID, SimulationConfig(20000))
+# samps = job.get_simulated_samples()
+# RR_I = samps.con1.analog['1']
+# RR_Q = samps.con1.analog['2']
+#
+# plt.figure(figsize=(16,12))
+# plt.plot(RR_I)
+# plt.title('Simulated PDH output signal', fontsize=30)
+# plt.xlabel('Time [ns]', fontsize=30)
+# plt.ylabel('Raw DAC signal', fontsize=30)
+# plt.xticks(fontsize= 20)
+# plt.yticks(fontsize= 20)
+# plt.grid()
+# you can also plot the other quadrature if you want:
+# plt.figure(figsize=(10,8))
+# plt.plot(RR_Q)
+
+#
+# with for_(ind, 0, ind < PID_length, ind + 1):
+#     update_frequency('RR', f)
+#     reset_phase('RR')
+#     reset_phase('Pound_demod')
+#     play('pound_pulse', 'RR')
+#     measure('pound_demod_pulse', 'Pound_demod', None, demod.full('integ_pound', pound_signal, 'out1'))
+#     save(pound_signal, pound_signal_st)
+#     assign(error, Cast.mul_int_by_fixed(-1e8, pound_signal))
+#     assign(integrator_error, Cast.mul_int_by_fixed(integrator_error, 1 - alpha) +
+#            Cast.mul_int_by_fixed(error, alpha))
+#     assign(derivative_error, old_error - error)
+#     assign(f, f + Cast.mul_int_by_fixed(error, gain_P) +
+#            Cast.mul_int_by_fixed(integrator_error, gain_I) +
+#            Cast.mul_int_by_fixed(derivative_error, gain_D))
