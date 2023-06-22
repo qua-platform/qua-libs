ec978da1d8ff13f75455ba0047a123f7dc277047
TheoQM <theo@quantum-machines.co>
2023-06-22 13:29:39 +0200

    2 qb gates

--- Quantum-Control-Applications/Superconducting/Multiple Flux Tunable Transmons/19_iSWAP.py
+++ Quantum-Control-Applications/Superconducting/Multiple Flux Tunable Transmons/19_iSWAP.py
@@ -2,8 +2,6 @@ from qm.QuantumMachinesManager import QuantumMachinesManager
 from qm.qua import *
 from qm import SimulationConfig
 from configuration import *
-import matplotlib
-matplotlib.use('TKAgg')
 import matplotlib.pyplot as plt
 from qualang_tools.loops import from_array
 from qualang_tools.results import fetching_tool
@@ -40,7 +38,7 @@ with program() as iswap:
                 set_dc_offset("q1_z", "single", dc0_q1)
                 wait(10)
                 align()
-                multiplexed_readout(I, I_st, Q, Q_st, weights="rotated_")
+                multiplexed_readout(I, I_st, Q, Q_st, resonators=[1, 2], weights="rotated_")
                 wait(cooldown_time * u.ns)
 
     with stream_processing():
@@ -58,37 +56,40 @@ with program() as iswap:
 #####################################
 qmm = QuantumMachinesManager(host=qop_ip, port=qop_port)
 
-simulate = True
+simulate = False
 if simulate:
     job = qmm.simulate(config, iswap, SimulationConfig(11000))
     job.get_simulated_samples().con1.plot()
 else:
     qm = qmm.open_qm(config)
     job = qm.execute(iswap)
-    fig, ax = plt.subplots(2,2)
+    fig = plt.figure()
     interrupt_on_close(fig, job)
     results = fetching_tool(job, ["n", "I1", "Q1", "I2", "Q2"], mode="live")
-    while job.result_handles.is_processing():
+    while results.is_processing():
         n, I1, Q1, I2, Q2 = results.fetch_all()
         progress_counter(n, n_avg, start_time=results.start_time)
 
-        ax[0, 0].cla()
-        ax[0, 0].pcolor(amps, 4*ts, I1)
-        ax[0, 0].set_title('q1 - I')
-        ax[0, 0].set_ylabel("Interaction time (ns)")
-        ax[1, 0].cla()
-        ax[1, 0].pcolor(amps, 4*ts, Q1)
-        ax[1, 0].set_title('q1 - Q')
-        ax[1, 0].set_xlabel("FLux amplitude (V)")
-        ax[1, 0].set_ylabel("Interaction time (ns)")
-        ax[0, 1].cla()
-        ax[0, 1].pcolor(amps, 4*ts, I2)
-        ax[0, 1].set_title('q2 - I')
-        ax[1, 1].cla()
-        ax[1, 1].pcolor(amps, 4*ts, Q2)
-        ax[1, 1].set_title('q2 - Q')
-        ax[1, 1].set_xlabel("FLux amplitude (V)")
+        plt.subplot(221)
+        plt.cla()
+        plt.pcolor(amps, 4*ts, I1)
+        plt.title('q1 - I')
+        plt.ylabel("Interaction time (ns)")
+        plt.subplot(223)
+        plt.cla()
+        plt.pcolor(amps, 4*ts, Q1)
+        plt.title('q1 - Q')
+        plt.xlabel("FLux amplitude (V)")
+        plt.ylabel("Interaction time (ns)")
+        plt.subplot(222)
+        plt.cla()
+        plt.pcolor(amps, 4*ts, I2)
+        plt.title('q2 - I')
+        plt.subplot(224)
+        plt.cla()
+        plt.pcolor(amps, 4*ts, Q2)
+        plt.title('q2 - Q')
+        plt.xlabel("FLux amplitude (V)")
         plt.tight_layout()
         plt.pause(0.1)
     # np.savez(save_dir / 'iswap', I1=I1, Q1=Q1, I2=I2, ts=ts, amps=amps)
-plt.show()
--- Quantum-Control-Applications/Superconducting/Multiple Flux Tunable Transmons/19_iSWAP_pulsed.py
+++ Quantum-Control-Applications/Superconducting/Multiple Flux Tunable Transmons/19_iSWAP_pulsed.py
@@ -1,11 +1,7 @@
-# %%
 from qm.QuantumMachinesManager import QuantumMachinesManager
 from qm.qua import *
 from qm import SimulationConfig
 from configuration import *
-import matplotlib
-
-# matplotlib.use("TKAgg")
 import matplotlib.pyplot as plt
 from qualang_tools.loops import from_array
 from qualang_tools.results import fetching_tool
@@ -20,9 +16,8 @@ from qualang_tools.bakery import baking
 ##########
 
 # FLux pulse waveform generation
-flux_waveform = np.array(
-    [const_flux_amp] * const_flux_len
-)  # The variable const_flux_len is defined in the configuration
+# The variable const_flux_len is defined in the configuration
+flux_waveform = np.array([const_flux_amp] * const_flux_len)
 
 
 def baked_waveform(waveform, pulse_duration):
@@ -52,7 +47,7 @@ square_pulse_segments = baked_waveform(flux_waveform, const_flux_len)
 dc0_q2 = config["controllers"]["con1"]["analog_outputs"][8]["offset"]
 dc0_q1 = config["controllers"]["con1"]["analog_outputs"][7]["offset"]
 amps = np.arange(1.2, 1.8, 0.005)
-cooldown_time = 5 * 50 * u.us
+cooldown_time = 5 * u.us
 n_avg = 60
 
 with program() as iswap:
@@ -66,7 +61,7 @@ with program() as iswap:
             with for_(segment, 0, segment <= const_flux_len, segment + 1):
                 play("x180", "q1_xy")
                 align()
-                wait(4)  # TODO: to guarantee that flux and drive don't overlap, can be calibrated
+                wait(4)
 
                 with switch_(segment):
                     for j in range(0, const_flux_len + 1):
@@ -74,10 +69,7 @@ with program() as iswap:
                             square_pulse_segments[j].run(amp_array=[("q1_z", a)])
 
                 align()
-
-                wait(25)
-                align()
-                multiplexed_readout(I, I_st, Q, Q_st, weights="rotated_")
+                multiplexed_readout(I, I_st, Q, Q_st, resonators=[1, 2], weights="rotated_")
                 wait(cooldown_time * u.ns)
 
     with stream_processing():
@@ -93,16 +85,13 @@ with program() as iswap:
 #####################################
 #  Open Communication with the QOP  #
 #####################################
-qmm = QuantumMachinesManager(host=qop_ip, port=83)
+qmm = QuantumMachinesManager(host=qop_ip, port=qop_port)
 
 simulate = True
 if simulate:
     job = qmm.simulate(config, iswap, SimulationConfig(11000))
-    # job.get_simulated_samples().con1.plot()
-    from qualang_tools.plot import plot_simulator_output
+    job.get_simulated_samples().con1.plot()
 
-    fig = plot_simulator_output(job, config, duration_ns=11000, plot_axes=[["q1_xy"], ["q1_z"], ["rr1"], ["rr2"]])
-    fig.show()
 else:
     qm = qmm.open_qm(config)
     job = qm.execute(iswap)
@@ -110,7 +99,7 @@ else:
     interrupt_on_close(fig, job)
     results = fetching_tool(job, ["n", "I1", "Q1", "I2", "Q2"], mode="live")
     xplot = np.arange(0, const_flux_len + 0.1, 1)
-    while job.result_handles.is_processing():
+    while results.is_processing():
         n, I1, Q1, I2, Q2 = results.fetch_all()
         progress_counter(n, n_avg, start_time=results.start_time)
 
@@ -137,6 +126,3 @@ else:
         plt.tight_layout()
         plt.pause(0.1)
     # np.savez(save_dir / 'iswap', I1=I1, Q1=Q1, I2=I2, ts=ts, amps=amps)
-plt.show()
-
-# %%
