308bdcc466f86fb0ee003d1c1316307ff3fcb5c3
TheoQM <theo@quantum-machines.co>
2023-06-26 08:56:14 +0200

    Add interleaved RB

---
+++ Quantum-Control-Applications/Superconducting/Two Flux Tunable Transmons/Standard Configuration/18_single_qubit_RB_interleaved.py
@@ -0,0 +1,297 @@
+"""
+Performs a single qubit interleaved randomized benchmarking to measure a specific single qubit gate fidelity (works for gates longer than 40ns)
+"""
+from qm.qua import *
+from qm.QuantumMachinesManager import QuantumMachinesManager
+from qm import SimulationConfig
+from scipy.optimize import curve_fit
+from configuration import *
+import matplotlib.pyplot as plt
+import numpy as np
+from qualang_tools.bakery.randomized_benchmark_c1 import c1_table
+from qualang_tools.results import fetching_tool, progress_counter
+from qualang_tools.plot import interrupt_on_close
+from macros import multiplexed_readout
+
+
+#############################################
+# Program dependent variables and functions #
+#############################################
+qubit = 1
+
+if qubit == 1:
+    threshold = ge_threshold_q1
+elif qubit == 2:
+    threshold = ge_threshold_q2
+else:
+    threshold = 0
+state_discrimination = False
+
+inv_gates = [int(np.where(c1_table[i, :] == 0)[0][0]) for i in range(24)]
+# index from play_sequence() function defined below of the gate under study
+# Correspondence table:
+#  0: identity |  1: x180 |  2: y180
+# 12: x90      | 13: -x90 | 14: y90 | 15: -y90 |
+interleaved_gate_index = 2
+max_circuit_depth = 1000
+num_of_sequences = 5
+n_avg = 100
+seed = 345323
+cooldown_time = 5 * qubit_T1 // 4
+delta_clifford = 10  # Must be > 1
+assert (max_circuit_depth / delta_clifford).is_integer(), "max_circuit_depth / delta_clifford must be an integer."
+
+
+def power_law(power, a, b, p):
+    return a * (p**power) + b
+
+
+def generate_sequence(interleaved_gate_index):
+    cayley = declare(int, value=c1_table.flatten().tolist())
+    inv_list = declare(int, value=inv_gates)
+    current_state = declare(int)
+    step = declare(int)
+    sequence = declare(int, size=2 * max_circuit_depth + 1)
+    inv_gate = declare(int, size=2 * max_circuit_depth + 1)
+    i = declare(int)
+    rand = Random(seed=seed)
+
+    assign(current_state, 0)
+    with for_(i, 0, i < 2 * max_circuit_depth, i + 2):
+        assign(step, rand.rand_int(24))
+        assign(current_state, cayley[current_state * 24 + step])
+        assign(sequence[i], step)
+        assign(inv_gate[i], inv_list[current_state])
+        # interleaved gate
+        assign(step, interleaved_gate_index)
+        assign(current_state, cayley[current_state * 24 + step])
+        assign(sequence[i + 1], step)
+        assign(inv_gate[i + 1], inv_list[current_state])
+
+    return sequence, inv_gate
+
+
+def play_sequence(sequence_list, depth, qubit):
+    i = declare(int)
+    with for_(i, 0, i <= depth, i + 1):
+        with switch_(sequence_list[i], unsafe=True):
+            with case_(0):
+                wait(pi_len // 4, f"q{qubit}_xy")
+            with case_(1):
+                play("x180", f"q{qubit}_xy")
+            with case_(2):
+                play("y180", f"q{qubit}_xy")
+            with case_(3):
+                play("y180", f"q{qubit}_xy")
+                play("x180", f"q{qubit}_xy")
+            with case_(4):
+                play("x90", f"q{qubit}_xy")
+                play("y90", f"q{qubit}_xy")
+            with case_(5):
+                play("x90", f"q{qubit}_xy")
+                play("-y90", f"q{qubit}_xy")
+            with case_(6):
+                play("-x90", f"q{qubit}_xy")
+                play("y90", f"q{qubit}_xy")
+            with case_(7):
+                play("-x90", f"q{qubit}_xy")
+                play("-y90", f"q{qubit}_xy")
+            with case_(8):
+                play("y90", f"q{qubit}_xy")
+                play("x90", f"q{qubit}_xy")
+            with case_(9):
+                play("y90", f"q{qubit}_xy")
+                play("-x90", f"q{qubit}_xy")
+            with case_(10):
+                play("-y90", f"q{qubit}_xy")
+                play("x90", f"q{qubit}_xy")
+            with case_(11):
+                play("-y90", f"q{qubit}_xy")
+                play("-x90", f"q{qubit}_xy")
+            with case_(12):
+                play("x90", f"q{qubit}_xy")
+            with case_(13):
+                play("-x90", f"q{qubit}_xy")
+            with case_(14):
+                play("y90", f"q{qubit}_xy")
+            with case_(15):
+                play("-y90", f"q{qubit}_xy")
+            with case_(16):
+                play("-x90", f"q{qubit}_xy")
+                play("y90", f"q{qubit}_xy")
+                play("x90", f"q{qubit}_xy")
+            with case_(17):
+                play("-x90", f"q{qubit}_xy")
+                play("-y90", f"q{qubit}_xy")
+                play("x90", f"q{qubit}_xy")
+            with case_(18):
+                play("x180", f"q{qubit}_xy")
+                play("y90", f"q{qubit}_xy")
+            with case_(19):
+                play("x180", f"q{qubit}_xy")
+                play("-y90", f"q{qubit}_xy")
+            with case_(20):
+                play("y180", f"q{qubit}_xy")
+                play("x90", f"q{qubit}_xy")
+            with case_(21):
+                play("y180", f"q{qubit}_xy")
+                play("-x90", f"q{qubit}_xy")
+            with case_(22):
+                play("x90", f"q{qubit}_xy")
+                play("y90", f"q{qubit}_xy")
+                play("x90", f"q{qubit}_xy")
+            with case_(23):
+                play("-x90", f"q{qubit}_xy")
+                play("y90", f"q{qubit}_xy")
+                play("-x90", f"q{qubit}_xy")
+
+
+###################
+# The QUA program #
+###################
+with program() as rb:
+    depth = declare(int)
+    depth_target = declare(int)
+    saved_gate = declare(int)
+    m = declare(int)
+    n = declare(int)
+    I = declare(fixed)
+    Q = declare(fixed)
+    state = declare(bool)
+    if state_discrimination:
+        state_st = declare_stream()
+    else:
+        I_st = declare_stream()
+        Q_st = declare_stream()
+    m_st = declare_stream()
+
+    with for_(m, 0, m < num_of_sequences, m + 1):
+        # Generates the RB sequence with a gate interleaved after each Clifford
+        sequence_list, inv_gate_list = generate_sequence(interleaved_gate_index=interleaved_gate_index)
+        # Depth_target is used to always play the gates by pairs [(random_gate-interleaved_gate)^depth/2-inv_gate]
+        assign(depth_target, 0)
+        with for_(depth, 1, depth <= 2 * max_circuit_depth, depth + 1):
+            # Replacing the last gate in the sequence with the sequence's inverse gate
+            # The original gate is saved in 'saved_gate' and is being restored at the end
+            assign(saved_gate, sequence_list[depth])
+            assign(sequence_list[depth], inv_gate_list[depth - 1])
+
+            with if_((depth == 2) | (depth == depth_target)):
+                with for_(n, 0, n < n_avg, n + 1):
+                    # Can replace by active reset
+                    wait(cooldown_time, f"rr{qubit}")
+
+                    align()
+                    with strict_timing_():
+                        play_sequence(sequence_list, depth, qubit)
+
+                    align()
+                    # Play through the 2nd resonator to be in the same condition as when the readout was optimized
+                    measure("readout", f"rr{qubit%2 + 1}", None)
+                    multiplexed_readout([I], [I_st], [Q], [Q_st], resonators=[qubit], weights="rotated_")
+                    # Make sure you updated the ge_threshold
+                    if state_discrimination:
+                        assign(state, I > threshold)
+                        save(state, state_st)
+                # always play the random gate followed by the interleaved gate
+                assign(depth_target, depth_target + 2 * delta_clifford)
+            assign(sequence_list[depth], saved_gate)
+        save(m, m_st)
+
+    with stream_processing():
+        m_st.save("iteration")
+        if state_discrimination:
+            state_st.boolean_to_int().buffer(n_avg).map(FUNCTIONS.average()).buffer(
+                max_circuit_depth / delta_clifford + 1
+            ).average().save("state_avg")
+        else:
+            I_st.buffer(n_avg).map(FUNCTIONS.average()).buffer(max_circuit_depth / delta_clifford + 1).average().save(
+                "I"
+            )
+            Q_st.buffer(n_avg).map(FUNCTIONS.average()).buffer(max_circuit_depth / delta_clifford + 1).average().save(
+                "Q"
+            )
+
+
+#####################################
+#  Open Communication with the QOP  #
+#####################################
+qmm = QuantumMachinesManager(qop_ip)
+
+simulate = False
+
+if simulate:
+    simulation_config = SimulationConfig(duration=10000)  # in clock cycles
+    job = qmm.simulate(config, rb, simulation_config)
+    job.get_simulated_samples().con1.plot()
+
+else:
+    qm = qmm.open_qm(config)
+
+    job = qm.execute(rb)
+    # Get results from QUA program
+    if state_discrimination:
+        results = fetching_tool(job, data_list=["state_avg", "iteration"], mode="live")
+    else:
+        results = fetching_tool(job, data_list=["I", "Q", "iteration"], mode="live")
+    # Live plotting
+    fig = plt.figure()
+    interrupt_on_close(fig, job)  # Interrupts the job when closing the figure
+    # data analysis
+    x = np.arange(0, max_circuit_depth + 0.1, delta_clifford)
+    x[0] = 1  # to set the first value of 'x' to be depth = 1 as in the experiment
+    while results.is_processing():
+        # data analysis
+        if state_discrimination:
+            state_avg, iteration = results.fetch_all()
+            value_avg = 1 - state_avg
+            error_avg = np.std(state_avg)
+        else:
+            I, Q, iteration = results.fetch_all()
+            value_avg = np.sqrt(I**2 + Q**2)
+            error_avg = np.std(np.sqrt(I**2 + Q**2))
+
+        # Progress bar
+        progress_counter(iteration, num_of_sequences, start_time=results.get_start_time())
+        # Plot results
+        plt.cla()
+        plt.errorbar(x, value_avg, yerr=error_avg, marker=".")
+        pars, cov = curve_fit(
+            f=power_law,
+            xdata=x,
+            ydata=value_avg,
+            p0=[0.5, 0.5, 0.9],
+            bounds=(-np.inf, np.inf),
+            maxfev=2000,
+        )
+        plt.plot(x, power_law(x, *pars), linestyle="--", linewidth=2)
+        plt.xlabel("Number of Clifford gates")
+        plt.ylabel("Sequence Fidelity")
+        plt.title("Single qubit RB")
+        plt.pause(0.1)
+
+    stdevs = np.sqrt(np.diag(cov))
+
+    print("#########################")
+    print("### Fitted Parameters ###")
+    print("#########################")
+    print(f"A = {pars[0]:.3} ({stdevs[0]:.1}), B = {pars[1]:.3} ({stdevs[1]:.1}), p = {pars[2]:.3} ({stdevs[2]:.1})")
+    print("Covariance Matrix")
+    print(cov)
+
+    one_minus_p = 1 - pars[2]
+    r_c = one_minus_p * (1 - 1 / 2**1)
+    r_g = r_c / 1.875  # 1.875 is the average number of gates in clifford operation
+    r_c_std = stdevs[2] * (1 - 1 / 2**1)
+    r_g_std = r_c_std / 1.875
+
+    print("#########################")
+    print("### Useful Parameters ###")
+    print("#########################")
+    print(
+        f"Error rate: 1-p = {np.format_float_scientific(one_minus_p, precision=2)} ({stdevs[2]:.1})\n"
+        f"Clifford set infidelity: r_c = {np.format_float_scientific(r_c, precision=2)} ({r_c_std:.1})\n"
+        f"Gate infidelity: r_g = {np.format_float_scientific(r_g, precision=2)}  ({r_g_std:.1})"
+    )
+
+    # np.savez("rb_values", value)
