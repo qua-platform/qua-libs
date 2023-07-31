70b9d3c93515a95481e3bdc48eefe0fe78558b91
Kevin Villegas <kevin@quantum-machines.co>
2023-07-31 16:35:18 -0400

    04_odmr with *amp(0)

--- Quantum-Control-Applications/Optically addressable spin qubits/NV center in a confocal setup/04_cw_odmr.py
+++ Quantum-Control-Applications/Optically addressable spin qubits/NV center in a confocal setup/04_cw_odmr.py
@@ -13,11 +13,13 @@ from configuration import *
 
 f_vec = np.arange(-30 * u.MHz, 70 * u.MHz, 2 * u.MHz)  # f_max + 0.1 so that f_max is included
 n_avg = 1_000_000  # number of averages
+wait_between_runs = 10_000
 
 with program() as cw_odmr:
     times = declare(int, size=100)
     counts = declare(int)  # variable for number of counts
     counts_st = declare_stream()  # stream for counts
+    counts_dark_st = declare_stream()  # stream for counts
     f = declare(int)  # frequencies
     n = declare(int)  # number of iterations
     n_st = declare_stream()  # stream for number of iterations
@@ -25,16 +27,32 @@ with program() as cw_odmr:
     with for_(n, 0, n < n_avg, n + 1):
         with for_(*from_array(f, f_vec)):
             update_frequency("NV", f)  # update frequency
+
             align()  # align all elements
-            play("cw", "NV", duration= long_meas_len_1 * u.ns)  # play microwave pulse
+
+            play("cw"*amp(1), "NV", duration= long_meas_len_1 * u.ns)  # play microwave pulse
             play("laser_ON", "AOM1", duration=long_meas_len_1 * u.ns)
             measure("long_readout", "SPCM1", None, time_tagging.analog(times, long_meas_len_1, counts))
 
             save(counts, counts_st)  # save counts on stream
+
+            wait(wait_between_runs * u.ns)
+
+            align()  # align all elements
+
+            play("cw"*amp(0), "NV", duration= long_meas_len_1 * u.ns)  # play microwave pulse
+            play("laser_ON", "AOM1", duration=long_meas_len_1 * u.ns)
+            measure("long_readout", "SPCM1", None, time_tagging.analog(times, long_meas_len_1, counts))
+
+            save(counts, counts_dark_st)  # save counts on stream
+
+            wait(wait_between_runs * u.ns)
+
             save(n, n_st)  # save number of iteration inside for_loop
 
     with stream_processing():
         counts_st.buffer(len(f_vec)).average().save("counts")
+        counts_dark_st.buffer(len(f_vec)).average().save("counts_dark")
         n_st.save("iteration")
 
 #####################################
@@ -55,19 +73,20 @@ else:
     job = qm.execute(cw_odmr)  # execute QUA program
 
     # Get results from QUA program
-    results = fetching_tool(job, data_list=["counts", "iteration"], mode="live")
+    results = fetching_tool(job, data_list=["counts", "counts_dark", "iteration"], mode="live")
     # Live plotting
     fig = plt.figure()
     interrupt_on_close(fig, job)  # Interrupts the job when closing the figure
 
     while results.is_processing():
         # Fetch results
-        counts, iteration = results.fetch_all()
+        counts, counts_dark, iteration = results.fetch_all()
         # Progress bar
         progress_counter(iteration, n_avg, start_time=results.get_start_time())
         # Plot data
         plt.cla()
         plt.plot((NV_LO_freq * 0 + f_vec) / u.MHz, counts / 1000 / (long_meas_len_1 * 1e-9))
+        plt.plot((NV_LO_freq * 0 + f_vec) / u.MHz, counts_dark / 1000 / (long_meas_len_1 * 1e-9))
         plt.xlabel("Frequency [MHz]")
         plt.ylabel("Intensity [kcps]")
         plt.title("ODMR")
