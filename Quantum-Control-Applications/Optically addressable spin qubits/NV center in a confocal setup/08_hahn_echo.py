566ba4cacf28c5637b5507922ba08db6328b8a14
Kevin Villegas <kevin@quantum-machines.co>
2023-08-01 10:45:37 -0400

    08_hanh_echo with dark

--- Quantum-Control-Applications/Optically addressable spin qubits/NV center in a confocal setup/08_hahn_echo.py
+++ Quantum-Control-Applications/Optically addressable spin qubits/NV center in a confocal setup/08_hahn_echo.py
@@ -18,23 +18,26 @@ n_avg = 1_000_000
 with program() as hahn_echo:
     counts1 = declare(int)  # saves number of photon counts
     counts2 = declare(int)  # saves number of photon counts
+    counts2_dark = declare(int)  # saves number of photon counts
     times1 = declare(int, size=100)
     times2 = declare(int, size=100)
+    times2_dark = declare(int, size=100)
     t = declare(int)  # variable to sweep over in time
     n = declare(int)  # variable to for_loop
     counts_1_st = declare_stream()  # stream for counts
     counts_2_st = declare_stream()  # stream for counts
+    counts_2_st_dark = declare_stream()  # stream for counts
     n_st = declare_stream()  # stream to save iterations
 
     play("laser_ON", "AOM1")
     wait(100 * u.ns)
     with for_(n, 0, n < n_avg, n + 1):
         with for_(*from_array(t, t_vec)):
-            play("x90", "NV")  # Pi/2 pulse to qubit
+            play("x90"*amp(1), "NV")  # Pi/2 pulse to qubit
             wait(t, "NV")  # variable delay in spin Echo
-            play("x180", "NV")  # Pi pulse to qubit
+            play("x180"*amp(1), "NV")  # Pi pulse to qubit
             wait(t, "NV")  # variable delay in spin Echo
-            play("x90", "NV")  # Pi/2 pulse to qubit
+            play("x90"*amp(1), "NV")  # Pi/2 pulse to qubit
 
             align()
 
@@ -45,11 +48,27 @@ with program() as hahn_echo:
 
             align()
 
-            play("x90", "NV")  # Pi/2 pulse to qubit
+            play("x90"*amp(1), "NV")  # Pi/2 pulse to qubit
             wait(t, "NV")  # variable delay in spin Echo
-            play("x180", "NV")  # Pi pulse to qubit
+            play("x180"*amp(1), "NV")  # Pi pulse to qubit
             wait(t, "NV")  # variable delay in spin Echo
-            play("-x90", "NV")  # Pi/2 pulse to qubit
+            play("-x90"*amp(1), "NV")  # Pi/2 pulse to qubit
+            reset_frame("NV")
+
+            align()
+
+            play("laser_ON", "AOM1")
+            measure("readout", "SPCM1", None, time_tagging.analog(times2, meas_len_1, counts2))
+            save(counts2, counts_2_st)  # save counts
+            wait(100 * u.ns, "AOM1")
+
+            align()
+
+            play("x90"*amp(0), "NV")  # Pi/2 pulse to qubit
+            wait(t, "NV")  # variable delay in spin Echo
+            play("x180"*amp(0), "NV")  # Pi pulse to qubit
+            wait(t, "NV")  # variable delay in spin Echo
+            play("-x90"*amp(0), "NV")  # Pi/2 pulse to qubit
             reset_frame("NV")
 
             align()
@@ -64,6 +83,7 @@ with program() as hahn_echo:
     with stream_processing():
         counts_1_st.buffer(len(t_vec)).average().save("counts1")
         counts_2_st.buffer(len(t_vec)).average().save("counts2")
+        counts_2_st_dark.buffer(len(t_vec)).average().save("counts2_dark")
         n_st.save("iteration")
 
 #####################################
@@ -83,19 +103,21 @@ else:
     # execute QUA program
     job = qm.execute(hahn_echo)
     # Get results from QUA program
-    results = fetching_tool(job, data_list=["counts1", "counts2", "iteration"], mode="live")
+    results = fetching_tool(job, data_list=["counts1", "counts2", "counts2_dark", "iteration"], mode="live")
     # Live plotting
     fig = plt.figure()
     interrupt_on_close(fig, job)  # Interrupts the job when closing the figure
 
     while results.is_processing():
         # Fetch results
-        counts1, counts2, iteration = results.fetch_all()
+        counts1, counts2, counts2_dark, iteration = results.fetch_all()
         # Progress bar
         progress_counter(iteration, n_avg, start_time=results.get_start_time())
         # Plot data
         plt.cla()
-        plt.plot(4 * t_vec, counts1 / 1000 / (meas_len_1 / u.s), counts2 / 1000 / (meas_len_1 / u.s))
+        plt.plot(4 * t_vec, counts1 / 1000 / (meas_len_1 / u.s))
+        plt.plot(4 * t_vec, counts2 / 1000 / (meas_len_1 / u.s))
+        plt.plot(4 * t_vec, counts2_dark / 1000 / (meas_len_1 / u.s))
         plt.xlabel("Dephasing time [ns]")
         plt.ylabel("Intensity [kcps]")
         plt.title("Hahn Echo")
