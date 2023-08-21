b6712e946e858f364b382f75617941219074b672
TheoQM <theo@quantum-machines.co>
2023-08-21 09:27:11 +0200

    ODMR

--- Quantum-Control-Applications/Optically addressable spin qubits/NV center in a confocal setup/04_cw_odmr.py
+++ Quantum-Control-Applications/Optically addressable spin qubits/NV center in a confocal setup/04_cw_odmr.py
@@ -1,25 +1,33 @@
 """
-cw_odmr.py: Counts photons while sweeping the frequency of the applied MW.
+        CW Optically Detected Magnetic Resonance (ODMR)
+The program consists in playing a mw pulse and the readout laser pulse simultaneously to extract
+the photon counts received by the SPCM across varying intermediate frequencies.
+
+The data is then post-processed to determine the spin resonance frequency.
+This frequency can be used to update the NV intermediate frequency in the configuration under "NV_IF_freq".
+
+Next steps before going to the next node:
+    - Update the NV frequency, labeled as "NV_IF_freq", in the configuration.
 """
+
 from qm.QuantumMachinesManager import QuantumMachinesManager
 from qm.qua import *
 from qm import SimulationConfig
 import matplotlib.pyplot as plt
 from configuration import *
-import warnings
 
-warnings.filterwarnings("ignore")
 
 ###################
 # The QUA program #
 ###################
 
+# Frequency vector
 f_vec = np.arange(-30 * u.MHz, 70 * u.MHz, 2 * u.MHz)
-n_avg = 1_000_000  # number of averages
-wait_between_runs = 100
+n_avg = 100_000  # number of averages
+readout_len = long_meas_len_1  # Readout duration for this experiment
 
 with program() as cw_odmr:
-    times = declare(int, size=100)
+    times = declare(int, size=100)  # QUA vector for storing the time-tags
     counts = declare(int)  # variable for number of counts
     counts_st = declare_stream()  # stream for counts
     counts_dark_st = declare_stream()  # stream for counts
@@ -29,25 +37,29 @@ with program() as cw_odmr:
 
     with for_(n, 0, n < n_avg, n + 1):
         with for_(*from_array(f, f_vec)):
-            update_frequency("NV", f)  # update frequency
-
-            align()  # align all elements
-
-            play("cw" * amp(1), "NV", duration=long_meas_len_1 * u.ns)  # play microwave pulse
-            play("laser_ON", "AOM1", duration=long_meas_len_1 * u.ns)
-            wait(1_000 * u.ns, "SPCM1")  # so readout don't catch the first part of spin reinitializaiton
-            measure("long_readout", "SPCM1", None, time_tagging.analog(times, long_meas_len_1, counts))
+            # Update the frequency of the digital oscillator linked to the element "NV"
+            update_frequency("NV", f)
+            # align all elements before starting the sequence
+            align()
+            # Play the mw pulse...
+            play("cw" * amp(1), "NV", duration=readout_len * u.ns)
+            # ... and the laser pulse simultaneously (the laser pulse is delayed by 'laser_delay_1')
+            play("laser_ON", "AOM1", duration=readout_len * u.ns)
+            wait(1_000 * u.ns, "SPCM1")  # so readout don't catch the first part of spin reinitialization
+            # Measure and detect the photons of SPCM1
+            measure("long_readout", "SPCM1", None, time_tagging.analog(times, readout_len, counts))
 
             save(counts, counts_st)  # save counts on stream
 
+            # Wait and align all elements before measuring the dark events
             wait(wait_between_runs * u.ns)
-
             align()  # align all elements
-
-            play("cw" * amp(0), "NV", duration=long_meas_len_1 * u.ns)  # play microwave pulse
-            play("laser_ON", "AOM1", duration=long_meas_len_1 * u.ns)
+            # Play the mw pulse with zero amplitude...
+            play("cw" * amp(0), "NV", duration=readout_len * u.ns)
+            # ... and the laser pulse simultaneously (the laser pulse is delayed by 'laser_delay_1')
+            play("laser_ON", "AOM1", duration=readout_len * u.ns)
             wait(1_000 * u.ns, "SPCM1")  # so readout don't catch the first part of spin reinitialization
-            measure("long_readout", "SPCM1", None, time_tagging.analog(times, long_meas_len_1, counts))
+            measure("long_readout", "SPCM1", None, time_tagging.analog(times, readout_len, counts))
 
             save(counts, counts_dark_st)  # save counts on stream
 
@@ -56,6 +68,7 @@ with program() as cw_odmr:
             save(n, n_st)  # save number of iteration inside for_loop
 
     with stream_processing():
+        # Cast the data into a 1D vector, average the 1D vectors together and store the results on the OPX processor
         counts_st.buffer(len(f_vec)).average().save("counts")
         counts_dark_st.buffer(len(f_vec)).average().save("counts_dark")
         n_st.save("iteration")
@@ -93,9 +106,10 @@ else:
         progress_counter(iteration, n_avg, start_time=results.get_start_time())
         # Plot data
         plt.cla()
-        plt.plot((NV_LO_freq * 0 + f_vec) / u.MHz, counts / 1000 / (long_meas_len_1 * 1e-9))
-        plt.plot((NV_LO_freq * 0 + f_vec) / u.MHz, counts_dark / 1000 / (long_meas_len_1 * 1e-9))
-        plt.xlabel("Frequency [MHz]")
+        plt.plot((NV_LO_freq * 0 + f_vec) / u.MHz, counts / 1000 / (readout_len * 1e-9), label="photon counts")
+        plt.plot((NV_LO_freq * 0 + f_vec) / u.MHz, counts_dark / 1000 / (readout_len * 1e-9), label="dark counts")
+        plt.xlabel("MW frequency [MHz]")
         plt.ylabel("Intensity [kcps]")
         plt.title("ODMR")
+        plt.legend()
         plt.pause(0.1)
