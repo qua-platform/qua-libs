094e0709d4d46448e6485c45f6f7602ed1015247
TheoQM <theo@quantum-machines.co>
2023-08-21 14:51:19 +0200

    Up to RB

--- Quantum-Control-Applications/Superconducting/Single-Flux-Tunable-Transmon/18_DRAG_calibration_Google.py
+++ Quantum-Control-Applications/Superconducting/Single-Flux-Tunable-Transmon/18_DRAG_calibration_Google.py
@@ -1,3 +1,22 @@
+"""
+        DRAG PULSE CALIBRATION (GOOGLE METHOD)
+The sequence consists in applying an increasing number of x180 and -x180 pulses successively while varying the DRAG
+coefficient alpha. After such a sequence, the qubit is expected to always be in the ground state if the DRAG
+coefficient has the correct value. Note that the idea is very similar to what is done in power_rabi_error_amplification.
+
+This protocol is described in more details in https://journals.aps.org/prl/abstract/10.1103/PhysRevLett.117.190503
+
+Prerequisites:
+    - Having found the resonance frequency of the resonator coupled to the qubit under study (resonator_spectroscopy).
+    - Having calibrated qubit pi pulse (x180) by running qubit, spectroscopy, rabi_chevron, power_rabi and updated the config.
+    - (optional) Having calibrated the readout (readout_frequency, amplitude, duration_optimization IQ_blobs) for better SNR.
+    - Set the DRAG coefficient to a non-zero value in the config: such as drag_coef = 1
+    - Set the desired flux bias.
+
+Next steps before going to the next node:
+    - Update the DRAG coefficient (drag_coef) in the configuration.
+"""
+
 from qm.qua import *
 from qm.QuantumMachinesManager import QuantumMachinesManager
 from configuration import *
@@ -6,81 +25,87 @@ import numpy as np
 from qm import SimulationConfig
 from macros import readout_macro
 from qualang_tools.loops import from_array
+import warnings
 
-"""
-An experiment to calibrate the DRAG coefficient: drag_coef
-This protocol is described in https://journals.aps.org/prl/abstract/10.1103/PhysRevLett.117.190503
-"""
+warnings.filterwarnings("ignore")
 
 ###################
 # The QUA program #
 ###################
-
-# set the drag_coef in the configuration
-drag_coef = 1
-
 n_avg = 1000
 
-cooldown_time = 5 * qubit_T1
-
+# Scan the DRAG coefficient pre-factor
 a_min = 0.0
 a_max = 1.0
 da = 0.1
 amps = np.arange(a_min, a_max + da / 2, da)  # + da/2 to add a_max to amplitudes
 
+# Scan the number of pulses
 iter_min = 0
 iter_max = 25
 d = 1
 iters = np.arange(iter_min, iter_max + 0.1, d)
 
 with program() as drag:
-    n = declare(int)
-    n_st = declare_stream()
-    a = declare(fixed)
-    I = declare(fixed)
-    Q = declare(fixed)
-    I_st = declare_stream()
-    Q_st = declare_stream()
-    state = declare(bool)
-    state_st = declare_stream()
-    it = declare(int)
-    pulses = declare(int)
+    n = declare(int)  # QUA variable for the averaging loop
+    a = declare(fixed)  # QUA variable for the DRAG coefficient pre-factor
+    it = declare(int)  # QUA variable for the number of qubit pulses
+    pulses = declare(int)  # QUA variable for counting the qubit pulses
+    I = declare(fixed)  # QUA variable for the measured 'I' quadrature
+    Q = declare(fixed)  # QUA variable for the measured 'Q' quadrature
+    state = declare(bool)  # QUA variable for the qubit state
+    I_st = declare_stream()  # Stream for the 'I' quadrature
+    Q_st = declare_stream()  # Stream for the 'Q' quadrature
+    state_st = declare_stream()  # Stream for the qubit state
+    n_st = declare_stream()  # Stream for the averaging iteration 'n'
 
-    with for_(n, 0, n < n_avg, n + 1):
-        with for_(*from_array(a, amps)):
-            with for_(*from_array(it, iters)):
+    with for_(n, 0, n < n_avg, n + 1):  # QUA for_ loop for averaging
+        with for_(*from_array(a, amps)):  # QUA for_ loop for sweeping the pulse amplitude
+            with for_(*from_array(it, iters)):  # QUA for_ loop for sweeping the number of pulses
+                # Loop for error amplification (perform many qubit pulses with varying DRAG coefficients)
                 with for_(pulses, iter_min, pulses <= it, pulses + d):
                     play("x180" * amp(1, 0, 0, a), "qubit")
                     play("x180" * amp(-1, 0, 0, -a), "qubit")
-            align("qubit", "resonator")
-            state, I, Q = readout_macro(threshold=ge_threshold, state=state, I=I, Q=Q)
-            save(I, I_st)
-            save(Q, Q_st)
-            save(state, state_st)
-            wait(cooldown_time * u.ns, "resonator")
+                # Align the two elements to measure after playing the qubit pulses.
+                align("qubit", "resonator")
+                # Measure the resonator and extract the qubit state
+                state, I, Q = readout_macro(threshold=ge_threshold, state=state, I=I, Q=Q)
+                # Wait for the qubit to decay to the ground state
+                wait(thermalization_time * u.ns, "resonator")
+                # Save the 'I' & 'Q' quadratures to their respective streams
+                save(I, I_st)
+                save(Q, Q_st)
+                save(state, state_st)
+        # Save the averaging iteration to get the progress bar
         save(n, n_st)
 
     with stream_processing():
+        # Cast the data into a 2D matrix, average the 2D matrices together and store the results on the OPX processor
         I_st.buffer(len(iters)).buffer(len(amps)).average().save("I")
         Q_st.buffer(len(iters)).buffer(len(amps)).average().save("Q")
-        n_st.save("iteration")
         state_st.boolean_to_int().buffer(len(iters)).buffer(len(amps)).average().save("state")
+        n_st.save("iteration")
 
 #####################################
 #  Open Communication with the QOP  #
 #####################################
-qmm = QuantumMachinesManager(qop_ip, qop_port, octave=octave_config)
+qmm = QuantumMachinesManager(qop_ip, cluster_name=cluster_name, octave=octave_config)
 
+###########################
+# Run or Simulate Program #
+###########################
 simulate = False
 
 if simulate:
-    simulation_config = SimulationConfig(duration=1000)  # in clock cycles
+    # Simulates the QUA program for the specified duration
+    simulation_config = SimulationConfig(duration=10_000)  # In clock cycles = 4ns
     job = qmm.simulate(config, drag, simulation_config)
     job.get_simulated_samples().con1.plot()
 
 else:
+    # Open the quantum machine
     qm = qmm.open_qm(config)
-
+    # Send the QUA program to the OPX, which compiles and executes it
     job = qm.execute(drag)
     # Get results from QUA program
     results = fetching_tool(job, data_list=["I", "Q", "state", "iteration"], mode="live")
@@ -94,9 +119,22 @@ else:
         # Progress bar
         progress_counter(iteration, n_avg, start_time=results.get_start_time())
         # Plot results
+        plt.suptitle("DRAG calibration (Google)")
+        plt.subplot(131)
+        plt.cla()
+        plt.pcolor(iters, amps * drag_coef, I, cmap="magma")
+        plt.xlabel("Number of iterations")
+        plt.ylabel(r"Drag coefficient $\alpha$")
+        plt.title("I")
+        plt.subplot(132)
+        plt.cla()
+        plt.pcolor(iters, amps * drag_coef, Q, cmap="magma")
+        plt.xlabel("Number of iterations")
+        plt.title("Q")
+        plt.subplot(133)
         plt.cla()
         plt.pcolor(iters, amps * drag_coef, state, cmap="magma")
         plt.xlabel("Number of iterations")
-        plt.ylabel("Drag coef")
-    # Close the quantum machines at the end in order to put all flux biases to 0 so that the fridge doesn't heat-up
-    qm.close()
+        plt.title("State")
+        plt.tight_layout()
+        plt.pause(0.1)
