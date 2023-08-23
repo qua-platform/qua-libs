eae1dc2e790b5ead4d1f665453c7aa18ba17f95d
Kevin Villegas <kevin@quantum-machines.co>
2023-08-23 13:30:11 -0400

    black

--- Quantum-Control-Applications/Superconducting/Single-Flux-Tunable-Transmon/Use Case 3 - Ma Lab - Parametric Drive iSWAP/blue_sideband_chevron.py
+++ Quantum-Control-Applications/Superconducting/Single-Flux-Tunable-Transmon/Use Case 3 - Ma Lab - Parametric Drive iSWAP/blue_sideband_chevron.py
@@ -3,6 +3,7 @@ from qm.qua import *
 from qm import LoopbackInterface
 from qm.QuantumMachinesManager import QuantumMachinesManager
 from configuration_bs import *
+
 # from malab import *
 import matplotlib.pyplot as plt
 import numpy as np
@@ -28,14 +29,14 @@ t_min = 16 // 4  # in units of clock cycles
 t_max = 1200 // 4  # in units of clock cycles
 dt = 8 // 4
 
-times = np.arange(t_min, t_max + dt/2, dt)
+times = np.arange(t_min, t_max + dt / 2, dt)
 
 # sideband modulation freq
 f_min = 95e6
 f_max = 110e6
 df = 0.1e6
 
-freqs = np.arange(f_min, f_max + df/2, df)  # + df/2 to add f_max to freqs
+freqs = np.arange(f_min, f_max + df / 2, df)  # + df/2 to add f_max to freqs
 
 N_max = 50
 
@@ -43,7 +44,6 @@ cooldown_time = 100000 // 4
 flux_dur = 100000 // 4
 
 with program() as chevron_rabi:
-
     # Declare QUA variables
     ###################
     f = declare(int)  # variable for freqs sweep
@@ -54,23 +54,25 @@ with program() as chevron_rabi:
     Q = declare(fixed)
     I_st = declare_stream()
     Q_st = declare_stream()
-    a = declare(fixed, value = ff_a)
-
+    a = declare(fixed, value=ff_a)
 
     # Pulse sequence
     ################
-    with for_(n, 0, n < N_max, n+1):
-
+    with for_(n, 0, n < N_max, n + 1):
         with for_(tau, t_min, tau <= t_max, tau + dt):
             with for_(f, f_min, f <= f_max, f + df):
-                update_frequency('flux1', f)  # update frequency of operations to the qubit
-                wait(cooldown_time, 'qubit1', 'flux1')  # for qubit to decay
-                align('qubit1', 'flux1')
-                play('const' * amp(a), 'flux1', duration=tau) # apply blue sideband flux modulation for the qubit
-                align('resonator1', 'flux1')
-                measure('readout', 'resonator1', None,
-                        dual_demod.full('cos', 'out1', 'minus_sin', 'out2', I),
-                        dual_demod.full('sin', 'out1', 'cos', 'out2', Q))
+                update_frequency("flux1", f)  # update frequency of operations to the qubit
+                wait(cooldown_time, "qubit1", "flux1")  # for qubit to decay
+                align("qubit1", "flux1")
+                play("const" * amp(a), "flux1", duration=tau)  # apply blue sideband flux modulation for the qubit
+                align("resonator1", "flux1")
+                measure(
+                    "readout",
+                    "resonator1",
+                    None,
+                    dual_demod.full("cos", "out1", "minus_sin", "out2", I),
+                    dual_demod.full("sin", "out1", "cos", "out2", Q),
+                )
                 save(I, I_st)
                 save(Q, Q_st)
 
@@ -80,9 +82,9 @@ with program() as chevron_rabi:
     # Stream processing
     ###################
     with stream_processing():
-        n_st.save('iteration')
-        I_st.buffer(len(times), len(freqs)).average().save('I')
-        Q_st.buffer(len(times), len(freqs)).average().save('Q')
+        n_st.save("iteration")
+        I_st.buffer(len(times), len(freqs)).average().save("I")
+        Q_st.buffer(len(times), len(freqs)).average().save("Q")
 
 #######################
 # Simulate or execute #
@@ -92,8 +94,9 @@ simulate = True
 
 if simulate:
     # simulation properties
-    simulate_config = SimulationConfig(duration=100000
-                                       , simulation_interface=LoopbackInterface(([('con1', 1, 'con1', 1)])))
+    simulate_config = SimulationConfig(
+        duration=100000, simulation_interface=LoopbackInterface(([("con1", 1, "con1", 1)]))
+    )
     job = qmm.simulate(config, chevron_rabi, simulate_config)  # do simulation with qmm
     job.get_simulated_samples().con1.plot()  # visualize played pulses
 
@@ -101,11 +104,11 @@ else:
     job = qm.execute(chevron_rabi)  # execute QUA program
 
     res_handles = job.result_handles  # get access to handles
-    I_handle = res_handles.get('I')
+    I_handle = res_handles.get("I")
     I_handle.wait_for_values(1)
-    Q_handle = res_handles.get('Q')
+    Q_handle = res_handles.get("Q")
     Q_handle.wait_for_values(1)
-    iteration_handle = res_handles.get('iteration')
+    iteration_handle = res_handles.get("iteration")
     iteration_handle.wait_for_values(1)
 
     while res_handles.is_processing():
@@ -113,10 +116,10 @@ else:
             I = I_handle.fetch_all()
             Q = Q_handle.fetch_all()
             iteration = iteration_handle.fetch_all() + 1
-            plt.title('Chevron pattern')
-            plt.pcolor(freqs, times*4, I)
-            plt.xlabel('modulation freqs')
-            plt.ylabel('Variable pulse length [ns]')
+            plt.title("Chevron pattern")
+            plt.pcolor(freqs, times * 4, I)
+            plt.xlabel("modulation freqs")
+            plt.ylabel("Variable pulse length [ns]")
             plt.pause(0.1)
             plt.clf()
             print(iteration)
@@ -126,7 +129,7 @@ else:
 
     I = I_handle.fetch_all()
     Q = Q_handle.fetch_all()
-    plt.title('Chevron pattern')
-    plt.pcolor(freqs, times*4, I)
-    plt.xlabel('modulation freqs')
-    plt.ylabel('Variable pulse length [ns]')
+    plt.title("Chevron pattern")
+    plt.pcolor(freqs, times * 4, I)
+    plt.xlabel("modulation freqs")
+    plt.ylabel("Variable pulse length [ns]")
