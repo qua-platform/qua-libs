Fix rotation weight

Quantum-Control-Applications/Superconducting/Single-Flux-Tunable-Transmon/configuration.py
@@ -405,14 +405,14 @@ config = {
         },
         "rotated_cosine_weights": {
             "cosine": [(np.cos(rotation_angle), readout_len)],
-            "sine": [(-np.sin(rotation_angle), readout_len)],
+            "sine": [(np.sin(rotation_angle), readout_len)],
         },
         "rotated_sine_weights": {
-            "cosine": [(np.sin(rotation_angle), readout_len)],
+            "cosine": [(-np.sin(rotation_angle), readout_len)],
             "sine": [(np.cos(rotation_angle), readout_len)],
         },
         "rotated_minus_sine_weights": {
-            "cosine": [(-np.sin(rotation_angle), readout_len)],
+            "cosine": [(np.sin(rotation_angle), readout_len)],
             "sine": [(-np.cos(rotation_angle), readout_len)],
         },
     },

Quantum-Control-Applications/Superconducting/Two-Flux-Tunable-Transmons/Rapid Prototyping/configuration.py
@@ -397,20 +397,20 @@ def build_config(quam: QuAM):
             **{
                 f"rotated_cosine_weights{i}": {
                     "cosine": [(np.cos(quam.resonators[i].rotation_angle), quam.resonators[i].readout_pulse_length)],
-                    "sine": [(-np.sin(quam.resonators[i].rotation_angle), quam.resonators[i].readout_pulse_length)],
+                    "sine": [(np.sin(quam.resonators[i].rotation_angle), quam.resonators[i].readout_pulse_length)],
                 }
                 for i in range(len(quam.resonators))
             },
             **{
                 f"rotated_sine_weights{i}": {
-                    "cosine": [(np.sin(quam.resonators[i].rotation_angle), quam.resonators[i].readout_pulse_length)],
+                    "cosine": [(-np.sin(quam.resonators[i].rotation_angle), quam.resonators[i].readout_pulse_length)],
                     "sine": [(np.cos(quam.resonators[i].rotation_angle), quam.resonators[i].readout_pulse_length)],
                 }
                 for i in range(len(quam.resonators))
             },
             **{
                 f"rotated_minus_sine_weights{i}": {
-                    "cosine": [(-np.sin(quam.resonators[i].rotation_angle), quam.resonators[i].readout_pulse_length)],
+                    "cosine": [(np.sin(quam.resonators[i].rotation_angle), quam.resonators[i].readout_pulse_length)],
                     "sine": [(-np.cos(quam.resonators[i].rotation_angle), quam.resonators[i].readout_pulse_length)],
                 }
                 for i in range(len(quam.resonators))

Quantum-Control-Applications/Superconducting/Two-Flux-Tunable-Transmons/Standard Configuration/configuration_with_octave.py
@@ -527,26 +527,26 @@ config = {
         },
         "rotated_cosine_weights_q1": {
             "cosine": [(np.cos(rotation_angle_q1), readout_len)],
-            "sine": [(-np.sin(rotation_angle_q1), readout_len)],
+            "sine": [(np.sin(rotation_angle_q1), readout_len)],
         },
         "rotated_sine_weights_q1": {
-            "cosine": [(np.sin(rotation_angle_q1), readout_len)],
+            "cosine": [(-np.sin(rotation_angle_q1), readout_len)],
             "sine": [(np.cos(rotation_angle_q1), readout_len)],
         },
         "rotated_minus_sine_weights_q1": {
-            "cosine": [(-np.sin(rotation_angle_q1), readout_len)],
+            "cosine": [(np.sin(rotation_angle_q1), readout_len)],
             "sine": [(-np.cos(rotation_angle_q1), readout_len)],
         },
         "rotated_cosine_weights_q2": {
             "cosine": [(np.cos(rotation_angle_q2), readout_len)],
-            "sine": [(-np.sin(rotation_angle_q2), readout_len)],
+            "sine": [(np.sin(rotation_angle_q2), readout_len)],
         },
         "rotated_sine_weights_q2": {
-            "cosine": [(np.sin(rotation_angle_q2), readout_len)],
+            "cosine": [(-np.sin(rotation_angle_q2), readout_len)],
             "sine": [(np.cos(rotation_angle_q2), readout_len)],
         },
         "rotated_minus_sine_weights_q2": {
-            "cosine": [(-np.sin(rotation_angle_q2), readout_len)],
+            "cosine": [(np.sin(rotation_angle_q2), readout_len)],
             "sine": [(-np.cos(rotation_angle_q2), readout_len)],
         },
     },