c89ae68eb070524e24e449e42a9bcb098289bd8e
Maximilian Zanner <maximilian@quantum-machines.co>
2023-09-06 23:22:10 +0200

    state tomography runs and analysis runs, not tested yet

--- Quantum-Control-Applications/Superconducting/Two-Flux-Tunable-Transmons/Standard Configuration/state_tomography.py
+++ Quantum-Control-Applications/Superconducting/Two-Flux-Tunable-Transmons/Standard Configuration/state_tomography.py
@@ -22,14 +22,16 @@ from qm.qua import *
 from lib.tomography import hist_da_to_qiskit_state_tomo_results
 from configuration import *
 from qm.QuantumMachinesManager import QuantumMachinesManager
-
+from macros import qua_declaration, multiplexed_readout
+from qualang_tools.results import fetching_tool
+import xarray as xr
 #%%
 
 # %% Qua program
-shots = 10
+shots = 10_000
 with program() as prog:
-            
-            n = declare(int)
+            I, I_st, Q, Q_st, n, n_st = qua_declaration(nb_of_qubits=2)
+            align()
             basis_q1 = declare(int)
             basis_q2 = declare(int)
 
@@ -47,20 +49,19 @@ with program() as prog:
             tomo_amp3 = {q: declare(fixed) for q in qp_iter}
             tomo_amp4 = {q: declare(fixed) for q in qp_iter}
 
-
-            I1 = declare(fixed)
-            Q1 = declare(fixed)
-            I2 = declare(fixed)
-            Q2 = declare(fixed)
-
             state1 = declare(bool)
             state2 = declare(bool)
             state = declare(int)
-            states = declare_stream()
+            state_st = declare_stream()
+            # the serialization contains this
+            # wait((4+(0*(Cast.to_int(v2)+Cast.to_int(v4)))), "rr1")
+            # wait((4+(0*(Cast.to_int(v3)+Cast.to_int(v5)))), "rr2") 
+            # what does this do??? does it come from the assign to element?
 
             # machine.all_flux_to_idle()
             # align()
             with for_(n, 0, n < shots, n+1):
+                save(n,n_st)
                 with for_each_(basis_q1,bases['mbasis_q1']):
                     with for_each_(basis_q2,bases['mbasis_q2']):
                         # prep
@@ -83,7 +84,7 @@ with program() as prog:
                                     assign(tomo_amp4[q], 0.5)
 
                         # implement active reset on both qubits and remove wait
-                        wait(10*qubit_T1)
+                        # wait(10*qubit_T1)
 
 
                         ######################
@@ -105,57 +106,44 @@ with program() as prog:
                         for q in qp_iter:
                             play('x180'*amp(tomo_amp1[q], tomo_amp2[q], tomo_amp3[q], tomo_amp4[q]), f'{q}_xy')
                             wait(20, f'{q}_xy')
-                        measure("readout" * amp(1),
-                                "rr1",
-                                None,
-                                dual_demod.full("cos", "out1", "sin", "out2", I1),
-                                dual_demod.full("minus_sin", "out1", "cos", "out2", Q1))
-                        measure("readout" * amp(1),
-                                "rr2",
-                                None,
-                                dual_demod.full("cos", "out1", "sin", "out2", I2),
-                                dual_demod.full("minus_sin", "out1", "cos", "out2", Q2))
                         align()
-                        assign(state1, (I1 > ge_threshold_q1))
-                        assign(state2, (I2 > ge_threshold_q2))
+                        multiplexed_readout(I, None, Q, None, resonators=[1, 2], weights="rotated_")
+                        align()
+                        assign(state1, (I[0] > ge_threshold_q1))
+                        assign(state2, (I[1] > ge_threshold_q2))
                         
                         # this corresponds to qiskit little-endian convention
                         # TODO: do we have a macro for this?
                         assign(state, (Cast.unsafe_cast_int(state1)+(Cast.unsafe_cast_int(state2)<<1)))
                         
-                        save(state, states)
+                        save(state, state_st)
+                        
                         wait(100, )
             with stream_processing():
-                states.buffer(10, 3, 3).save("statequbit0_qubit1")
-                    # r1.save("qubit0_qubit1")
-
-
-
-
-
+                state_st.buffer(shots, 3, 3).save("statequbit0_qubit1")
+                n_st.save("shot")
 # %% Calibrate
 qmm = QuantumMachinesManager(host='127.0.0.1', port=8080)#, octave=octave_config)
-
-# simulate = False
-# if simulate:
-#     job = qmm.simulate(config, cryoscope, SimulationConfig(11000))
-#     job.get_simulated_samples().con1.plot()
-# else:
 qm = qmm.open_qm(config)
 job = qm.execute(prog)
+results = fetching_tool(job, ['shot', 'statequbit0_qubit1' ], mode="wait_for_all")
+n , data = results.fetch_all()
+#%%
+data_vars = {'state': (['qp','shots', 'mbasis_q1', 'mbasis_q2'], [data])}
+coords = {'qp':['qubit0_qubit1'],'shots': np.arange(0,shots,1),
+          'mbasis_q1': np.array([0, 1, 2]),  'mbasis_q2': np.array([0, 1, 2])}
+ds = xr.Dataset(data_vars=data_vars, coords=coords)
+
+
 
 # %%
-ds = prog.get_results(job, batch_label="qp", filen='bell_state_tomo')
+# ds = prog.get_results(job, batch_label="qp", filen='bell_state_tomo')
 
 dsh = integer_histogram(ds.state, 'shots')
 dsh
 # %%
-
-for qp in machine.active_qubit_pairs():
-    dsh_stacked = dsh.sel(qp=qp_name(qp)).stack(
-        mbasis=['mbasis_q0', 'mbasis_q1'])
-    qiskit_tomo_res = hist_da_to_qiskit_state_tomo_results(
-        dsh_stacked, [qp.q0.name, qp.q1.name])
+dsh_stacked = dsh.sel(qp='qubit0_qubit1').stack(mbasis=['mbasis_q1', 'mbasis_q2'])
+qiskit_tomo_res = hist_da_to_qiskit_state_tomo_results(dsh_stacked, ['q1', 'q2'])
 
 # try several different visualization methods
 
@@ -208,4 +196,12 @@ sampled_expectation_value(qiskit_tomo_res.data()[0]['counts'], Pauli('ZZ'))
 
 marginal_counts(qstdata1.data(0)['counts'], [0])
 
+# %%
+from qm import generate_qua_script
+qmm = QuantumMachinesManager(host='127.0.0.1', port=8080)
+qm = qmm.open_qm(config)
+
+sourceFile = open('debug.py', 'w')
+print(generate_qua_script(prog, config), file=sourceFile)
+sourceFile.close()
 # %%
