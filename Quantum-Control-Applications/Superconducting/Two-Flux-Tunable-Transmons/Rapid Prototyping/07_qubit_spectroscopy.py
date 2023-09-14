cd4d240431de926d136d5b6d390ea917db190ba7
TheoQM <theo@quantum-machines.co>
2023-09-14 18:13:27 +0200

    qubit specs

--- Quantum-Control-Applications/Superconducting/Two-Flux-Tunable-Transmons/Rapid Prototyping/07_qubit_spectroscopy.py
+++ Quantum-Control-Applications/Superconducting/Two-Flux-Tunable-Transmons/Rapid Prototyping/07_qubit_spectroscopy.py
@@ -1,47 +1,74 @@
+"""
+        QUBIT SPECTROSCOPY
+This sequence involves sending a saturation pulse to the qubit, placing it in a mixed state,
+and then measuring the state of the resonator across various qubit drive intermediate dfs.
+In order to facilitate the qubit search, the qubit pulse duration and amplitude can be changed manually in the QUA
+program directly without having to modify the configuration.
 
-from qm.QuantumMachinesManager import QuantumMachinesManager
+The data is post-processed to determine the qubit resonance frequency, which can then be used to adjust
+the qubit intermediate frequency in the configuration under "center".
+
+Note that it can happen that the qubit is excited by the image sideband or LO leakage instead of the desired sideband.
+This is why calibrating the qubit mixer is highly recommended.
+
+This step can be repeated using the "x180" operation instead of "saturation" to adjust the pulse parameters (amplitude,
+duration, frequency) before performing the next calibration steps.
+
+Prerequisites:
+    - Identification of the resonator's resonance frequency when coupled to the qubit in question (referred to as "resonator_spectroscopy").
+    - Calibration of the IQ mixer connected to the qubit drive line (whether it's an external mixer or an Octave port).
+    - Set the flux bias to the maximum frequency point, labeled as "max_frequency_point", in the state.
+    - Configuration of the saturation pulse amplitude and duration to transition the qubit into a mixed state.
+    - Specification of the expected qubit T1 in the state.
+
+Before proceeding to the next node:
+    - Update the qubit frequency, labeled as f_01, in the state.
+    - Save the current state by calling machine._save("current_state.json")
+"""
+
 from qm.qua import *
+from qm.QuantumMachinesManager import QuantumMachinesManager
 from qm import SimulationConfig
-import matplotlib.pyplot as plt
-from qualang_tools.loops import from_array
-from qualang_tools.results import fetching_tool
-from qualang_tools.plot import interrupt_on_close
-from qualang_tools.results import progress_counter
-from macros import qua_declaration, multiplexed_readout
-from quam import QuAM
 from configuration import *
+from qualang_tools.results import progress_counter, fetching_tool
+from qualang_tools.plot import interrupt_on_close
+from qualang_tools.loops import from_array
+import matplotlib.pyplot as plt
+from macros import qua_declaration, multiplexed_readout
+import warnings
+
+warnings.filterwarnings("ignore")
+
 
 #########################################
 # Set-up the machine and get the config #
 #########################################
 machine = QuAM("current_state.json", flat_data=False)
+
+# Build the config
 config = build_config(machine)
 
-qb1 = machine.qubits[active_qubits[0]]
-qb2 = machine.qubits[active_qubits[1]]
-q1_z = machine.qubits[active_qubits[0]].name + "_z"
-q2_z = machine.qubits[active_qubits[1]].name + "_z"
-rr1 = machine.resonators[active_qubits[0]]
-rr2 = machine.resonators[active_qubits[1]]
+# Get the qubit frequencies (IFs and LOs)
 lo1 = machine.local_oscillators.qubits[qb1.xy.LO_index].freq
 lo2 = machine.local_oscillators.qubits[qb2.xy.LO_index].freq
-
 qb_if_1 = qb1.xy.f_01 - lo1
 qb_if_2 = qb2.xy.f_01 - lo2
 
 ###################
 # The QUA program #
 ###################
-t = 40 * u.ns  # Qubit pulse length
+n_avg = 100  # The number of averages
+# Adjust the pulse duration and amplitude to drive the qubit into a mixed state
+saturation_len = 10 * u.us  # In ns
+saturation_amp = 0.5  # pre-factor to the value defined in the config - restricted to [-2; 2)
 cooldown_time = 5 * max(qb1.T1, qb2.T1)
-
+# Qubit detuning sweep with respect to their resonance frequencies
 dfs = np.arange(-60e6, +80e6, 1e6)
-n_avg = 300
-
 
 with program() as multi_qubit_spec:
+    # Macro to declare I, Q, n and their respective streams for a given number of qubit (defined in macros.py)
     I, I_st, Q, Q_st, n, n_st = qua_declaration(nb_of_qubits=2)
-    df = declare(int)
+    df = declare(int)  # QUA variable for the qubit frequency
 
     # Bring the active qubits to the maximum frequency point
     set_dc_offset(q1_z, "single", qb1.z.max_frequency_point)
@@ -50,19 +77,20 @@ with program() as multi_qubit_spec:
     with for_(n, 0, n < n_avg, n + 1):
         save(n, n_st)
         with for_(*from_array(df, dfs)):
+            # Update the qubit frequencies
             update_frequency(qb1.name + "_xy", df + qb_if_1)
             update_frequency(qb2.name + "_xy", df + qb_if_2)
 
             # qubit 1
-            play("x180" * amp(1), qb1.name + "_xy", duration=t * u.ns)
+            play("cw" * amp(saturation_amp), qb1.name + "_xy", duration=saturation_len * u.ns)
             align(qb1.name + "_xy", rr1.name)
             # qubit 2
-            play("x180" * amp(1), qb2.name + "_xy", duration=t * u.ns)
+            play("cw" * amp(saturation_amp), qb2.name + "_xy", duration=saturation_len * u.ns)
             align(qb2.name + "_xy", rr2.name)
 
-            # readout (reduce amplitude to minimize measurement induced transitions)
-            multiplexed_readout(I, I_st, Q, Q_st, resonators=active_qubits, amplitude=0.9)
-
+            # QUA macro the readout the state of the active resonators (defined in macros.py)
+            multiplexed_readout(I, I_st, Q, Q_st, resonators=active_qubits)
+            # Wait for the qubit to decay to the ground state
             wait(cooldown_time * u.ns)
 
     with stream_processing():
@@ -79,26 +107,36 @@ with program() as multi_qubit_spec:
 #####################################
 qmm = QuantumMachinesManager(machine.network.qop_ip, cluster_name=machine.network.cluster_name, octave=octave_config)
 
-simulate = False
-if simulate:
-    # simulate the test_config QUA program
-    job = qmm.simulate(config, multi_qubit_spec, SimulationConfig(11000))
-    job.get_simulated_samples().con1.plot()
-    plt.show()
-else:
-    # execute QUA:
-    qm = qmm.open_qm(config)
-    job = qm.execute(multi_qubit_spec)
+###########################
+# Run or Simulate Program #
+###########################
 
-    fig = plt.figure()
-    interrupt_on_close(fig, job)
+simulate = False
+
+if simulate:
+    # Simulates the QUA program for the specified duration
+    simulation_config = SimulationConfig(duration=10_000)  # In clock cycles = 4ns
+    job = qmm.simulate(config, multi_qubit_spec, simulation_config)
+    job.get_simulated_samples().con1.plot()
+else:
+    # Open the quantum machine
+    qm = qmm.open_qm(config)
+    # Send the QUA program to the OPX, which compiles and executes it
+    job = qm.execute(multi_qubit_spec)
+    # Get results from QUA program
     results = fetching_tool(job, ["n", "I1", "Q1", "I2", "Q2"], mode="live")
+    # Live plotting
+    fig = plt.figure()
+    interrupt_on_close(fig, job)  # Interrupts the job when closing the figure
     while results.is_processing():
+        # Fetch results
         n, I1, Q1, I2, Q2 = results.fetch_all()
+        # Progress bar
         progress_counter(n, n_avg, start_time=results.start_time)
+        # Convert results into Volts
         s1 = u.demod2volts(I1 + 1j * Q1, rr1.readout_pulse_length)
         s2 = u.demod2volts(I2 + 1j * Q2, rr2.readout_pulse_length)
-        
+        # Plot results
         plt.suptitle("Qubit spectroscopy")
         plt.subplot(221)
         plt.cla()
@@ -123,9 +161,12 @@ else:
         plt.grid("on")
         plt.xlabel(f"{qb1.name} detuning [MHz]")
         plt.tight_layout()
-        plt.pause(2)
+        plt.pause(0.1)
+
     # Close the quantum machines at the end in order to put all flux biases to 0 so that the fridge doesn't heat-up
     qm.close()
+
+    # Fit the results to extract the resonance frequency
     try:
         from qualang_tools.plot.fitting import Fit
 
@@ -149,8 +190,5 @@ else:
         qb2.xy.f_01 = res_2["f"][0] * u.MHz + lo2
     except (Exception,):
         pass
-# qb1.xy.f_01 =
-# qb2.xy.f_01 =
+
 # machine._save("current_state.json")
-
-
--- Quantum-Control-Applications/Superconducting/Two-Flux-Tunable-Transmons/Rapid Prototyping/08_qubit_spectroscopy_vs_flux.py
+++ Quantum-Control-Applications/Superconducting/Two-Flux-Tunable-Transmons/Rapid Prototyping/08_qubit_spectroscopy_vs_flux.py
@@ -1,58 +1,76 @@
+"""
+        QUBIT SPECTROSCOPY VERSUS FLUX
+This sequence involves doing a qubit spectroscopy for several flux biases in order to exhibit the qubit frequency
+versus flux response.
 
-from qm.QuantumMachinesManager import QuantumMachinesManager
+Prerequisites:
+    - Identification of the resonator's resonance frequency when coupled to the qubit in question (referred to as "resonator_spectroscopy").
+    - Calibration of the IQ mixer connected to the qubit drive line (whether it's an external mixer or an Octave port).
+    - Identification of the approximate qubit frequency ("qubit_spectroscopy").
+
+Before proceeding to the next node:
+    - Update the qubit frequency, labeled as "f_01", in the state.
+    - Update the relevant flux points in the state.
+    - Save the current state by calling machine._save("current_state.json")
+"""
+
 from qm.qua import *
+from qm.QuantumMachinesManager import QuantumMachinesManager
 from qm import SimulationConfig
-import matplotlib.pyplot as plt
-from qualang_tools.loops import from_array
-from qualang_tools.results import fetching_tool
-from qualang_tools.plot import interrupt_on_close
-from qualang_tools.results import progress_counter
-from macros import qua_declaration, multiplexed_readout
-from quam import QuAM
 from configuration import *
+from qualang_tools.results import progress_counter, fetching_tool
+from qualang_tools.plot import interrupt_on_close
+from qualang_tools.loops import from_array
+from macros import qua_declaration, multiplexed_readout
+import matplotlib.pyplot as plt
+import warnings
+
+warnings.filterwarnings("ignore")
 
 #########################################
 # Set-up the machine and get the config #
 #########################################
 machine = QuAM("current_state.json")
 
-qb1 = machine.qubits[active_qubits[0]]
-qb2 = machine.qubits[active_qubits[1]]
-rr1 = machine.resonators[active_qubits[0]]
-rr2 = machine.resonators[active_qubits[1]]
+# Get the qubit frequencies (IFs and LOs)
 lo1 = machine.local_oscillators.qubits[qb1.xy.LO_index].freq
 lo2 = machine.local_oscillators.qubits[qb2.xy.LO_index].freq
 qb_if_1 = qb1.xy.f_01 - lo1
 qb_if_2 = qb2.xy.f_01 - lo2
 
+# Build the config
 config = build_config(machine)
 
 ###################
 # The QUA program #
 ###################
+n_avg = 100  # Number of averaging loops
 cooldown_time = 5 * max(qb1.T1, qb2.T1)
-t = 10 * u.us  # Qubit drive
-n_avg = 100
 
+# Adjust the pulse duration and amplitude to drive the qubit into a mixed state
+saturation_len = 10 * u.us  # In ns
+saturation_amp = 0.5  # pre-factor to the value defined in the config - restricted to [-2; 2)
+
+# Qubit detuning sweep with respect to their resonance frequencies
 dfs = np.arange(-50e6, 100e6, 0.1e6)
-dcs = np.linspace(-0.05, 0.05, 40)  # flux
+# Flux bias sweep
+dcs = np.linspace(-0.05, 0.05, 40)
 
-
-
+# Adjust the qubits IFs locally to help find the qubits
 # qb_if_1 = 340e6
 # qb_if_2 = 0
 
 with program() as multi_qubit_spec_vs_flux:
+    # Macro to declare I, Q, n and their respective streams for a given number of qubit (defined in macros.py)
     I, I_st, Q, Q_st, n, n_st = qua_declaration(nb_of_qubits=2)
-    df = declare(int)
-    f_q1 = declare(int)
-    f_q2 = declare(int)
-    dc = declare(fixed)
+    df = declare(int)  # QUA variable for the qubit frequency
+    dc = declare(fixed)  # QUA variable for the flux dc level
 
     with for_(n, 0, n < n_avg, n + 1):
         save(n, n_st)
 
         with for_(*from_array(df, dfs)):
+            # Update the qubit frequencies
             update_frequency(qb1.name + "_xy", df + qb_if_1)
             update_frequency(qb2.name + "_xy", df + qb_if_2)
             with for_(*from_array(dc, dcs)):
@@ -62,12 +80,15 @@ with program() as multi_qubit_spec_vs_flux:
                 wait(100)  # Wait for the flux to settle
 
                 # Saturate qubit
-                # play("cw" * amp(1), qb1.name + "_xy", duration=t * u.ns)
-                # play("cw" * amp(1), qb2.name + "_xy", duration=t * u.ns)
-                play("x180", qb1.name + "_xy")
-                play("x180", qb2.name + "_xy")
-                # readout
-                multiplexed_readout(I, I_st, Q, Q_st, resonators=active_qubits, amplitude=0.9)
+                play("cw" * amp(saturation_amp), qb1.name + "_xy", duration=saturation_len * u.ns)
+                play("cw" * amp(saturation_amp), qb2.name + "_xy", duration=saturation_len * u.ns)
+                # Play x180 once the qubits are found
+                # play("x180", qb1.name + "_xy")
+                # play("x180", qb2.name + "_xy")
+
+                # QUA macro the readout the state of the active resonators (defined in macros.py)
+                multiplexed_readout(I, I_st, Q, Q_st, resonators=active_qubits)
+                # Wait for the qubit to decay to the ground state
                 wait(cooldown_time * u.ns)
 
     with stream_processing():
@@ -84,65 +105,71 @@ with program() as multi_qubit_spec_vs_flux:
 #####################################
 qmm = QuantumMachinesManager(machine.network.qop_ip, cluster_name=machine.network.cluster_name, octave=octave_config)
 
+#######################
+# Simulate or execute #
+#######################
 simulate = False
+
 if simulate:
-    # simulate the test_config QUA program
-    job = qmm.simulate(config, multi_qubit_spec_vs_flux, SimulationConfig(11000))
+    # Simulates the QUA program for the specified duration
+    simulation_config = SimulationConfig(duration=10_000)  # In clock cycles = 4ns
+    job = qmm.simulate(config, multi_qubit_spec_vs_flux, simulation_config)
     job.get_simulated_samples().con1.plot()
-    plt.show()
 else:
-    # qm = qmm.open_qm(config)
-    # job = qm.execute(multi_qubit_spec_vs_flux)
-    from qm.QmJob import QmJob
-    from qualang_tools.multi_user import qm_session
+    # Open the quantum machine
+    qm = qmm.open_qm(config)
+    # Send the QUA program to the OPX, which compiles and executes it
+    job = qm.execute(multi_qubit_spec_vs_flux)
+    # Get results from QUA program
+    results = fetching_tool(job, ["n", "I1", "Q1", "I2", "Q2"], mode="live")
+    # Live plotting
+    fig = plt.figure()
+    interrupt_on_close(fig, job)  #  Interrupts the job when closing the figure
+    while results.is_processing():
+        # Fetch results
+        n, I1, Q1, I2, Q2 = results.fetch_all()
+        # Progress bar
+        progress_counter(n, n_avg, start_time=results.start_time)
+        # Convert results into Volts
+        s1 = u.demod2volts(I1 + 1j * Q1, rr1.readout_pulse_length)
+        s2 = u.demod2volts(I2 + 1j * Q2, rr2.readout_pulse_length)
+        # 2D spectroscopy plot
+        plt.suptitle("Qubit spectroscopy vs flux")
+        plt.subplot(221)
+        plt.cla()
+        plt.pcolor(dcs, (qb_if_1 + dfs) / u.MHz, np.abs(s1))
+        plt.plot(qb1.z.max_frequency_point, qb_if_1 / u.MHz, "r*")
+        plt.xlabel("Flux [V]")
+        plt.ylabel(f"{qb1.name} IF [MHz]")
+        plt.title(f"{qb1.name} (f_01: {int(qb1.xy.f_01 / u.MHz)} MHz)")
+        plt.subplot(223)
+        plt.cla()
+        plt.pcolor(dcs, (qb_if_1 + dfs) / u.MHz, np.unwrap(np.angle(s1)))
+        plt.plot(qb1.z.max_frequency_point, qb_if_1 / u.MHz, "r*")
+        plt.xlabel("Flux [V]")
+        plt.ylabel(f"{qb1.name} IF [MHz]")
+        plt.subplot(222)
+        plt.cla()
+        plt.pcolor(dcs, (qb_if_2 + dfs) / u.MHz, np.abs(s2))
+        plt.plot(qb2.z.max_frequency_point, qb_if_2 / u.MHz, "r*")
+        plt.title(f"{qb2.name} (f_01: {int(qb2.xy.f_01 / u.MHz)} MHz)")
+        plt.ylabel(f"{qb2.name} IF [MHz]")
+        plt.xlabel("flux [V]")
+        plt.subplot(224)
+        plt.cla()
+        plt.pcolor(dcs, (qb_if_2 + dfs) / u.MHz, np.unwrap(np.angle(s2)))
+        plt.plot(qb2.z.max_frequency_point, qb_if_2 / u.MHz, "r*")
+        plt.xlabel("Flux [V]")
+        plt.ylabel(f"{qb2.name} IF [MHz]")
+        plt.tight_layout()
+        plt.pause(0.1)
 
-    with qm_session(qmm, config, timeout=100) as qm:
-        job: QmJob = qm.execute(multi_qubit_spec_vs_flux)
-
-        fig = plt.figure()
-        interrupt_on_close(fig, job)
-        results = fetching_tool(job, ["n", "I1", "Q1", "I2", "Q2"], mode="live")
-        while results.is_processing():
-            n, I1, Q1, I2, Q2 = results.fetch_all()
-            progress_counter(n, n_avg, start_time=results.start_time)
-
-            s1 = u.demod2volts(I1 + 1j * Q1, rr1.readout_pulse_length)
-            s2 = u.demod2volts(I2 + 1j * Q2, rr2.readout_pulse_length)
-
-            plt.suptitle("Qubit spectroscopy vs flux")
-            plt.subplot(221)
-            plt.cla()
-            plt.pcolor(dcs, (qb_if_1 + dfs) / u.MHz, np.abs(s1))
-            plt.plot(qb1.z.max_frequency_point, qb_if_1 / u.MHz, "r*")
-            plt.xlabel("Flux [V]")
-            plt.ylabel(f"{qb1.name} IF [MHz]")
-            plt.title(f"{qb1.name} (f_01: {int(qb1.xy.f_01 / u.MHz)} MHz)")
-            plt.subplot(223)
-            plt.cla()
-            plt.pcolor(dcs, (qb_if_1 + dfs) / u.MHz, np.unwrap(np.angle(s1)))
-            plt.plot(qb1.z.max_frequency_point, qb_if_1 / u.MHz, "r*")
-            plt.xlabel("Flux [V]")
-            plt.ylabel(f"{qb1.name} IF [MHz]")
-            plt.subplot(222)
-            plt.cla()
-            plt.pcolor(dcs, (qb_if_2 + dfs) / u.MHz, np.abs(s2))
-            plt.plot(qb2.z.max_frequency_point, qb_if_2 / u.MHz, "r*")
-            plt.title(f"{qb2.name} (f_01: {int(qb2.xy.f_01 / u.MHz)} MHz)")
-            plt.ylabel(f"{qb2.name} IF [MHz]")
-            plt.xlabel("flux [V]")
-            plt.subplot(224)
-            plt.cla()
-            plt.pcolor(dcs, (qb_if_2 + dfs) / u.MHz, np.unwrap(np.angle(s2)))
-            plt.plot(qb2.z.max_frequency_point, qb_if_2 / u.MHz, "r*")
-            plt.xlabel("Flux [V]")
-            plt.ylabel(f"{qb2.name} IF [MHz]")
-            plt.tight_layout()
-            plt.pause(1)
     # Close the quantum machines at the end in order to put all flux biases to 0 so that the fridge doesn't heat-up
     # qm.close()
+
 # Set the relevant flux points
-# qb1.z. =
-# qb2.z. =
+# qb1.z.max_frequency_point =
+# qb1.z.min_frequency_point =
 # machine._save("quam_bootstrap_state.json")
 
 
