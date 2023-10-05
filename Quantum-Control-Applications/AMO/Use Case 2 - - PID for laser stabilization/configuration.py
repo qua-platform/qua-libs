c478648a5b1621f9c5faf69efc19c36a8443afce
TheoQM <theo@quantum-machines.co>
2023-10-05 10:30:29 +0200

    first commit

---
+++ Quantum-Control-Applications/AMO/Use Case 2 - - PID for laser stabilization/configuration.py
@@ -0,0 +1,136 @@
+import numpy as np
+
+######################
+# AUXILIARY FUNCTIONS:
+######################
+
+
+def gauss(amplitude, mu, sigma, length):
+    t = np.linspace(-length / 2, length / 2, length)
+    gauss_wave = amplitude * np.exp(-((t - mu) ** 2) / (2 * sigma**2))
+    return [float(x) for x in gauss_wave]
+
+
+######################
+# Network parameters #
+######################
+qop_ip = "172.16.33.100"  # Write the QM router IP address
+cluster_name = "Cluster_83"  # Write your cluster_name if version >= QOP220
+qop_port = None  # Write the QOP port if version < QOP220
+
+
+################
+# CONFIGURATION:
+################
+
+# AOM
+AOM_IF = 63e6
+const_amplitude = 0.1
+const_len = 100
+
+# Noise
+noise_IF = 200e3
+noise_amplitude = 0.01
+noise_len = 100
+
+# Photo-diode
+readout_len = 100
+time_of_flight = 192
+
+
+config = {
+    "version": 1,
+    "controllers": {
+        "con1": {
+            "analog_outputs": {
+                1: {"offset": 0.0},
+                2: {"offset": 0.0},
+            },
+            "digital_outputs": {
+                1: {},
+            },
+            "analog_inputs": {
+                1: {"offset": 0.0},
+            },
+        }
+    },
+    "elements": {
+        "AOM": {
+            "singleInput": {
+                "port": ("con1", 1),
+            },
+            "intermediate_frequency": AOM_IF,
+            "operations": {
+                "cw": "cw_pulse",
+            },
+        },
+        "noise": {
+            "singleInput": {
+                "port": ("con1", 1),
+            },
+            "intermediate_frequency": noise_IF,
+            "operations": {
+                "cw": "noise_pulse",
+            },
+        },
+        "photo-diode": {
+            "singleInput": {
+                "port": ("con1", 2),
+            },
+            "operations": {
+                "readout": "readout_pulse",
+            },
+            "digitalInputs": {
+                "trigger": {
+                    "port": ("con1", 1),
+                    "delay": 136,
+                    "buffer": 0,
+                }
+            },
+            "outputs": {
+                "out1": ("con1", 1),
+            },
+            "time_of_flight": time_of_flight,
+            "smearing": 0,
+        },
+    },
+    "pulses": {
+        "cw_pulse": {
+            "operation": "control",
+            "length": const_len,
+            "waveforms": {
+                "single": "const_wf",
+            },
+        },
+        "noise_pulse": {
+            "operation": "control",
+            "length": noise_len,
+            "waveforms": {
+                "single": "noise_wf",
+            },
+        },
+        "readout_pulse": {
+            "operation": "measurement",
+            "length": readout_len,
+            "waveforms": {
+                "single": "zero_wf",
+            },
+            "integration_weights": {
+                "constant": "constant_weights",
+            },
+            "digital_marker": "ON",
+        },
+    },
+    "waveforms": {
+        "const_wf": {"type": "constant", "sample": const_amplitude},
+        "noise_wf": {"type": "constant", "sample": noise_amplitude},
+        "zero_wf": {"type": "constant", "sample": 0.0},
+    },
+    "digital_waveforms": {"ON": {"samples": [(1, 0)]}},
+    "integration_weights": {
+        "constant_weights": {
+            "cosine": [(1.0, readout_len)],
+            "sine": [(0.0, readout_len)],
+        },
+    },
+}
---
+++ Quantum-Control-Applications/AMO/Use Case 2 - - PID for laser stabilization/laser_stabilization_PID.py
@@ -0,0 +1,154 @@
+"""
+A simple sandbox to showcase different QUA functionalities during the installation.
+"""
+
+from qm.qua import *
+from qm.QuantumMachinesManager import QuantumMachinesManager
+from qm import SimulationConfig
+from configuration import *
+import matplotlib.pyplot as plt
+import warnings
+
+warnings.filterwarnings("ignore")
+
+
+###################
+# The QUA program #
+###################
+target = 0.008
+gain_P = 2.55
+gain_I = -0.1
+gain_D = 0.6 * 0
+# gain_I_bitshift=0
+# integral_size=10
+
+alpha = 0.1
+N_shots = 12000
+
+with program() as PID_prog:
+    # adc_st = declare_stream(adc_trace=True)
+    n = declare(int)
+    integ = declare(fixed)
+    integral_st = declare_stream()
+    amplitude = declare(fixed, value=1.0)
+    error = declare(fixed, value=0)
+    error_st = declare_stream()
+    amp_st = declare_stream()
+    integrator_error = declare(fixed, value=0)
+    integrator_error_st = declare_stream()
+    old_error = declare(fixed, value=0.0)
+    derivative_error = declare(fixed)
+    total_error = declare(fixed)
+    total_error_st = declare_stream()
+
+    with for_(n, 0, n < N_shots, n + 1):
+        # start noise only after 1/4 of the shots
+        # The noise is a different element outputting to the same ports and adding a 100kHz or so noise signal
+        # on the gaussians. since the pulse processors are phase coherent from shot to shot then the phase of the kHz signal
+        # is preserved shot to shot and so we make slow, kHz noise, on a train of gaussians which are 100ns long
+        with if_(n > N_shots / 4):
+            play("cw", "noise")
+
+        # play the gaussian pulse and measure the intensity on an envelope detector
+        # we are playing DC from the OPX and modulating the IQ mixer like a vector modulator (i.e. modulating the LO directly to make 100ns gaussians)
+        # the signal is acquired on an envelope detector at the RF port of the mixer and goes into the OPX
+        play("cw" * amp(amplitude), "AOM")
+        measure("readout", "photo-diode", None, integration.full("constant", integ, "out1"))
+
+        # calculate the error
+        assign(error, (target - integ) << 11)
+        # calculate the integrator error with exponentially decreasing weights with coefficient alpha
+        assign(integrator_error, (1 - alpha) * integrator_error + alpha * error)
+        # calculate the derivative error
+        assign(derivative_error, old_error - error)
+        # start the lock at first then stop to show the noise without lock, then lock on the noise. then keep lock and remove noise to compare the noise suppression to the no noise case and see if we are detection limited
+
+        # apply correction during the first 1/4 of shots just to show that it locks to a different sepoint quickly
+        # lower the gain_P to see that the lock becomes softer
+        with if_(n < int(N_shots / 4)):
+            assign(amplitude, amplitude + (gain_P * error + gain_I * integrator_error + gain_D * derivative_error))
+        # stop the lock while the noise is introduced for another 1/4 of shots to show the noise without lock
+        # start the lock again after 1/2 the shots and see the performance
+        with if_(n > int(N_shots / 2)):
+            assign(amplitude, amplitude + (gain_P * error + gain_I * integrator_error + gain_D * derivative_error))
+
+        # save old error to be error
+        assign(old_error, error)
+
+        save(integ, integral_st)
+        save(error, error_st)
+        save(amplitude, amp_st)
+        save(total_error, total_error_st)
+        save(integrator_error, integrator_error_st)
+
+    with stream_processing():
+        integral_st.save_all("integral")
+        error_st.save_all("error")
+        amp_st.save_all("amplitude")
+        integrator_error_st.save_all("integrator_error")
+        total_error_st.save_all("total_error")
+
+#####################################
+#  Open Communication with the QOP  #
+#####################################
+qmm = QuantumMachinesManager(host=qop_ip, cluster_name=cluster_name)
+
+###########################
+# Run or Simulate Program #
+###########################
+
+simulate = True
+
+if simulate:
+    # Simulates the QUA program for the specified duration
+    simulation_config = SimulationConfig(duration=10_000)  # In clock cycles = 4ns
+    # Simulate blocks python until the simulation is done
+    job = qmm.simulate(config, PID_prog, simulation_config)
+    # Plot the simulated samples
+    job.get_simulated_samples().con1.plot()
+else:
+    # Open a quantum machine to execute the QUA program
+    qm = qmm.open_qm(config)
+    # Send the QUA program to the OPX, which compiles and executes it - Execute does not block python!
+    job = qm.execute(PID_prog)
+    #
+    job.result_handles.wait_for_all_values()
+    res = job.result_handles
+    res.wait_for_all_values()
+    # adc1 = res.get("adc1").fetch_all()
+    error = res.get("error").fetch_all()["value"]
+    total_error = res.get("total_error").fetch_all()["value"]
+    integral = res.get("integral").fetch_all()["value"]
+    amplitude = res.get("amplitude").fetch_all()["value"]
+    integrator_error = res.get("integrator_error").fetch_all()["value"]
+
+    # I calculated on the scope 0.38us between pulses
+    Time = [x * 0.38 * 0 + x for x in range(N_shots)]
+    # just checking the std after the lock
+    # print(integral[N_shots:-1].std())
+
+    # plotting stuff
+
+    plt.figure(figsize=(20, 16))
+    plt.plot(Time, error, ".-", linewidth=5, markersize=10)
+    plt.title("Intensity lock error", fontsize=40)
+    plt.xlabel("Time [μs]", fontsize=40)
+    plt.ylabel("Amplitude Error [arb. units]", fontsize=40)
+    plt.xticks(fontsize=30)
+    plt.yticks(fontsize=30)
+
+    # plt.figure(figsize=(20,16))
+    # plt.plot(Time, integral)
+    # plt.title('Intensity')
+    plt.figure(figsize=(20, 16))
+    plt.plot(Time, amplitude)
+    plt.title("Applied amplitude")
+    # plt.figure(figsize=(20,16))
+    # plt.plot(Time, integrator_error)
+    # plt.title('integrator error')
+    # plt.figure(figsize=(20,16))
+    # plt.plot(Time, total_error)
+    # plt.title('total error')
+
+    # plt.figure()
+    # plt.plot(adc1)
