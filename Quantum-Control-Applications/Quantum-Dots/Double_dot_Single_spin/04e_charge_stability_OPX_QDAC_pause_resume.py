31946e372cbb1815e69f678f200869e44772491a
Kevin Villegas <kevin@quantum-machines.co>
2023-11-13 21:39:45 -0500

    qdac_opx_puase_resume

--- Quantum-Control-Applications/Quantum-Dots/Double_dot_Single_spin/04e_charge_stability_OPX_QDAC_pause_resume.py
+++ Quantum-Control-Applications/Quantum-Dots/Double_dot_Single_spin/04e_charge_stability_OPX_QDAC_pause_resume.py
@@ -20,27 +20,34 @@ n_avg = 100
 n_points_slow = 10
 n_points_fast = 10
 
+voltage_values_slow = list(np.linspace(-1.5, 1.5, n_points_slow))
+voltage_values_fast = list(np.linspace(-1.5, 1.5, n_points_fast))
+
+
 with program() as charge_stability:
     n = declare(int)  # QUA integer used as an index for the averaging loop
     i = declare(int)  # QUA integer used as an index to loop over the voltage points
     j = declare(int)  # QUA integer used as an index to loop over the voltage points
     I = declare(fixed)  # QUA fixed used to store the outcome of the readout
     I_st = declare_stream()  # Data stream used to process and transfer the saved results
+    Q = declare(fixed)  # QUA fixed used to store the outcome of the readout
+    Q_st = declare_stream()  # Data stream used to process and transfer the saved results
     n_st = declare_stream()
 
     with for_(i, 0, i < n_points_slow + 1, i + 1):
-
-        pause()
 
         with for_(j, 0, j < n_points_fast, j + 1):
 
             pause()
 
+            wait(10 * u.ms, 'charge_sensor_RF')
+
             with for_(n, 0, n < n_avg, n + 1):  # The averaging loop
                 # Readout: the voltage measured by the analog input 1 is recorded and the integrated result is stored in "I"
-                measure('readout', 'charge_sensor_DC', None, integration.full('cos', I, 'out1'))
+                measure('readout', 'charge_sensor_RF', None, demod.full('cos', I, 'out2'), demod.full('sin', Q, 'out2'))
                 # Transfer the results from the OPX FPGA to its processor
                 save(I, I_st)
+                save(Q, Q_st)
                 # Wait at each iteration in order to ensure that the data will not be transfered faster than 1 sample per µs to the stream processing.
                 # Otherwise the processor will recieve the samples fatser than it can process them which can cause the OPX to crash.
                 wait(1_000 * u.ns)  # in ns
@@ -51,6 +58,7 @@ with program() as charge_stability:
     with stream_processing():
         # The points sent to "I_st" are grouped in a 1D buffer and then averaged together
         I_st.buffer(n_avg).map(FUNCTIONS.average()).buffer(n_points_fast).save_all("I")
+        Q_st.buffer(n_avg).map(FUNCTIONS.average()).buffer(n_points_fast).save_all("Q")
         n_st.save_all("iteration")
 
 
@@ -77,41 +85,72 @@ def wait_until_job_is_paused(current_job):
 
 # Open the quantum machine
 qm = qmm.open_qm(config)
+### QDAC2 section
+# TODO: ask what channels to use
+qdac_channel_fast = 
+qdac_channel_slow = 
+# Set the channel output range
+qdac.write(f"sour{qdac_channel_fast}:rang low")
+# Set the channel output filter
+qdac.write(f"sour{qdac_channel_fast}:filt med")
+# Set the slew rate in V/s to avoid transients when abruptly stepping the voltage -Must be within [0.01; 2e7] V/s
+qdac.write(f"sour{qdac_channel_fast}:dc:volt:slew {1000}")
+qdac.write(f"sour{qdac_channel_fast}:volt:mode fix")
+# Set the channel output range
+qdac.write(f"sour{qdac_channel_slow}:rang low")
+# Set the channel output filter
+qdac.write(f"sour{qdac_channel_slow}:filt med")
+# Set the slew rate in V/s to avoid transients when abruptly stepping the voltage -Must be within [0.01; 2e7] V/s
+qdac.write(f"sour{qdac_channel_slow}:dc:volt:slew {1000}")
+qdac.write(f"sour{qdac_channel_slow}:volt:mode fix")
+
+
 # Send the QUA program to the OPX, which compiles and executes it
 job = qm.execute(charge_stability)
 # Creates results handles to fetch the data
 res_handles = job.result_handles
 I_handle = res_handles.get("I")
+Q_handle = res_handles.get("Q")
 n_handle = res_handles.get("iteration")
 # Initialize empty vectors to store the global 'I' & 'Q' results
 I_tot = []
+Q_tot = []
 # Live plotting
 fig = plt.figure()
 interrupt_on_close(fig, job)  # Interrupts the job when closing the figure
 for i in range(n_points_slow):  # Loop over y-voltages
     # Set voltage
-    # Resume the QUA program (escape the 'pause' statement)
-    job.resume()
-    # Wait until the program reaches the 'pause' statement again, indicating that the QUA program is done
-    wait_until_job_is_paused(job)
+    # Update the QDAC level
+    qdac.write(f"sour{qdac_channel_slow}:volt {voltage_values_slow[i]}")
+
     for j in range(n_points_fast):  # Loop over x-voltages
         # Set voltage
         # Resume the QUA program (escape the 'pause' statement)
+        qdac.write(f"sour{qdac_channel_fast}:volt {voltage_values_fast[i]}")
+
         job.resume()
         # Wait until the program reaches the 'pause' statement again, indicating that the QUA program is done
         wait_until_job_is_paused(job)
+        
     # Wait until the data of this run is processed by the stream processing
     I_handle.wait_for_values(i + 1)
+    Q_handle.wait_for_values(i + 1)
     n_handle.wait_for_values(i + 1)
     # Fetch the data from the last OPX run corresponding to the current LO frequency
     I = np.concatenate(I_handle.fetch(i)["value"])
+    Q = np.concatenate(Q_handle.fetch(i)["value"])
     iteration = n_handle.fetch(i)["value"][0]
     # Update the list of global results
     I_tot.append(I)
+    Q_tot.append(Q)
     # Progress bar
     progress_counter(iteration, n_points_slow)
+    plt.subplot(121)
     plt.cla()
-    plt.pcolor(n_points_fast, n_points_slow, I_tot)
+    plt.pcolor(n_points_fast, n_points_slow[:len(I_tot)], I_tot)
+    plt.subplot(122)
+    plt.cla()
+    plt.pcolor(n_points_fast, n_points_slow[:len(Q_tot)], Q_tot)
     plt.pause(0.1)
 
 # Interrupt the FPGA program
