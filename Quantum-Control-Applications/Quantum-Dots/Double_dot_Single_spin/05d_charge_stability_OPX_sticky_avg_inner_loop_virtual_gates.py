eb7ad34a15be807249fd1c6630930b3f4fb68b8b
Dean Poulos <dean.poulos7@gmail.com>
2023-11-20 17:14:30 +1100

    Update virtual gate charge stability diagram based on experimental testing.

--- Quantum-Control-Applications/Quantum-Dots/Double_dot_Single_spin/05d_charge_stability_OPX_sticky_avg_inner_loop_virtual_gates.py
+++ Quantum-Control-Applications/Quantum-Dots/Double_dot_Single_spin/05d_charge_stability_OPX_sticky_avg_inner_loop_virtual_gates.py
@@ -1,7 +1,12 @@
-# %%
 """
         CHARGE STABILITY DIAGRAM
 """
+
+from importlib import reload
+import configuration
+
+reload(configuration)
+from configuration import *
 
 from qm.qua import *
 from qm.QuantumMachinesManager import QuantumMachinesManager
@@ -12,84 +17,64 @@ from qualang_tools.loops import from_array
 from qualang_tools.results import fetching_tool, progress_counter
 from qualang_tools.plot import interrupt_on_close
 
+#%matplotlib qt
+
 ###################
 # The QUA program #
 ###################
 
-n_avg = 100  # Number of averaging loops
+n_avg = 10  # Number of averaging loops
 
-origin_P2 = 0.0
-origin_P1 = 0.0 
-intercept_U = 0.0
-intercept_E = 0.0
+offset_min_P1 = -0.04
+offset_max_P1 = 0.04
+d_offset_P1 = 0.001
+offsets_P1 = np.arange(offset_min_P1, offset_max_P1, d_offset_P1)
 
-offset_min_E = -0.02
-offset_max_E = 0.02
-d_offset_E = 0.001
-offsets_E = np.arange(offset_min_E, offset_max_E, d_offset_E) + origin_P2
-
-offset_min_U = -0.02
-offset_max_U = 0.02
-d_offset_U = 0.001
-offsets_U = np.arange(offset_min_U, offset_max_U, d_offset_U) + origin_P2
-
-def play_detuning(E):
-    play('bias'*amp((- E - intercept_E)/P1_amp), 'P1_sticky')
-    play('bias'*amp(E/P2_amp), 'P2_sticky')
-
-def play_energy(U):
-    play('bias'*amp((- U - intercept_U)/P1_amp), 'P1_sticky')
-    play('bias'*amp(U/P2_amp), 'P2_sticky')
-
-def measure_RF_DC():
-    measure('readout', 'charge_sensor_RF', None, demod.full('cos', I, 'out2'), demod.full('sin', Q, 'out2'))
-    measure('readout', 'charge_sensor_DC', None, integration.full('cos', IDC, 'out1'))
-    save(I, I_st)
-    save(IDC, IDC_st)
-    save(Q, Q_st)
+offset_min_P2 = -0.04
+offset_max_P2 = 0.04
+d_offset_P2 = 0.001
+offsets_P2 = np.arange(offset_min_P2, offset_max_P2, d_offset_P2)
 
 with program() as charge_stability:
-    
-    dc_E = declare(fixed)
-    dc_U = declare(fixed)
+    dc_p1 = declare(fixed)
+    dc_p2 = declare(fixed)
     n = declare(int)
     I = declare(fixed)
     I_st = declare_stream()
-    IDC = declare(fixed)
-    IDC_st = declare_stream()
     Q = declare(fixed)
     Q_st = declare_stream()
     counter = declare(int, value=0)
     counter_st = declare_stream()
 
-    play_detuning(offset_min_E)
-    # -> add wait(5*tau * u.ns, 'P1_sticky') // to avoid transients due to the large change of voltage
+    play('bias' * amp(offset_min_P1 / P1_amp), 'P1_sticky')  # -2 to 2
+    wait(10 * u.ms, 'P1_sticky')
 
-    with for_(*from_array(dc_U, offsets_U)):
+    with for_(*from_array(dc_p1, offsets_P1)):
         save(counter, counter_st)
-        play_energy(offset_min_U)
-        # -> add wait(5*tau * u.ns, 'P2_sticky') // to avoid transients due to the large change of voltage
+        play('bias' * amp(offset_min_P2 / P2_amp), 'P2_sticky')
+        wait(10 * u.ms, 'P2_sticky')
 
-        align('P1_sticky', 'P2_sticky', 'charge_sensor_RF', 'charge_sensor_DC')
-        with for_(*from_array(dc_E, offsets_E)):
-            play_energy(d_offset_U)
+        align('P1_sticky', 'P2_sticky', 'charge_sensor_RF')
+        with for_(*from_array(dc_p2, offsets_P2)):
+            play('bias' * amp(d_offset_P2 / P2_amp), 'P2_sticky')
+            wait(10 * u.ms, 'P2_sticky')
 
-            align('P1_sticky', 'P2_sticky', 'charge_sensor_RF', 'charge_sensor_DC')
-            with for_(n, 0, n < n_avg, n+1):
-                measure_RF_DC()
+            align('P1_sticky', 'P2_sticky', 'charge_sensor_RF')
+            with for_(n, 0, n < n_avg, n + 1):
+                measure('readout', 'charge_sensor_RF', None, demod.full('cos', I, 'out2'), demod.full('sin', Q, 'out2'))
+                save(I, I_st)
+                save(Q, Q_st)
+        align('P1_sticky', 'P2_sticky', 'charge_sensor_RF')
+        ramp_to_zero('P2_sticky', 10 * u.ms)
+        play('bias' * amp(d_offset_P1 / P1_amp), 'P1_sticky')
+        wait(10 * u.ms, 'P1_sticky')
+        assign(counter, counter + 1)
 
-        align('P1_sticky', 'P2_sticky', 'charge_sensor_RF', 'charge_sensor_DC')
-        ramp_to_zero('P2_sticky')
-        # -> add wait(5*tau * u.ns, 'P2_sticky') // to avoid transients due to the large change of voltage
-        play_detuning(d_offset_E)
-        assign(counter, counter+1)
-
-    ramp_to_zero('P1_sticky')
+    ramp_to_zero('P1_sticky', 10 * u.ms)
 
     with stream_processing():
-        I_st.buffer(n_avg).map(FUNCTIONS.average()).buffer(len(offsets_E)).save_all('I')
-        IDC_st.buffer(n_avg).map(FUNCTIONS.average()).buffer(len(offsets_E)).save_all('IDC')
-        Q_st.buffer(n_avg).map(FUNCTIONS.average()).buffer(len(offsets_E)).save_all('Q')
+        I_st.buffer(n_avg).map(FUNCTIONS.average()).buffer(len(offsets_P2)).save_all('I')
+        Q_st.buffer(n_avg).map(FUNCTIONS.average()).buffer(len(offsets_P2)).save_all('Q')
         counter_st.save('counter')
 
 #####################################
@@ -116,33 +101,29 @@ else:
     # Send the QUA program to the OPX, which compiles and executes it
     job = qm.execute(charge_stability)
     # Get results from QUA program
-    my_results = fetching_tool(job, data_list=['counter', 'I', 'Q', 'IDC'], mode="live")
+    my_results = fetching_tool(job, data_list=['counter', 'I', 'Q'], mode="live")
     # Live plotting
-    fig = plt.figure()
+    fig = plt.figure(figsize=(10, 4))
     interrupt_on_close(fig, job)  # Interrupts the job when closing the figure
     while my_results.is_processing():
-        counter, I, Q, IDC = my_results.fetch_all()
+        counter, I, Q = my_results.fetch_all()
         I_volts = u.demod2volts(I, reflectometry_readout_length)
-        IDC_volts = u.demod2volts(IDC, readout_len)
         Q_volts = u.demod2volts(Q, reflectometry_readout_length)
-        progress_counter(counter, len(offsets_U), start_time=my_results.get_start_time())
-        plt.subplot(131)
+        progress_counter(counter, len(offsets_P1), start_time=my_results.get_start_time())
+        plt.subplot(121)
         plt.cla()
-        plt.pcolor(offsets_U, offsets_E[:len(I_volts)], I_volts)
+        plt.pcolor(offsets_P2, offsets_P1[:len(I_volts)], I_volts)
         plt.xlabel('P2 [V]')
         plt.ylabel('P1 [V]')
-        plt.subplot(132)
-        plt.cla()
-        plt.pcolor(offsets_U, offsets_E[:len(Q_volts)], Q_volts)
-        plt.xlabel('P2 [V]')
-        plt.ylabel('P1 [V]')
-        plt.subplot(133)
-        plt.cla()
-        plt.pcolor(offsets_U, offsets_E[:len(IDC_volts)], IDC_volts)
-        plt.xlabel('P2 [V]')
-        plt.ylabel('P1 [V]')
-        plt.tight_layout()
         plt.pause(0.1)
+        plt.subplot(122)
+        plt.cla()
+        plt.pcolor(offsets_P2, offsets_P1[:len(Q_volts)], Q_volts)
+        plt.xlabel('P2 [V]')
+        plt.ylabel('P1 [V]')
+        plt.pause(0.1)
+    plt.subplot(121)
+    plt.colorbar()
+    plt.subplot(122)
     plt.colorbar()
     plt.show()
-# %%
