03e67741e7e72fec5a396a42be99d34d494a946a
TheoQM <theo@quantum-machines.co>
2023-12-08 17:53:35 +0100

    clean files

--- Quantum-Control-Applications/Quantum-Dots/Single_Spin_EDSR_theo/qdac1_driver.py
+++
@@ -1,8 +0,0 @@
-######################################
-# pip install qcodes_contrib_drivers #
-######################################
-from qcodes_contrib_drivers.drivers.QDevil.QDAC1 import QDac, Mode
-
-# then, e.g.,
-# address = 'ASRL2::INSTR'
-# qdac = QDac(name='qdac', address=address, update_currents=False)
