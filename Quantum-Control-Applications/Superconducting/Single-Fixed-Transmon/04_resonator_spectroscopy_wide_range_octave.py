dd5069b408069037ff3bed894a83a882ed8e36e1
TheoQM <theo@quantum-machines.co>
2024-02-02 16:54:37 +0100

    Tested files

--- Quantum-Control-Applications/Superconducting/Single-Fixed-Transmon/04_resonator_spectroscopy_wide_range_octave.py
+++ Quantum-Control-Applications/Superconducting/Single-Fixed-Transmon/04_resonator_spectroscopy_wide_range_octave.py
@@ -1,17 +1,21 @@
 """
-        QUBIT SPECTROSCOPY OVER A WIDE RANGE with the Octave
-This procedure conducts a broad 1D frequency sweep of the qubit, measuring the resonator while sweeping the Octave
-LO frequency simultaneously. The Octave LO frequency is swept in the outer loop to optimize run time.
+        RESONATOR SPECTROSCOPY OVER A WIDE RANGE with the Octave
+This sequence involves measuring the resonator by sending a readout pulse and demodulating the signals to extract the
+'I' and 'Q' quadratures across varying readout intermediate and LO frequencies. The Octave LO frequency is swept in the
+outer loop to optimize run time.
 The Octave port can be calibrated at all the involved LO frequencies and at the median intermediate frequency.
 The correction parameters are updated in Python after setting the LO frequency.
+The data is then post-processed to determine the resonator resonance frequency.
+This frequency can be used to update the readout intermediate frequency in the configuration under "resonator_IF".
 
 Prerequisites:
-    -Identification of the resonator's resonance frequency when coupled to the qubit being studied (referred to as "resonator_spectroscopy").
-    -Calibration of the IQ mixer connected to the qubit drive line (be it an external mixer or an Octave port).
-    -Configuration of the saturation pulse amplitude and duration to transition the qubit into a mixed state.
+    - Ensure calibration of the time of flight, offsets, and gains (referenced as "time_of_flight").
+    - Calibrate the IQ mixer connected to the readout line (whether it's an external mixer or an Octave port).
+    - Define the readout pulse amplitude and duration in the configuration.
+    - Specify the expected resonator depletion time in the configuration.
 
 Before proceeding to the next node:
-    -Adjust the qubit frequency settings, labeled as "qubit_IF" and "qubit_LO", in the configuration.
+    - Update the readout frequencies, labeled as "resonator_IF" and "resonator_LO", in the configuration.
 """
 
 from qm.qua import *
@@ -21,6 +25,7 @@ from qualang_tools.results import progress_counter, wait_until_job_is_paused
 from qualang_tools.plot import interrupt_on_close
 from qualang_tools.loops import from_array
 import matplotlib.pyplot as plt
+from scipy.signal import detrend
 
 
 ###################
@@ -29,12 +34,12 @@ import matplotlib.pyplot as plt
 
 n_avg = 100  # The number of averages
 # The intermediate frequency sweep parameters
-f_min = 1 * u.MHz
-f_max = 251 * u.MHz
+f_min = 21 * u.MHz
+f_max = 271 * u.MHz
 df = 500 * u.kHz
 IFs = np.arange(f_min, f_max + 0.1, df)  # The intermediate frequency vector (+ 0.1 to add f_max to IFs)
 # This is to make sure that the center IF is the one used in the config for the correction parameters to be updated.
-config["elements"]["qubit"]["intermediate_frequency"] = IFs[len(IFs) // 2]
+config["elements"]["resonator"]["intermediate_frequency"] = IFs[len(IFs) // 2]
 
 # The LO frequency sweep parameters
 f_min_lo = 4.0e9
@@ -58,12 +63,7 @@ with program() as qubit_spec:
         with for_(n, 0, n < n_avg, n + 1):
             with for_(*from_array(f, IFs)):
                 # Update the frequency of the digital oscillator linked to the qubit element
-                update_frequency("qubit", f)
-                # Play the saturation pulse to put the qubit in a mixed state
-                play("saturation", "qubit")
-                # Align the two elements to measure after playing the qubit pulse.
-                # One can also measure the resonator while driving the qubit (2-tone spectroscopy) by commenting the 'align'
-                align("qubit", "resonator")
+                update_frequency("resonator", f)
                 # Measure the state of the resonator
                 measure(
                     "readout",
@@ -73,7 +73,7 @@ with program() as qubit_spec:
                     dual_demod.full("minus_sin", "out1", "cos", "out2", Q),
                 )
                 # Wait for the qubit to decay to the ground state
-                wait(thermalization_time * u.ns, "resonator")
+                wait(depletion_time * u.ns, "resonator")
                 # Save the 'I' & 'Q' quadratures to their respective streams
                 save(I, I_st)
                 save(Q, Q_st)
@@ -104,7 +104,7 @@ calibrate = True
 if calibrate:
     for lo in LOs:
         print(f"Calibrate (LO, IF) = ({lo/u.MHz}, {IFs[len(IFs) // 2]/u.MHz}) MHz")
-        qm.calibrate_element("qubit", {lo: (IFs[len(IFs) // 2],)})
+        qm.calibrate_element("resonator", {lo: (IFs[len(IFs) // 2],)})
 
 # Send the QUA program to the OPX, which compiles and executes it. It will stop at the 'pause' statement.
 job = qm.execute(qubit_spec)
@@ -119,8 +119,8 @@ interrupt_on_close(fig, job)  # Interrupts the job when closing the figure
 
 for i in range(len(LOs)):  # Loop over the LO frequencies
     # Set the frequency of the LO source
-    qm.octave.set_lo_frequency("qubit", LOs[i])
-    qm.octave.set_element_parameters_from_calibration_db("qubit", job)
+    qm.octave.set_lo_frequency("resonator", LOs[i])
+    qm.octave.set_element_parameters_from_calibration_db("resonator", job)
 
     # Resume the QUA program (escape the 'pause' statement)
     job.resume()
@@ -138,7 +138,7 @@ for i in range(len(LOs)):  # Loop over the LO frequencies
     # Convert results into Volts
     S = u.demod2volts(I + 1j * Q, readout_len)
     R = np.abs(S)  # Amplitude
-    phase = np.angle(S)  # Phase
+    phase = np.unwrap(np.angle(S))  # Phase
     # Plot results
     plt.suptitle("Qubit spectroscopy")
     ax1 = plt.subplot(211)
@@ -146,7 +146,7 @@ for i in range(len(LOs)):  # Loop over the LO frequencies
     plt.xlabel("qubit frequency [MHz]")
     plt.ylabel(r"$\sqrt{I^2 + Q^2}$ [V]")
     plt.subplot(212, sharex=ax1)
-    plt.plot((IFs + LOs[i]) / u.MHz, phase, ".")
+    plt.plot((IFs + LOs[i]) / u.MHz, detrend(phase), ".")
     plt.xlabel("qubit frequency [MHz]")
     plt.ylabel("Phase [rad]")
     plt.pause(0.1)
@@ -159,7 +159,7 @@ I = np.concatenate(I_tot)
 Q = np.concatenate(Q_tot)
 S = u.demod2volts(I + 1j * Q, readout_len)
 R = np.abs(S)  # Amplitude
-phase = np.angle(S)  # Phase
+phase = detrend(np.unwrap(np.angle(S)))  # Phase
 # Final plot
 plt.figure()
 plt.suptitle("Qubit spectroscopy")
@@ -168,7 +168,7 @@ plt.plot(frequency / u.MHz, R, ".")
 plt.xlabel("qubit frequency [MHz]")
 plt.ylabel(r"$\sqrt{I^2 + Q^2}$ [V]")
 plt.subplot(212, sharex=ax1)
-plt.plot(frequency / u.MHz, phase, ".")
+plt.plot(frequency / u.MHz, detrend(phase), ".")
 plt.xlabel("qubit frequency [MHz]")
 plt.ylabel("Phase [rad]")
 plt.pause(0.1)
