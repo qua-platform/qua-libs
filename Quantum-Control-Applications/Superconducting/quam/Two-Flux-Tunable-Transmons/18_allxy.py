da63a86ff6c94f60b8cf5a62853d2556d2a6b6bf
Serwan Asaad <serwan.asaad@quantum-machines.co>
2024-05-17 08:31:03 +0200

    change data results structure

--- Quantum-Control-Applications/Superconducting/quam/Two-Flux-Tunable-Transmons/18_allxy.py
+++ Quantum-Control-Applications/Superconducting/quam/Two-Flux-Tunable-Transmons/18_allxy.py
@@ -199,9 +199,11 @@ else:
             plt.tight_layout()
             plt.pause(0.1)
 
-            data[f"{qb.name}_I"] = I
-            data[f"{qb.name}_Q"] = Q
-            data[f"{qb.name}_figure"] = fig
+            data[qb.name] = {
+                "I": I,
+                "Q": Q,
+                "figure": fig
+            }
 
     # Close the quantum machines at the end in order to put all flux biases to 0 so that the fridge doesn't heat-up
     qm.close()
