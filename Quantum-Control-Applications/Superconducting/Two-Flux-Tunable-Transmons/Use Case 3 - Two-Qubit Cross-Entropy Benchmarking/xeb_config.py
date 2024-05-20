4cbc36826efd331fb2caf158a81041f5f60a7681
Arthur Strauss <arthurostrauss@gmail.com>
2024-05-20 09:51:13 +0800

    Added new class interface for XEB experiment
    XEB now follows a similar approach to the 2Q RB Use case, where all the details are abstracted away from the user behind the simple declaration of a class `XEB`. The novelty here is that it is possible to run the experiment from this class interface, but also to simulate it through Qiskit Aer using a method `simulate`, taking as input a Qiskit `AerBackend` object (which is expected to contain a representative noise model of the user's device) and using it to sample from a noisy circuit-level quantum simulation.
    
    All the previous plots that could be retrieved are still available through different plot methods of the `XEBResult`class.

--- Quantum-Control-Applications/Superconducting/Two-Flux-Tunable-Transmons/Use Case 3 - Two-Qubit Cross-Entropy Benchmarking/xeb_config.py
+++ Quantum-Control-Applications/Superconducting/Two-Flux-Tunable-Transmons/Use Case 3 - Two-Qubit Cross-Entropy Benchmarking/xeb_config.py
@@ -1,34 +1,50 @@
 from dataclasses import dataclass, asdict, field
-from typing import Literal
+from typing import Literal, List, Union, Optional
 
 import numpy as np
+from gateset import generate_gate_set
+from qua_gate import QUAGate
 
 
 @dataclass
 class XEBConfig:
     """
-    Configuration for running XEB
+    Experiments parameters for running XEB
 
     Args:
         seqs (int): Number of random sequences to run per depth
         depths (np.ndarray): Array of depths to iterate through
         n_shots (int): Number of averages per sequence
-        impose_0_cycle (bool): Whether to impose the first gate at 0-cycle
-        apply_two_qb_gate (bool): Whether to apply a two-qubit gate or not
+        qubits_ids (List[Union[int, str]]): List of qubit identifiers for the targeted qubits as specified in the QuAM
+        baseline_gate_name (str): Name of the baseline gate implementing a pi/2 rotation around the x-axis (default "sx")
         gate_set_choice (str): Choice of gate set for XEB (choose "sw" or "t")
+        two_qb_gate (Optional[QUAGate]): Two-qubit gate to be used in the experiment
+        impose_0_cycle (bool): Whether to impose the first gate at 0-cycle
         save_dir (str): Directory where the data will be saved
         should_save_data (bool): Whether to save the data
         generate_new_data (bool): Whether to generate new data
+        disjoint_processing (bool): Whether to process the data in a disjoint manner (that is compute qubit states independently, relevant only when no two-qubit gate is provided)
 
 
     """
 
     seqs: int
-    depths: np.ndarray
+    depths: Union[np.ndarray, List[int]]
     n_shots: int = 101
-    impose_0_cycle: bool = False
-    apply_two_qb_gate: bool = False
+    qubits_ids: List[Union[int, str]] = field(default_factory=lambda: [0, 1])
+    baseline_gate_name: str = "sx"
     gate_set_choice: Literal["sw", "t"] = "sw"
+    two_qb_gate: Optional[QUAGate] = None
+    impose_0_cycle: bool = False
     save_dir: str = ""
     should_save_data: bool = True
     generate_new_data: bool = True
+    disjoint_processing: bool = False
+
+    def __post_init__(self):
+        if isinstance(self.depths, List):
+            self.depths = np.array(self.depths)
+
+        self.gate_dict = generate_gate_set(self.gate_set_choice)
+        self.n_qubits = len(self.qubits_ids)
+        self.dim = 2**self.n_qubits
