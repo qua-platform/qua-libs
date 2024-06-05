55e2848ce482088572974f4839bcae42f6fcabdd
Serwan Asaad <serwan.asaad@quantum-machines.co>
2024-06-05 18:19:39 +0200

    fix calibration of octave

--- Quantum-Control-Applications/Superconducting/quam/quam_components/quam.py
+++ Quantum-Control-Applications/Superconducting/quam/quam_components/quam.py
@@ -78,5 +78,10 @@ class QuAM(QuamRoot):
         Args:
             QM (QuantumMachine): the running quantum machine.
         """
+        from qm.octave.octave_mixer_calibration import NoCalibrationElements
+
         for name in self.active_qubit_names:
-            self.qubits[name].calibrate_octave(QM)
+            try:
+                self.qubits[name].calibrate_octave(QM)
+            except NoCalibrationElements:
+                print(f"No calibration elements found for {name}. Skipping calibration.")
