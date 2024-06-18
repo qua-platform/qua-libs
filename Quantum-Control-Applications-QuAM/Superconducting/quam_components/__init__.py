fb36ed04438a17e9b80a81f13e6ce0a943d89982
Serwan Asaad <serwan.asaad@quantum-machines.co>
2024-06-18 17:14:15 +0200

    fix: qubit_pair -> transmon_pair

--- Quantum-Control-Applications-QuAM/Superconducting/quam_components/__init__.py
+++ Quantum-Control-Applications-QuAM/Superconducting/quam_components/__init__.py
@@ -2,7 +2,7 @@ from .transmon import *
 from .readout_resonator import *
 from .flux_line import *
 from .tunable_coupler import *
-from .qubit_pair import *
+from .transmon_pair import *
 from .quam_root import *
 
 __all__ = [
@@ -10,6 +10,6 @@ __all__ = [
     *readout_resonator.__all__,
     *flux_line.__all__,
     *tunable_coupler.__all__,
-    *qubit_pair.__all__,
+    *transmon_pair.__all__,
     *quam_root.__all__,
 ]
