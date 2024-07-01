574f703ad835ee5c4afd8b20f717979e4ed8843b
HiroQM <hiroyuki.inoue@quantum-machines.co>
2024-07-01 16:58:17 +0000

    added offset attributes

--- Quantum-Control-Applications-QuAM/Superconducting/quam_components/tunable_coupler.py
+++ Quantum-Control-Applications-QuAM/Superconducting/quam_components/tunable_coupler.py
@@ -8,6 +8,21 @@ __all__ = ["TunableCoupler"]
 class TunableCoupler(SingleChannelLfFem):
     """
     Example QuAM component for a tunable coupler.
+
+    Args:
+        decouple_offset (float): the tunable coupler for which the .
+        interaction_offset (float): the tunable coupler for which the .
     """
     output_mode: str = "amplified"
     upsampling_mode: str = "pulse"
+
+    decouple_offset: float = 0.0
+    interaction_offset: float = 0.0
+
+    def to_decouple_idle(self):
+        """Set the tunable coupler to the decouple offset"""
+        self.set_dc_offset(self.decouple_offset)
+
+    def to_interaction_idle(self):
+        """Set the tunable coupler to the interaction offset"""
+        self.set_dc_offset(self.interaction_offset)
