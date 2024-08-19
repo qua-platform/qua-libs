df1805b80a383cb30ddb15fc6bc824ddcc0d1ce9
TheoQM <theo@quantum-machines.co>
2024-08-19 15:51:26 +0200

    Add 2qb out of a 5qb chip with only 1OPX and 1 OCtave

--- Quantum-Control-Applications-QuAM/Superconducting/quam_libs/wiring/test/conftest.py
+++ Quantum-Control-Applications-QuAM/Superconducting/quam_libs/wiring/test/conftest.py
@@ -12,6 +12,16 @@ def instruments_qw_soprano(request) -> Instruments:
     instruments.add_octave(indices=2)
     return instruments
 
+@pytest.fixture(params=["opx+"])
+def instruments_1OPX1Octave(request) -> Instruments:
+    instruments = Instruments()
+    if request.param == "lf-fem":
+        instruments.add_lf_fem(con=1, slots=[1])
+    elif request.param == "opx+":
+        instruments.add_opx_plus(cons=1)
+    instruments.add_octave(indices=1)
+    return instruments
+
 @pytest.fixture(params=["lf-fem"])#, "opx+"])
 def instruments_1octave(request) -> Instruments:
     instruments = Instruments()
--- Quantum-Control-Applications-QuAM/Superconducting/quam_libs/wiring/test/test_wirer_lf_and_octave.py
+++ Quantum-Control-Applications-QuAM/Superconducting/quam_libs/wiring/test/test_wirer_lf_and_octave.py
@@ -36,3 +36,46 @@ def test_qw_soprano_allocation(instruments_qw_soprano):
 
     if visualize:
         visualize_chassis(connectivity.elements)
+
+def test_qw_soprano_2qb_allocation(instruments_1OPX1Octave):
+    active_qubits = [1, 2]
+
+    connectivity = Connectivity()
+    # TODO: is the port here the Octave port?
+    connectivity.add_resonator_line(qubits=active_qubits, con=1, port=2)
+    connectivity.add_qubit_drive_lines(qubits=[1], con=1, port=2)
+    connectivity.add_qubit_drive_lines(qubits=[2], con=1, port=4)
+    connectivity.add_qubit_flux_lines(qubits=active_qubits)
+
+    allocate_wiring(connectivity, instruments_1OPX1Octave)
+
+    pprint(connectivity.elements)
+
+    if visualize:
+        visualize_chassis(connectivity.elements)
+
+def test_qw_soprano_2qb_among_5_allocation(instruments_1OPX1Octave):
+    all_qubits = [1, 2, 3, 4, 5]
+    active_qubits = [1, 2]
+    other_qubits = list(set(all_qubits) - set(active_qubits))
+
+    connectivity = Connectivity()
+    # TODO: I want here to declare 2 qubits that I can address with my hardware (1OPX+ and 1 Octave)
+    connectivity.add_resonator_line(qubits=active_qubits, con=1, port=1)
+    connectivity.add_qubit_drive_lines(qubits=[1], con=1, port=2)
+    connectivity.add_qubit_drive_lines(qubits=[2], con=1, port=4)
+    connectivity.add_qubit_flux_lines(qubits=active_qubits)
+    # TODO: I want to add here the remaining qubits so that the QuAM can be created for the entire chip.
+    #  I thus connect the other qubits to the same ports as the active qubits.
+    #  Can I have the same ports used in several qubits as it is done for the resonator line?
+    connectivity.add_resonator_line(qubits=other_qubits, con=1, port=1)
+    connectivity.add_qubit_drive_lines(qubits=other_qubits, con=1, port=2)
+    connectivity.add_qubit_flux_lines(qubits=other_qubits, con=1, port=10)
+
+
+    allocate_wiring(connectivity, instruments_1OPX1Octave)
+
+    pprint(connectivity.elements)
+
+    if visualize:
+        visualize_chassis(connectivity.elements)
