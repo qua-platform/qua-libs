947ecaca2cf5708cb5e87dac690a9da0a2a7dbd7
TheoQM <theo@quantum-machines.co>
2024-10-08 09:50:24 +0200

    make_wiring examples

---
+++ Quantum-Control-Applications-QuAM/Superconducting/configuration/make_wiring_lffem_octave.py
@@ -0,0 +1,48 @@
+# %%
+from qualang_tools.wirer.wirer.channel_specs import lf_fem_spec, lf_fem_iq_octave_spec, octave_spec
+from qualang_tools.wirer import Instruments, Connectivity, allocate_wiring, visualize
+from quam_libs.quam_builder.machine import build_quam_wiring
+
+# Define static parameters
+host_ip = "127.0.0.1"  # QOP IP address
+cluster_name = "Cluster_1"  # Name of the cluster
+# Desired location of wiring.json and state.json
+# The folder must not contain other json files.
+path = "./quam_state"
+
+# Define the available instrument setup
+instruments = Instruments()
+instruments.add_lf_fem(controller=1, slots=[1, 2])
+instruments.add_octave(indices=1)
+
+# Define which quantum elements are present in the system
+qubits = [1, 2, 3]
+# Allocate the wiring to the connectivity object based on the available instruments
+connectivity = Connectivity()
+
+# Single feed-line for reading the resonators & individual qubit drive lines
+# Define any custom/hardcoded channel addresses
+q1_res_ch = octave_spec(index=1, rf_out=1, rf_in=1)
+connectivity.add_resonator_line(qubits=qubits, constraints=q1_res_ch)
+connectivity.add_qubit_flux_lines(qubits=qubits)
+connectivity.add_qubit_drive_lines(qubits=qubits)
+# connectivity.add_qubit_pair_flux_lines(qubit_pairs=[(1,2), (2,3)])  # Tunable coupler
+allocate_wiring(connectivity, instruments)
+
+# Single feed-line for reading the resonators & driving the qubits + flux on specific fem slot
+# Define any custom/hardcoded channel addresses
+# q1_res_ch = octave_spec(index=1, rf_out=1, rf_in=1)
+# q1_drive_ch = lf_fem_iq_octave_spec(con=1, slot=1, out_port_i=3, out_port_q=4, octave_index=1, rf_out=2)
+# q1_flux_con = lf_fem_spec(con=1, out_slot=2, out_port=None)
+# connectivity.add_resonator_line(qubits=qubits, constraints=q1_res_ch)
+# connectivity.add_qubit_flux_lines(qubits=qubits, constraints=q1_flux_con)
+# # connectivity.add_qubit_pair_flux_lines(qubit_pairs=[(1,2)])  # Tunable coupler
+# for qubit in qubits:
+#     connectivity.add_qubit_drive_lines(qubits=qubit, constraints=q1_drive_ch)
+#     allocate_wiring(connectivity, instruments, block_used_channels=False)
+
+# Build the wiring and network into a QuAM machine and save it as "wiring.json"
+build_quam_wiring(connectivity, host_ip, cluster_name, path)
+
+# View wiring schematic
+visualize(connectivity.elements, available_channels=instruments.available_channels)
--- Quantum-Control-Applications-QuAM/Superconducting/configuration/make_wiring_opxp_octave.py
+++ Quantum-Control-Applications-QuAM/Superconducting/configuration/make_wiring_opxp_octave.py
@@ -1,10 +1,10 @@
 # %%
-from qualang_tools.wirer.wirer.channel_specs import *
+from qualang_tools.wirer.wirer.channel_specs import opx_spec, octave_spec, opx_iq_octave_spec
 from qualang_tools.wirer import Instruments, Connectivity, allocate_wiring, visualize
 from quam_libs.quam_builder.machine import build_quam_wiring
 
 # Define static parameters
-host_ip = "172.16.33.101"  # QOP IP address
+host_ip = "127.0.0.1"  # QOP IP address
 cluster_name = "Cluster_1"  # Name of the cluster
 # Desired location of wiring.json and state.json
 # The folder must not contain other json files.
@@ -12,40 +12,40 @@ path = "./quam_state"
 
 # Define the available instrument setup
 instruments = Instruments()
-# instruments.add_opx_plus(controllers = [1])
-# instruments.add_octave(indices = 1)
-instruments.add_mw_fem(controller=1, slots=[1, 2])
-instruments.add_lf_fem(controller=1, slots=[3, 4])
+instruments.add_opx_plus(controllers=[1, 2])
+instruments.add_octave(indices=1)
 
-# Define any custom/hardcoded channel addresses
-q1_res_ch = mw_fem_spec(con=1, slot=1, in_port=1, out_port=1)
-q1_drive_ch = mw_fem_spec(con=1, slot=1, in_port=None, out_port=2)
-q1_flux_fem = lf_fem_spec(con=1, in_slot=None, in_port=None, out_slot=4, out_port=None)
-
-# Define which quantum elements are present in the system
-qubits = [1, 2, 3, 4, 5, 6]
+# Define which qubit indices are present in the system
+qubits = [1, 2]
 # Allocate the wiring to the connectivity object based on the available instruments
 connectivity = Connectivity()
 
 # Single feed-line for reading the resonators & individual qubit drive lines
+# Define any custom/hardcoded channel addresses
+q1_res_ch = octave_spec(index=1, rf_out=1, rf_in=1)
 connectivity.add_resonator_line(qubits=qubits, constraints=q1_res_ch)
 connectivity.add_qubit_flux_lines(qubits=qubits)
-connectivity.add_qubit_drive_lines(qubits=qubits)
+connectivity.add_qubit_drive_lines(qubits=[1], constraints=octave_spec(index=1, rf_out=2))
+connectivity.add_qubit_drive_lines(
+    qubits=[2], constraints=opx_iq_octave_spec(con=1, out_port_i=7, out_port_q=8, octave_index=1, rf_out=4)
+)
+# connectivity.add_qubit_pair_flux_lines(qubit_pairs=[(1,2)])  # Tunable coupler
 allocate_wiring(connectivity, instruments)
 
 # Single feed-line for reading the resonators & driving the qubits + flux on specific fem slot
+# Define any custom/hardcoded channel addresses
+# q1_res_ch = octave_spec(index=1, rf_out=1, rf_in=1)
+# q1_drive_ch = opx_iq_octave_spec(con=1, out_port_i=3, out_port_q=4, octave_index=1, rf_out=2)
+# q1_flux_con = opx_spec(con=2, in_port=None, out_port=None)
 # connectivity.add_resonator_line(qubits=qubits, constraints=q1_res_ch)
-# connectivity.add_qubit_flux_lines(qubits=qubits, constraints=q1_flux_fem)
+# connectivity.add_qubit_flux_lines(qubits=qubits, constraints=q1_flux_con)
+# # connectivity.add_qubit_pair_flux_lines(qubit_pairs=[(1,2)])  # Tunable coupler
 # for qubit in qubits:
 #     connectivity.add_qubit_drive_lines(qubits=qubit, constraints=q1_drive_ch)
 #     allocate_wiring(connectivity, instruments, block_used_channels=False)
-
-connectivity.add_qubit_pair_flux_lines(qubit_pairs=[(1,2)])
 
 # Build the wiring and network into a QuAM machine and save it as "wiring.json"
 build_quam_wiring(connectivity, host_ip, cluster_name, path)
 
 # View wiring schematic
 visualize(connectivity.elements, available_channels=instruments.available_channels)
-
-# %%
