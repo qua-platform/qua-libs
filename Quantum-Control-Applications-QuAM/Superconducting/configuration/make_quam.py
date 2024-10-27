901da122d063db81e73da537fabe8df0c8b4d8db
HiroQM <hiroyuki.inoue@quantum-machines.co>
2024-10-27 13:42:23 +0900

    added make quam & wiring

--- Quantum-Control-Applications-QuAM/Superconducting/configuration/make_quam.py
+++ Quantum-Control-Applications-QuAM/Superconducting/configuration/make_quam.py
@@ -6,9 +6,9 @@ path = "./quam_state"
 
 machine = QuAM.load(path)
 
-# octave_settings = {"octave1": {"port": 11235} }  # externally configured: (11XXX where XXX are last three digits of oct ip)
+octave_settings = {"octave1": {"port": 11232} }  # externally configured: (11XXX where XXX are last three digits of oct ip)
 # octave_settings = {"oct1": {"ip": "192.168.88.250"} }  # "internally" configured: use the local ip address of the Octave
-octave_settings = {}
+# octave_settings = {}
 
 # Make the QuAM object and save it
 quam = build_quam(machine, quam_state_path=path, octaves_settings=octave_settings)
