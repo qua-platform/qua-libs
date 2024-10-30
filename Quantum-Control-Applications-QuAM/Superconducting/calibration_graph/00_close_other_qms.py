d85f5a3c92e7fc150018d62a741cfa609ee3306c
Dean Poulos <dean.poulos7@gmail.com>
2024-10-31 03:18:17 +1100

    Add node for closing the QM manually when needed.

---
+++ Quantum-Control-Applications-QuAM/Superconducting/calibration_graph/00_close_other_qms.py
@@ -0,0 +1,29 @@
+# %%
+
+"""
+A simple program to close all other open QMs.
+"""
+
+from typing import Optional, List
+from qualibrate import QualibrationNode, NodeParameters
+
+from quam_libs.components import QuAM
+
+
+# %% {Node_parameters}
+class Parameters(NodeParameters):
+    qubits: Optional[str] = None
+
+node = QualibrationNode(
+    name="00_Close_other_QMs",
+    parameters=Parameters()
+)
+
+# Instantiate the QuAM class from the state file
+machine = QuAM.load()
+
+# Generate the OPX and Octave configurations
+config = machine.generate_config()
+# Open Communication with the QOP
+qmm = machine.connect()
+qmm.close_all_qms()
