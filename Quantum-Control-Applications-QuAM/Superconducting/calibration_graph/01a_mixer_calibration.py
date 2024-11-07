70cbe45447e8d990a934853cbae98e143277ae27
TheoQM <theo@quantum-machines.co>
2024-11-07 13:58:55 +0100

    mixer cal

--- Quantum-Control-Applications-QuAM/Superconducting/calibration_graph/01a_mixer_calibration.py
+++ Quantum-Control-Applications-QuAM/Superconducting/calibration_graph/01a_mixer_calibration.py
@@ -4,39 +4,46 @@
 A simple program to calibrate Octave mixers for all qubits and resonators
 """
 
-from typing import Optional
 from qualibrate import QualibrationNode, NodeParameters
-
 from quam_libs.components import QuAM
+from qualang_tools.multi_user import qm_session
+from qualang_tools.units import unit
+from typing import Optional, List
 
 
 # %% {Node_parameters}
 class Parameters(NodeParameters):
-    qubits: Optional[str] = None
+    qubits: Optional[List[str]] = None
     calibrate_resonator: bool = True
     calibrate_drive: bool = True
+    timeout: int = 100
 
 
 node = QualibrationNode(name="00_Mixer_Calibration", parameters=Parameters())
 
+# %% {Initialize_QuAM_and_QOP}
+# Class containing tools to help handling units and conversions.
+u = unit(coerce_to_integer=True)
 # Instantiate the QuAM class from the state file
 machine = QuAM.load()
 
-# Generate the OPX and Octave configurations
-config = machine.generate_config()
-# Open Communication with the QOP
-qmm = machine.connect()
-qm = qmm.open_qm(config)
-
+# Get the relevant QuAM components
 if node.parameters.qubits is None or node.parameters.qubits == "":
     qubits = machine.active_qubits
 else:
     qubits = [machine.qubits[q] for q in node.parameters.qubits.replace(" ", "").split(",")]
 
-for qubit in qubits:
-    qubit.calibrate_octave(
-        qm, calibrate_drive=node.parameters.calibrate_drive, calibrate_resonator=node.parameters.calibrate_resonator
-    )
+# Generate the OPX and Octave configurations
+config = machine.generate_config()
+# Open Communication with the QOP
+qmm = machine.connect()
 
-qm.close()
+
+# %% {Execute calibration}
+with qm_session(qmm, config, timeout=node.parameters.timeout) as qm:
+    for qubit in qubits:
+        qubit.calibrate_octave(
+            qm, calibrate_drive=node.parameters.calibrate_drive, calibrate_resonator=node.parameters.calibrate_resonator
+        )
+
 # %%
