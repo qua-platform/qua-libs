a6b8862c55724cc310e98a32b7dff045e18bb712
TheoQM <theo@quantum-machines.co>
2024-11-08 19:11:40 +0100

    thermal as default

--- Quantum-Control-Applications-QuAM/Superconducting/calibration_graph/10b_Single_Qubit_Randomized_Benchmarking_Interleaved.py
+++ Quantum-Control-Applications-QuAM/Superconducting/calibration_graph/10b_Single_Qubit_Randomized_Benchmarking_Interleaved.py
@@ -51,7 +51,7 @@ class Parameters(NodeParameters):
     delta_clifford: int = 10
     seed: int = 345324
     flux_point_joint_or_independent: Literal["joint", "independent"] = "independent"
-    reset_type_thermal_or_active: Literal["thermal", "active"] = "active"
+    reset_type_thermal_or_active: Literal["thermal", "active"] = "thermal"
     simulate: bool = False
     simulation_duration_ns: int = 2500
     timeout: int = 100
