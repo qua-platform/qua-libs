46ca754eda03d17191e56fd05a55febdebfb106c
Dean Poulos <dean.poulos7@gmail.com>
2024-11-19 02:32:13 +1100

    Make resonator-spec-vs-amplitude fitting robust.

--- Quantum-Control-Applications-QuAM/Superconducting/calibration_graph/03a_Qubit_Spectroscopy.py
+++ Quantum-Control-Applications-QuAM/Superconducting/calibration_graph/03a_Qubit_Spectroscopy.py
@@ -25,11 +25,8 @@ Before proceeding to the next node:
 # %% {Imports}
 from qualibrate import QualibrationNode, NodeParameters
 
-from quam.quam.components import IQChannel
-
-from quam.quam.components.channels import MWChannel
 from quam_libs.components import QuAM
-from quam_libs.lib.constants import instrument_limits
+from quam_libs.lib.instrument_limits import instrument_limits
 from quam_libs.macros import qua_declaration
 from quam_libs.lib.qua_datasets import convert_IQ_to_V
 from quam_libs.lib.plot_utils import QubitGrid, grid_iter
