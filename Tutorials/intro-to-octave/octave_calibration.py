cc2557125e7c8b18c794b009b6ebf1f98fc00f70
TheoQM <theo@quantum-machines.co>
2024-11-19 14:27:15 +0100

    update tutorials intro-to-octave and readmes

---
+++ Tutorials/intro-to-octave/octave_calibration.py
@@ -0,0 +1,23 @@
+"""
+This file is used to configure the Octave's clock and do the automatic calibration.
+"""
+
+from qm import QuantumMachinesManager
+from configuration import *
+
+
+# Configure the Octave according to the elements settings and calibrate
+qmm = QuantumMachinesManager(host=qop_ip, port=qop_port, octave=octave_config, log_level="ERROR")
+qm = qmm.open_qm(config)
+
+
+##################
+# Calibration #
+##################
+calibration = True
+
+if calibration:
+    elements = ["qe1", "qe2", "qe3", "qe4", "qe5"]
+    for element in elements:
+        print("-" * 37 + f" Calibrates {element}")
+        qm.calibrate_element(element, {LO: (IF,)})  # can provide many IFs for specific LO
