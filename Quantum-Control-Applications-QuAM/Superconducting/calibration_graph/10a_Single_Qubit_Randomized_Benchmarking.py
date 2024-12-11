d2456c850df8efcd7e904c82e4aa022fb3ee7ec8
TomDvirQM <tom.dvir@quantum-machines.co>
2024-12-11 22:42:42 +0100

    imporved RB with XZ pulses

--- Quantum-Control-Applications-QuAM/Superconducting/calibration_graph/10a_Single_Qubit_Randomized_Benchmarking.py
+++ Quantum-Control-Applications-QuAM/Superconducting/calibration_graph/10a_Single_Qubit_Randomized_Benchmarking.py
@@ -49,8 +49,8 @@ class Parameters(NodeParameters):
     max_circuit_depth: int = 1000  # Maximum circuit depth
     delta_clifford: int = 20
     seed: int = 345324
-    flux_point_joint_or_independent: Literal["joint", "independent"] = "independent"
-    reset_type_thermal_or_active: Literal["thermal", "active"] = "thermal"
+    flux_point_joint_or_independent: Literal["joint", "independent"] = "joint"
+    reset_type_thermal_or_active: Literal["thermal", "active"] = "active"
     simulate: bool = False
     simulation_duration_ns: int = 2500
     timeout: int = 100
