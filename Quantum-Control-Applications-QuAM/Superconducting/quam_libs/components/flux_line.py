408da7020566bf68bfc3c65d039348d6421be1d7
TomDvirQM <tom.dvir@quantum-machines.co>
2024-12-11 22:45:13 +0100

    default settle time for the flux is 4 nS

--- Quantum-Control-Applications-QuAM/Superconducting/quam_libs/components/flux_line.py
+++ Quantum-Control-Applications-QuAM/Superconducting/quam_libs/components/flux_line.py
@@ -22,7 +22,7 @@ class FluxLine(SingleChannel):
     joint_offset: float = 0.0
     min_offset: float = 0.0
     arbitrary_offset: float = 0.0
-    settle_time: float = 0.0
+    settle_time: float = 4
 
     def settle(self):
         """Wait for the flux bias to settle"""
