e7fe5f85eb025a566d971a256450b7ef5a81cd67
TheoQM <theo@quantum-machines.co>
2025-02-07 11:32:35 +0100

    clean qua_datasets

--- Quantum-Control-Applications-QuAM/Superconducting/quam_libs/lib/qua_datasets.py
+++ Quantum-Control-Applications-QuAM/Superconducting/quam_libs/lib/qua_datasets.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from quam_libs.components.transmon import Transmon
+from configuration.get_my_quam import QuAM
 import numpy as np
 import xarray as xr
 
@@ -23,38 +23,43 @@ def extract_dict(ds: xr.Dataset, data_var) -> dict:
     return ds[[data_var]].to_dataframe().to_dict()[data_var]
 
 
-def convert_IQ_to_V(da: xr.DataArray, qubits: list[Transmon], IQ_list: list[str] = ("I", "Q")) -> xr.DataArray:
+def convert_IQ_to_V(da: xr.DataArray, qubits: list[QuAM.qubit_type], IQ_list: list[str] = ("I", "Q"), single_demod: bool = False) -> xr.DataArray:
+    # TODO: this is Transmon specific so it should probably not belong here
     """
     return data array with the 'I' and 'Q' quadratures converted to Volts.
 
     :param da: the array on which to calculate angle. Assumed to have complex data
     :type da: xr.DataArray
     :param qubits: the list of qubits components.
-    :type qubits: list[Transmon]
+    :type qubits: list[QuAM.qubit_type]
     :param IQ_list: the list of data to convert to V e.g. ["I", "Q"].
     :type IQ_list: list[str]
+    :param single_demod: Flag to add the additional factor of 2 needed for single demod.
+    :type single_demod: bool
     :return: a data array with the same dimensions and coordinates, with a 'I' and 'Q' in Volts
     :type: xr.DataArray
+
     """
     # Create a xarray with a coordinate 'qubit' and the value is q.resonator.operations["readout"].length
     readout_lengths = xr.DataArray(
         [q.resonator.operations["readout"].length for q in qubits], coords=[("qubit", [q.name for q in qubits])]
     )
-    return da.assign({key: da[key] * 2**12 / readout_lengths for key in IQ_list})
+    demod_factor = 2 if single_demod else 1
+    return da.assign({key: da[key] * demod_factor * 2**12 / readout_lengths for key in IQ_list})
 
 
 def apply_angle(da: xr.DataArray, dim: str, unwrap=True) -> xr.DataArray:
     """
     return data array with the angle of complex data calculated along dimension `dim`.
-    Useful when we have IQ data and we want to get the phase of it, usually together with `subtract_slope`.
+    Useful when we have IQ data, and we want to get the phase of it, usually together with `subtract_slope`.
 
     :param da: the array on which to calculate angle. Assumed to have complex data
     :type da: xr.DataArray
-    :param dim: the dimesnsion along which to subtract
+    :param dim: the dimension along which to subtract
     :type dim: str
     :param unwrap: whether to unwrap the phase
     :type unwrap: bool, optional
-    :return: a dataarray with the same dimensions and coordinates, with a linear slope subtrcated along dim
+    :return: a data-array with the same dimensions and coordinates, with a linear slope subtracted along dim
     :type: xr.DataArray
     """
     if unwrap:
@@ -77,9 +82,9 @@ def subtract_slope(da: xr.DataArray, dim: str) -> xr.DataArray:
 
     :param da: the array from which to subtract
     :type da: xr.DataArray
-    :param dim: the dimesnsion along which to subtract
+    :param dim: the dimension along which to subtract
     :type dim: str
-    :return: a dataarray with the same dimensions and coordinates, with a linear slope subtrcated along dim
+    :return: a data-array with the same dimensions and coordinates, with a linear slope subtracted along dim
     :rtype: xr.DataArray
     """
     x = da[dim]
@@ -92,8 +97,8 @@ def subtract_slope(da: xr.DataArray, dim: str) -> xr.DataArray:
             return np.polyval(c, x)
 
         pf = np.apply_along_axis(fit_func, -1, arr)
-        evaled = np.apply_along_axis(eval_func, -1, pf)
-        return arr - evaled
+        evaluated = np.apply_along_axis(eval_func, -1, pf)
+        return arr - evaluated
 
     return xr.apply_ufunc(sub_slope, da, input_core_dims=[[dim]], output_core_dims=[[dim]])
 
@@ -107,9 +112,9 @@ def unrotate_phase(da: xr.DataArray, dim: str) -> xr.DataArray:
 
     :param da: the array from which to unrotate
     :type da: xr.DataArray
-    :param dim: the dimesnsion along which to unrotate
+    :param dim: the dimension along which to unrotate
     :type dim: str
-    :return: a dataarray with the same dimensions and coordinates, with phase unrotated along dim
+    :return: a data-array with the same dimensions and coordinates, with phase unrotated along dim
     :rtype: xr.DataArray
     """
     x = da[dim]
@@ -123,13 +128,13 @@ def unrotate_phase(da: xr.DataArray, dim: str) -> xr.DataArray:
             return np.polyval(c, x)
 
         pf = np.apply_along_axis(fit_func, -1, angle)
-        evaled_angle = np.apply_along_axis(eval_func, -1, pf)
-        return arr * np.exp(-1j * evaled_angle)
+        evaluated_angle = np.apply_along_axis(eval_func, -1, pf)
+        return arr * np.exp(-1j * evaluated_angle)
 
     return xr.apply_ufunc(unrotate, da, input_core_dims=[[dim]], output_core_dims=[[dim]])
 
 
-def integer_histogram(da: xr.DataArray, dim: str, minlength: Optional[int] = None) -> xr.DataArray:
+def integer_histogram(da: xr.DataArray, dim: str, min_length: Optional[int] = None) -> xr.DataArray:
     """Calculate histogram of non-negative integer values of data array along
     a dimension.
 
@@ -137,8 +142,8 @@ def integer_histogram(da: xr.DataArray, dim: str, minlength: Optional[int] = Non
     :type da: xr.DataArray
     :param dim: dimension along which to calculate histogram. This dimension will be converted into a bins dimension.
     :type dim: str
-    :param minlength: if included, bins will include values from 0 to minlength-1
-    :type minlength: int
+    :param min_length: if included, bins will include values from 0 to min_length-1
+    :type min_length: int
     :return: a data array with counts on integers, all dimensions identical to `da`, except along `dim`.
     :rtype: xr.DataArray
     """
@@ -147,7 +152,7 @@ def integer_histogram(da: xr.DataArray, dim: str, minlength: Optional[int] = Non
         raise ValueError("da must have a name")
 
     def count_bins(vec):
-        return np.bincount(vec, minlength=minlength)
+        return np.bincount(vec, minlength=min_length)
 
     da2 = xr.apply_ufunc(count_bins, da, input_core_dims=[[dim]], output_core_dims=[[da.name]], vectorize=True)
     return da2.assign_coords({da.name: np.arange(len(da2[da.name]))})
