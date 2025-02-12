64cd72d0e4f5224f0b0ed2ada880623df752d815
TheoQM <theo@quantum-machines.co>
2025-02-12 10:20:37 +0100

    make the components hardware agnostic

--- Quantum-Control-Applications-QuAM/Superconducting/quam_libs/components/superconducting/architectural_elements/zz_drive.py
+++ Quantum-Control-Applications-QuAM/Superconducting/quam_libs/components/superconducting/architectural_elements/zz_drive.py
@@ -1,8 +1,7 @@
 from quam.core import quam_dataclass
 from quam.components.channels import IQChannel, MWChannel
-import numpy as np
 
-__all__ = ["ZZDrive", "ZZDriveIQ", "ZZDriveMW"]
+__all__ = ["ZZDriveIQ", "ZZDriveMW"]
 
 @quam_dataclass
 class ZZDriveBase:
@@ -21,12 +20,6 @@ class ZZDriveIQ(IQChannel, ZZDriveBase):
     def inferred_intermediate_frequency(self):
         return self.target_qubit_LO_frequency + self.target_qubit_IF_frequency - self.LO_frequency + self.detuning
 
-    # def get_output_power(self, operation, Z=50) -> float:
-    #     power = self.frequency_converter_up.power
-    #     amplitude = self.operations[operation].amplitude
-    #     x_mw = 10 ** (power / 10)
-    #     x_v = amplitude * np.sqrt(2 * Z * x_mw / 1000)
-    #     return 10 * np.log10(((x_v / np.sqrt(2)) ** 2 * 1000) / Z)
 
 @quam_dataclass
 class ZZDriveMW(MWChannel, ZZDriveBase):
@@ -37,15 +30,3 @@ class ZZDriveMW(MWChannel, ZZDriveBase):
     @property
     def upconverter_frequency(self):
         return self.opx_output.upconverter_frequency
-
-    # add property of upconverter here?
-    def get_output_power(self, operation, Z=50) -> float:
-        power = self.opx_output.full_scale_power_dbm
-        amplitude = self.operations[operation].amplitude
-        x_mw = 10 ** (power / 10)
-        x_v = amplitude * np.sqrt(2 * Z * x_mw / 1000)
-        return 10 * np.log10(((x_v / np.sqrt(2)) ** 2 * 1000) / Z)
-
-
-ZZDrive = ZZDriveIQ
-
