05a06ff9a71fd2f0a8793222a3bde60ed14ff053
TheoQM <theo@quantum-machines.co>
2025-02-12 12:26:16 +0100

    remove post_init

--- Quantum-Control-Applications-QuAM/Superconducting/quam_libs/components/superconducting/architectural_elements/tunable_coupler.py
+++ Quantum-Control-Applications-QuAM/Superconducting/quam_libs/components/superconducting/architectural_elements/tunable_coupler.py
@@ -21,11 +21,6 @@ class TunableCoupler(SingleChannel):
     decouple_offset: float = 0.0
     interaction_offset: float = 0.0
 
-    def __post_init__(self):
-        if isinstance(self.opx_output, LFFEMAnalogOutputPort):
-            self.opx_output.upsampling_mode = self.upsampling_mode
-            self.opx_output.output_mode = self.output_mode
-
     def to_decouple_idle(self):
         """Set the tunable coupler to the decouple offset"""
         self.set_dc_offset(self.decouple_offset)
