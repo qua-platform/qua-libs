0d747011b7cbb8ddeef9b0c28cf577d9d21a997c
omrieoqm <omrie.ovdat@quantum-machines.co>
2025-02-13 15:15:15 +0200

    qua bugs fixing

--- Quantum-Control-Applications-QuAM/Superconducting/random_exp/2QRB/rb.py
+++ Quantum-Control-Applications-QuAM/Superconducting/random_exp/2QRB/rb.py
@@ -24,6 +24,8 @@ from qualang_tools.units import unit
 from quam_libs.components import QuAM
 from quam_libs.experiments.rb.rb_utils import StandardRB, quantum_circuit_to_integer_and_angle_list
 
+import matplotlib.pyplot as plt
+
 
 
 # %% {Node_parameters}
@@ -33,22 +35,24 @@ basis_gates = ['rz', 'x90',  'x180', 'cz'] # , 'y90', 'y180']
 node = BaseNode(
     name="2Q_RB",
     parameters=Parameters(
-        qubit_pair_name="qC2-qC4",
-        circuit_lengths=[1,2,4,8,16,32],
-        num_circuits_per_length = 15,
-        num_averages = 100,
+        qubit_pair_name="qC2-qC1",
+        circuit_lengths=[4], # [1,2,4,8,16,32],
+        num_circuits_per_length = 1,
+        num_averages = 5,
         basis_gates = basis_gates, # gates are as written in state
         seed = 0,
         reset_type = "thermal",
-        simulate = False,
+        simulate = True,
         timeout = 100
     )
 )
 
-rz_id = 16
-cz_id = 32
-measure_id = 64
-barrier_id = 128
+x90_id = 2
+x180_id = 4
+rz_id = 8
+cz_id = 16
+measure_id = 32
+barrier_id = 64
 
 gate_to_int = dict(zip(basis_gates, range(len(basis_gates))))
 
@@ -112,19 +116,19 @@ for l, circuits in transpiled_circuits.items():
         qubit_indices.extend(qubit_int)
         circuits_as_int.extend(gate_int)
         circuit_gate_angles.extend(gate_angle)
-    # circuits_start_index.append(sum(circuits_start_index) + len(circuits_as_int))
 
-# circuits_lengths = list(np.array(circuits_start_index[1:]) - np.array(circuits_start_index[:-1]))
+
+circuits_as_int = [2,2,2,2,2]
 
 with program() as rb:
     
-    n_st = declare_stream()
+    # n_st = declare_stream()
     state_control = declare(int)
     state_target = declare(int)
     state = declare(int)
     # state_st_control = declare_stream()
     # state_st_target = declare_stream()
-    state_st = declare_stream()
+    # state_st = declare_stream()
 
     # machine.set_all_fluxes('joint', qubit_control)
     
@@ -135,28 +139,29 @@ with program() as rb:
     num_lengths = len(node.parameters.circuit_lengths)
     num_circuits_per_length = node.parameters.num_circuits_per_length
     
+    x90_id_qua = declare(int, value=x90_id)
+    x180_id_qua = declare(int, value=x180_id)
     cz_id_qua = declare(int, value=cz_id)
     rz_id_qua = declare(int, value=rz_id)
     measure_id_qua = declare(int, value=measure_id)
     barrier_id_qua = declare(int, value=barrier_id)
     qubit_indices_qua = declare(int, value=qubit_indices)
     circuit_qua = declare(int, value=circuits_as_int) # The circuits as integers
-    # angles_qua = declare(float, value=circuit_gate_angles) # The angles of the gates
-    # circuits_start_index_qua = declare(int, value=circuits_start_index) # The start index of each circuit
-    # circuits_lengths_qua = declare(int, value=circuits_lengths) # The lengths of the circuits
-    
+    angles_qua = declare(fixed, value=circuit_gate_angles) # The angles of the gates
+   
     with for_(n, 0, n < n_avg, n + 1):
-        save(n, n_st)
+        # save(n, n_st)
         
-        reset_qubits(node, qubit_control, qubit_target, thermalization_time * u.ns)
+        # qubit_control.xy.play('x90')
+        # reset_qubits(node, qubit_control, qubit_target, thermalization_time * u.ns)
             
         align() # align(qubit_control.name, qubit_target.name)
         
-        cnt = 0
+
         with for_(i, 0, i < len(circuits_as_int), i + 1):   
             
             g_id = circuit_qua[i]
-            with switch_(g_id):
+            with switch_(g_id, unsafe=True):
                 with case_(cz_id_qua):
                     cz.execute()
                 with case_(barrier_id_qua):
@@ -167,28 +172,35 @@ with program() as rb:
                     readout_state(qubit_target, state_target)
                     # assign(state, state_control*2 + state_target)
                     assign(state, (Cast.to_int(state_control) << 1) + Cast.to_int(state_target))
-
-                    
-                    # save(state_control, state_st_control)
-                    # save(state_target, state_st_target)
                     save(state, state_st)
                     
                     # reset qubits
                     reset_qubits(node, qubit_control, qubit_target, thermalization_time)
                 with case_(rz_id_qua):
-                    qubits[qubit_indices[cnt]].xy.frame_rotation(circuit_gate_angles[cnt])
-                with default_():
-                    qubits[qubit_indices[cnt]].xy.play(int_to_xy.get(circuits_as_int[cnt], 'x90'))
-            
-            cnt = cnt + 1
+                    with if_(qubit_indices_qua[i] == 0): # unfavourable becuase expected to genarate gaps from the case_ and the if_
+                        qubit_control.xy.frame_rotation(angles_qua[i])
+                    with else_():
+                        qubit_target.xy.frame_rotation(angles_qua[i])
+                with case_(x90_id_qua):
+                    with if_(qubit_indices_qua[i] == 0): # unfavourable becuase expected to genarate gaps from the case_ and the if_
+                        qubit_control.xy.play('x90')
+                    with else_():
+                        qubit_target.xy.play('x90')
+                        
+                with case_(x180_id_qua):
+                    with if_(qubit_indices_qua[i] == 0): # unfavourable becuase expected to genarate gaps from the case_ and the if_
+                        qubit_control.xy.play('x180')
+                    with else_():
+                        qubit_target.xy.play('x180')
+
             
 
-    with stream_processing():
-        n_st.save("n")
-        for i in range(1): # num qubit pairs
-            # state_st_control.buffer(num_circuits_per_length).buffer(num_lengths).buffer(n_avg).save(f"state_control{i + 1}")
-            # state_st_target.buffer(num_circuits_per_length).buffer(num_lengths).buffer(n_avg).save(f"state_target{i + 1}")
-            state_st.buffer(num_circuits_per_length).buffer(num_lengths).buffer(n_avg).save(f"state{i + 1}")
+    # with stream_processing():
+    #     n_st.save("n")
+    #     for i in range(1): # num qubit pairs
+    #         # state_st_control.buffer(num_circuits_per_length).buffer(num_lengths).buffer(n_avg).save(f"state_control{i + 1}")
+    #         # state_st_target.buffer(num_circuits_per_length).buffer(num_lengths).buffer(n_avg).save(f"state_target{i + 1}")
+    #         state_st.buffer(num_circuits_per_length).buffer(num_lengths).buffer(n_avg).save(f"state{i + 1}")
 
 # %% 
 ###########################
@@ -200,6 +212,14 @@ if node.parameters.simulate:
     simulation_config = SimulationConfig(duration=1_000)  # In clock cycles = 4ns
     job = qmm.simulate(config, rb, simulation_config)
     job.get_simulated_samples()
+    # Get the simulated samples and plot them for all controllers
+    samples = job.get_simulated_samples()
+    for i, con in enumerate(samples.keys()):
+        plt.plot(len(samples.keys()), 1, i + 1)
+        samples[con].plot()
+        plt.title(con)
+        plt.show()
+    
 else: #node.parameters.load_data_id is None:
     with qm_session(qmm, config, timeout=node.parameters.timeout) as qm:
         job = qm.execute(rb)
