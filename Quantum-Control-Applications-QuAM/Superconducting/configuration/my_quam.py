c0f8661cbb62c1d6f9aa495c0ae9d90e87de6e3f
Dean Poulos <dean.poulos7@gmail.com>
2025-02-17 23:32:32 +1100

    Simplify my_quam structure.

--- Quantum-Control-Applications-QuAM/Superconducting/configuration/my_quam.py
+++ Quantum-Control-Applications-QuAM/Superconducting/configuration/my_quam.py
@@ -1,15 +1,12 @@
+from quam_libs.components.superconducting.qpu.base_quam import BaseQuAM
 from quam_libs.components.superconducting.qpu.flux_tunable_qpu import QuAM as FluxTunableQuAM
 from quam_libs.components.superconducting.qpu.fixed_frequency_qpu import QuAM as FixedFrequencyQuAM
-from enum import Enum
 
-class Architecture(Enum):
-    FIXED_FREQUENCY = 1
-    FLUX_TUNABLE = 2
-    CUSTOM = 3
 
-get_quam = {
-    Architecture.FLUX_TUNABLE: FluxTunableQuAM(),
-    Architecture.FIXED_FREQUENCY: FixedFrequencyQuAM(),
-}
+BaseQuAM = FluxTunableQuAM
+# BaseQuAM = FixedFrequencyQuAM
 
-QuAM = get_quam[Architecture.FLUX_TUNABLE]
+# BaseQuAM = BaseQuAM  # use this for a clean-slate, custom QuAM
+
+class QuAM(BaseQuAM):
+    pass
