f630962792810e3cb22086698b356acd153164d7
Michal Goldenshtein <michal@quantum-machines.co>
2025-02-18 17:22:36 -0600

    add waveform report to SC/3D-storage-cavity

--- Quantum-Control-Applications/Superconducting/3D-storage-cavity/06b_storage_AC_Stark_shift_from_readout_pumping.py
+++ Quantum-Control-Applications/Superconducting/3D-storage-cavity/06b_storage_AC_Stark_shift_from_readout_pumping.py
@@ -99,9 +99,16 @@ simulate = True
 if simulate:
     # Simulates the QUA program for the specified duration
     simulation_config = SimulationConfig(duration=10_000)  # In clock cycles = 4ns
+    # Simulate blocks python until the simulation is done
     job = qmm.simulate(config, storage_AC_stark_shift, simulation_config)
-    job.get_simulated_samples().con1.plot()
-    plt.show()
+    # Get the simulated samples
+    samples = job.get_simulated_samples()
+    # Get the waveform report object
+    waveform_report = job.get_simulated_waveform_report()
+    # Cast the waveform report to a python dictionary
+    waveform_dict = waveform_report.to_dict()
+    # Visualize and save the waveform report
+    waveform_report.create_plot(samples, plot=True, save_path="./")
 else:
     # Open the quantum machine
     qm = qmm.open_qm(config)
