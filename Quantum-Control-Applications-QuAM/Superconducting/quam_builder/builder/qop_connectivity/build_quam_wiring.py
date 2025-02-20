039d33a721f2c34a1995bbc93d79f725946dd6d7
Dean Poulos <dean.poulos7@gmail.com>
2025-02-20 13:50:38 +1100

    Change to AnyQuAM.

--- Quantum-Control-Applications-QuAM/Superconducting/quam_builder/builder/qop_connectivity/build_quam_wiring.py
+++ Quantum-Control-Applications-QuAM/Superconducting/quam_builder/builder/qop_connectivity/build_quam_wiring.py
@@ -1,6 +1,6 @@
 from typing import Union
 from qualang_tools.wirer import Connectivity
-from quam_builder.architecture.superconducting.qpu import BaseQuAM, FixedFrequencyQuAM, FluxTunableQuAM
+from quam_builder.architecture.superconducting.qpu import AnyQuAM
 from quam_builder.builder.qop_connectivity.create_wiring import create_wiring
 from quam.components.ports import (
     FEMPortsContainer,
@@ -8,11 +8,9 @@ from quam.components.ports import (
 )
 
 
-QuAMType = Union[BaseQuAM, FixedFrequencyQuAM, FluxTunableQuAM]
-
 def build_quam_wiring(
     connectivity: Connectivity, host_ip: str, cluster_name: str,
-    quam_instance: QuAMType, port: int = None
+    quam_instance: AnyQuAM, port: int = None
 ):
 
     machine = quam_instance
@@ -22,7 +20,7 @@ def build_quam_wiring(
     save_machine(machine)
 
 
-def add_ports_container(connectivity: Connectivity, machine: QuAMType):
+def add_ports_container(connectivity: Connectivity, machine: AnyQuAM):
     """
     Detects whether the `connectivity` is using OPX+ or OPX1000 and returns
     the corresponding base object. Otherwise, raises a TypeError.
@@ -37,12 +35,12 @@ def add_ports_container(connectivity: Connectivity, machine: QuAMType):
 
 
 
-def add_name_and_ip(machine: QuAMType, host_ip: str, cluster_name: str, port: Union[int, None]):
+def add_name_and_ip(machine: AnyQuAM, host_ip: str, cluster_name: str, port: Union[int, None]):
     """Stores the minimal information to connect to a QuantumMachinesManager."""
     machine.network = {"host": host_ip, "port": port, "cluster_name": cluster_name}
 
 
-def save_machine(machine: QuAMType):
+def save_machine(machine: AnyQuAM):
     machine.save(
         content_mapping={
             "wiring.json": ["network", "wiring"],
