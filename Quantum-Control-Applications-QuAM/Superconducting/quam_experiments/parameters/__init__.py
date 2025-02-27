cb107eb8ce001b6bbe56cda20ac55b24b2d3f735
TheoQM <theo@quantum-machines.co>
2025-02-27 14:49:19 +0100

    refactor the parameters

--- Quantum-Control-Applications-QuAM/Superconducting/quam_experiments/parameters/__init__.py
+++ Quantum-Control-Applications-QuAM/Superconducting/quam_experiments/parameters/__init__.py
@@ -1,15 +1,9 @@
-from .data_loadable import DataLoadableNodeParameters
+from .common_parameters import CommonNodeParameters
 from .flux_controlled import FluxControlledNodeParameters
-from .multiplexable import MultiplexableNodeParameters
-from .qm_session import QmSessionNodeParameters
 from .qubits_experiment import QubitsExperimentNodeParameters
-from .simulatable import SimulatableNodeParameters
 
 __all__ = [
-    "DataLoadableNodeParameters",
+    "CommonNodeParameters",
     "FluxControlledNodeParameters",
-    "MultiplexableNodeParameters",
-    "QmSessionNodeParameters",
     "QubitsExperimentNodeParameters",
-    "SimulatableNodeParameters",
 ]
---
+++ Quantum-Control-Applications-QuAM/Superconducting/quam_experiments/parameters/common_parameters.py
@@ -0,0 +1,22 @@
+from typing import Optional
+from pydantic import Field
+from qualibrate.parameters import RunnableParameters
+
+
+class CommonNodeParameters(RunnableParameters):
+    load_data_id: Optional[int] = Field(
+        default=None, description="Qualibrate node run index for loading historical data."
+    )
+    simulate: bool = Field(
+        default=False, description="Simulate the waveforms on the OPX instead of executing the program."
+    )
+    simulation_duration_ns: int = Field(
+        default=50_000, description="Duration over which the simulation will collect samples (in nanoseconds)."
+    )
+    use_waveform_report: bool = Field(
+        default=True, description="Whether to use the interactive waveform report in simulation."
+    )
+    timeout: int = Field(
+        default=120,
+        description="Waiting time for the OPX resources to become available before giving up (in seconds).",
+    )
