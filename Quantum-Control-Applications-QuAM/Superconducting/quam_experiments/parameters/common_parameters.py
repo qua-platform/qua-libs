7b7b8640f0a5afd002f8561778d7c1b37383f925
TheoQM <theo@quantum-machines.co>
2025-02-28 15:24:48 +0100

    CommonParameters

--- Quantum-Control-Applications-QuAM/Superconducting/quam_experiments/parameters/common_parameters.py
+++ Quantum-Control-Applications-QuAM/Superconducting/quam_experiments/parameters/common_parameters.py
@@ -4,6 +4,16 @@ from qualibrate.parameters import RunnableParameters
 
 
 class CommonNodeParameters(RunnableParameters):
+    """
+    Common parameters for configuring a node in a quantum machine simulation or execution.
+
+    Attributes:
+    simulate (bool): Simulate the waveforms on the OPX instead of executing the program. Default is False.
+    simulation_duration_ns (int): Duration over which the simulation will collect samples (in nanoseconds). Default is 50,000 ns.
+    use_waveform_report (bool): Whether to use the interactive waveform report in simulation. Default is True.
+    timeout (int): Waiting time for the OPX resources to become available before giving up (in seconds). Default is 120 seconds.
+    load_data_id (Optional[int]): Qualibrate node run index for loading historical data. Default is None.
+    """
     simulate: bool = Field(
         default=False, description="Simulate the waveforms on the OPX instead of executing the program."
     )
