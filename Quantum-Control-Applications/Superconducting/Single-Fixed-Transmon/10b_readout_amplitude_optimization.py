c80efde7b6d7839050dd62be7781955e3ec01295
Michal Goldenshtein <michal@quantum-machines.co>
2025-03-03 12:31:54 -0600

    modify SC/single fixed transmon

--- Quantum-Control-Applications/Superconducting/Single-Fixed-Transmon/10b_readout_amplitude_optimization.py
+++ Quantum-Control-Applications/Superconducting/Single-Fixed-Transmon/10b_readout_amplitude_optimization.py
@@ -158,7 +158,7 @@ else:
         ground_fidelity_vec.append(gg)
 
     # Plot the data
-    plt.figure()
+    fig = plt.figure()
     plt.plot(amplitudes * readout_amp, fidelity_vec, "b.-", label="averaged fidelity")
     plt.plot(amplitudes * readout_amp, ground_fidelity_vec, "r.-", label="ground fidelity")
     plt.title("Readout amplitude optimization")
@@ -179,8 +179,10 @@ else:
     # Save results
     script_name = Path(__file__).name
     data_handler = DataHandler(root_data_folder=save_dir)
-    save_data_dict.update({"I_data": I})
-    save_data_dict.update({"Q_data": Q})
+    save_data_dict.update({"Ig_data": I_g})
+    save_data_dict.update({"Qg_data": Q_g})
+    save_data_dict.update({"Ie_data": I_e})
+    save_data_dict.update({"Qe_data": Q_e})
     save_data_dict.update({"fig_live": fig})
     data_handler.additional_files = {script_name: script_name, **default_additional_files}
     data_handler.save_data(data=save_data_dict, name="_".join(script_name.split("_")[1:]).split(".")[0])
