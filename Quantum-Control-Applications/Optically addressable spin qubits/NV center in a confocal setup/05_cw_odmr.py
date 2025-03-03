02c43aebad813b1e9a6b40d18ddc9fa3206a015f
Michal Goldenshtein <michal@quantum-machines.co>
2025-03-03 11:50:40 -0600

    add I and Q savings to Data_handler/QD and optically addressable spin qubits

--- Quantum-Control-Applications/Optically addressable spin qubits/NV center in a confocal setup/05_cw_odmr.py
+++ Quantum-Control-Applications/Optically addressable spin qubits/NV center in a confocal setup/05_cw_odmr.py
@@ -140,6 +140,8 @@ else:
     # Save results
     script_name = Path(__file__).name
     data_handler = DataHandler(root_data_folder=save_dir)
+    save_data_dict.update({"I_data": I})
+    save_data_dict.update({"Q_data": Q})
     save_data_dict.update({"fig_live": fig})
     data_handler.additional_files = {script_name: script_name, **default_additional_files}
     data_handler.save_data(data=save_data_dict, name="_".join(script_name.split("_")[1:]).split(".")[0])
