3b3a4a670857abf6b80374e463d1445545f69dcd
Arthur Strauss <arthurostrauss@gmail.com>
2025-03-06 15:49:34 +0100

    Removed wrong property statement on Transmon.sigma()

--- Quantum-Control-Applications-QuAM/Superconducting/quam_libs/components/transmon.py
+++ Quantum-Control-Applications-QuAM/Superconducting/quam_libs/components/transmon.py
@@ -92,7 +92,6 @@ class Transmon(QuamComponent):
             raise AttributeError(f"Error inferring anharmonicity for channel {name}: {self.f_12=} is not a number")
         return self.f_12 - self.f_01
 
-    @property
     def sigma(self, operation: Pulse):
         return operation.length / self.sigma_time_factor
 
