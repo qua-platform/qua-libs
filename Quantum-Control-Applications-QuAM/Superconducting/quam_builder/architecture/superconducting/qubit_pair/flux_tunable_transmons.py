8b35859c35cd9ad855ca8bd4e7d46424a867ed22
TheoQM <theo@quantum-machines.co>
2025-03-06 16:08:33 +0100

    update the flux transmon pair

--- Quantum-Control-Applications-QuAM/Superconducting/quam_builder/architecture/superconducting/qubit_pair/flux_tunable_transmons.py
+++ Quantum-Control-Applications-QuAM/Superconducting/quam_builder/architecture/superconducting/qubit_pair/flux_tunable_transmons.py
@@ -1,4 +1,4 @@
-from typing import Dict, Any, Optional, Union
+from typing import Dict, Any, Optional, Union, List
 from dataclasses import field
 from qm.qua import align, wait
 
@@ -16,7 +16,7 @@ class FluxTunableTransmonPair(QuamComponent):
     qubit_control: FluxTunableTransmon = None
     qubit_target: FluxTunableTransmon = None
     coupler: Optional[TunableCoupler] = None
-
+    mutual_flux_bias: List[float] = field(default_factory=lambda: [0, 0])
     extras: Dict[str, Any] = field(default_factory=dict)
 
     @property
@@ -25,22 +25,38 @@ class FluxTunableTransmonPair(QuamComponent):
         return self.id if isinstance(self.id, str) else f"q{self.qubit_control.id}-{self.qubit_target.id}"
 
     def align(self):
-        align(
-            self.qubit_control.xy.name,
-            self.qubit_control.z.name,
-            self.qubit_control.resonator.name,
-            self.qubit_target.xy.name,
-            self.qubit_target.z.name,
-            self.qubit_target.resonator.name,
-        )
+        channels = [self.qubit_control.xy.name, self.qubit_control.z.name, self.qubit_control.resonator.name,
+                    self.qubit_target.xy.name,
+                    self.qubit_target.z.name, self.qubit_target.resonator.name]
+
+        if self.coupler:
+            channels += [self.coupler.name]
+
+        if "Cz" in self.gates:
+            if hasattr(self.gates['Cz'], 'compensations'):
+                for compensation in self.gates['Cz'].compensations:
+                    channels += [compensation["qubit"].xy.name, compensation["qubit"].z.name,
+                                 compensation["qubit"].resonator.name]
+
+        align(*channels)
 
     def wait(self, duration):
-        wait(
-            duration,
-            self.qubit_control.xy.name,
-            self.qubit_control.z.name,
-            self.qubit_control.resonator.name,
-            self.qubit_target.xy.name,
-            self.qubit_target.z.name,
-            self.qubit_target.resonator.name,
-        )
+        channels = [self.qubit_control.xy.name, self.qubit_control.z.name, self.qubit_control.resonator.name,
+                    self.qubit_target.xy.name,
+                    self.qubit_target.z.name, self.qubit_target.resonator.name]
+
+        if self.coupler:
+            channels += [self.coupler.name]
+
+        if "Cz" in self.gates:
+            if hasattr(self.gates['Cz'], 'compensations'):
+                for compensation in self.gates['Cz'].compensations:
+                    channels += [compensation["qubit"].xy.name, compensation["qubit"].z.name,
+                                 compensation["qubit"].resonator.name]
+
+        wait(duration, *channels)
+
+    def to_mutual_idle(self):
+        """Set the flux bias to the mutual idle offset"""
+        self.qubit_control.z.set_dc_offset(self.mutual_flux_bias[0])
+        self.qubit_target.z.set_dc_offset(self.mutual_flux_bias[1])
