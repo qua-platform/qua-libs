6e04fc1a1e3eeeccc1413793f0776e68f1309a97
TheoQM <theo@quantum-machines.co>
2025-03-07 15:18:22 +0100

    base transmon docstrings

--- Quantum-Control-Applications-QuAM/Superconducting/quam_builder/architecture/superconducting/qubit/base_transmon.py
+++ Quantum-Control-Applications-QuAM/Superconducting/quam_builder/architecture/superconducting/qubit/base_transmon.py
@@ -1,3 +1,7 @@
+from typing import Dict, Any, Union, Optional, Literal
+from dataclasses import field
+from logging import getLogger
+
 from quam.core import quam_dataclass
 from quam.components.channels import IQChannel, MWChannel, Pulse
 from quam import QuamComponent
@@ -5,6 +9,7 @@ from quam_builder.architecture.superconducting.components.readout_resonator impo
     ReadoutResonatorIQ,
     ReadoutResonatorMW,
 )
+
 from qualang_tools.octave_tools import octave_calibration_tool
 from qm import QuantumMachine, logger
 from qm.qua import (
@@ -21,9 +26,7 @@ from qm.qua import (
     Math,
     Cast,
 )
-from typing import Dict, Any, Union, Optional, Literal
-from dataclasses import field
-from logging import getLogger
+
 
 __all__ = ["BaseTransmon"]
 
@@ -34,19 +37,37 @@ class BaseTransmon(QuamComponent):
     Example QuAM component for a transmon qubit.
 
     Attributes:
-        id (str, int): The id of the Transmon, used to generate the name.
-            Can be a string, or an integer in which case it will add`Channel._default_label`.
-        xy (IQChannel): The xy drive component.
-        resonator (ReadoutResonator): The readout resonator component.
-        T1 (float): The transmon T1 in s.
-        T2ramsey (float): The transmon T2* in s.
-        T2echo (float): The transmon T2 in s.
-        thermalization_time_factor (int): thermalization time in units of T1.
-        anharmonicity (int, float): the transmon anharmonicity in Hz.
-        sigma_time_factor:
-        GEF_frequency_shift (int):
-        chi (float):
-        grid_location (str): qubit location in the plot grid as "(column, row)"
+        id (Union[int, str]): The id of the Transmon, used to generate the name.
+            Can be a string, or an integer in which case it will add `Channel._default_label`.
+        xy (Union[MWChannel, IQChannel]): The xy drive component.
+        resonator (Union[ReadoutResonatorIQ, ReadoutResonatorMW]): The readout resonator component.
+        f_01 (float): The 0-1 transition frequency in Hz.
+        f_12 (float): The 1-2 transition frequency in Hz.
+        anharmonicity (int): The transmon anharmonicity in Hz. Default is 150e6.
+        T1 (float): The transmon T1 in seconds. Default is 10e-6.
+        T2ramsey (float): The transmon T2* in seconds.
+        T2echo (float): The transmon T2 in seconds.
+        thermalization_time_factor (int): Thermalization time in units of T1. Default is 5.
+        sigma_time_factor (int): Sigma time factor for pulse shaping. Default is 5.
+        GEF_frequency_shift (int): The frequency shift for the GEF states. Default is 10.
+        chi (float): The dispersive shift in Hz. Default is 0.0.
+        grid_location (str): Qubit location in the plot grid as "(column, row)".
+        extras (Dict[str, Any]): Additional attributes for the transmon.
+
+    Methods:
+        name: Returns the name of the transmon.
+        inferred_f_12: Returns the 0-2 (e-f) transition frequency in Hz, derived from f_01 and anharmonicity.
+        inferred_anharmonicity: Returns the transmon anharmonicity in Hz, derived from f_01 and f_12.
+        sigma: Returns the sigma value for a given pulse.
+        thermalization_time: Returns the transmon thermalization time in ns.
+        calibrate_octave: Calibrates the Octave channels (xy and resonator) linked to this transmon.
+        set_gate_shape: Sets the shape of the single qubit gates.
+        readout_state: Performs a readout of the qubit state using the specified pulse.
+        reset_qubit: Reset the qubit to the ground state ('g') with the specified method.
+        reset_qubit_thermal: Reset the qubit to the ground state ('g') using thermalization.
+        reset_qubit_active: Reset the qubit to the ground state ('g') using active reset.
+        reset_qubit_active_gef: Reset the qubit to the ground state ('g') using active reset with GEF state readout.
+        readout_state_gef: Perform a GEF state readout using the specified pulse and update the state variable.
     """
 
     id: Union[int, str]
@@ -117,7 +138,7 @@ class BaseTransmon(QuamComponent):
         """The transmon thermalization time in ns."""
         return int(self.thermalization_time_factor * self.T1 * 1e9 / 4) * 4
 
-    #todo: do we really want this one here?
+    # todo: do we really want this one here?
     def calibrate_octave(
         self, QM: QuantumMachine, calibrate_drive: bool = True, calibrate_resonator: bool = True
     ) -> None:
@@ -139,7 +160,9 @@ class BaseTransmon(QuamComponent):
                     intermediate_frequencies=self.resonator.intermediate_frequency,
                 )
             else:
-                raise RuntimeError(f"{self.resonator.name} doesn't have a 'frequency_converter_up' attribute, it is thus most likely not connected to an Octave.")
+                raise RuntimeError(
+                    f"{self.resonator.name} doesn't have a 'frequency_converter_up' attribute, it is thus most likely not connected to an Octave."
+                )
         if calibrate_drive and self.xy is not None:
             if hasattr(self.xy, "frequency_converter_up"):
                 logger.info(f"Calibrating {self.xy.name}")
@@ -150,14 +173,14 @@ class BaseTransmon(QuamComponent):
                     intermediate_frequencies=self.xy.intermediate_frequency,
                 )
             else:
-                raise RuntimeError(f"{self.xy.name} doesn't have a 'frequency_converter_up' attribute, it is thus most likely not connected to an Octave.")
-
+                raise RuntimeError(
+                    f"{self.xy.name} doesn't have a 'frequency_converter_up' attribute, it is thus most likely not connected to an Octave."
+                )
 
     def set_gate_shape(self, gate_shape: str) -> None:
         """Set the shape fo the single qubit gates defined as ["x180", "x90" "-x90", "y180", "y90", "-y90"]"""
         for gate in ["x180", "x90", "-x90", "y180", "y90", "-y90"]:
             self.xy.operations[gate] = f"#./{gate}_{gate_shape}"
-
 
     def readout_state(self, state, pulse_name: str = "readout", threshold: float = None):
         """
@@ -240,6 +263,25 @@ class BaseTransmon(QuamComponent):
         readout_pulse_name: str = "readout",
         max_attempts: int = 15,
     ):
+        """
+        Perform an active reset of the qubit.
+
+        This function performs an active reset of the qubit by repeatedly measuring the qubit state and applying a pi pulse
+        until the qubit is in the ground state or the maximum number of attempts is reached.
+
+        Args:
+            save_qua_var (Optional[StreamType]): The QUA variable to save the number of attempts to.
+            pi_pulse_name (str): The name of the pi pulse to use for the reset. Default is "x180".
+            readout_pulse_name (str): The name of the readout pulse to use for measuring the qubit state. Default is "readout".
+            max_attempts (int): The maximum number of attempts to reset the qubit. Default is 15.
+
+        Returns:
+            None
+
+        The function measures the qubit state using the specified readout pulse, applies a pi pulse if the qubit is not in the ground state,
+        and repeats this process until the qubit is in the ground state or the maximum number of attempts is reached.
+        If `save_qua_var` is provided, the number of attempts is saved to this variable.
+        """
         pulse = self.resonator.operations[readout_pulse_name]
 
         I = declare(fixed)
