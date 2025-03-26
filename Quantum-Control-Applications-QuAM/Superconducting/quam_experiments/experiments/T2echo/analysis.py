729dae159e1b2cd295d76ce3f3f1aca580170c60
TheoQM <theo@quantum-machines.co>
2025-03-26 16:47:47 +0100

    T2echo

---
+++ Quantum-Control-Applications-QuAM/Superconducting/quam_experiments/experiments/T2echo/analysis.py
@@ -0,0 +1,120 @@
+import logging
+from dataclasses import dataclass
+from typing import Optional, Tuple, Dict
+import numpy as np
+import xarray as xr
+
+from qualibrate import QualibrationNode
+from quam_libs.qua_datasets import add_amplitude_and_phase, convert_IQ_to_V
+from quam_experiments.analysis.fit import peaks_dips
+from quam_config.instrument_limits import instrument_limits
+
+
+@dataclass
+class FitParameters:
+    """Stores the relevant qubit spectroscopy experiment fit parameters for a single qubit"""
+
+    success: bool
+    qubit_name: Optional[str] = ""
+
+
+def log_fitted_results(fit_results: Dict, logger=None):
+    """
+    Logs the node-specific fitted results for all qubits from the fit xarray Dataset.
+
+    Parameters:
+    -----------
+    ds : xr.Dataset
+        Dataset containing the fitted results for all qubits.
+    logger : logging.Logger, optional
+        Logger for logging the fitted results. If None, a default logger is used.
+    """
+    if logger is None:
+        logger = logging.getLogger(__name__)
+    # for q in fit_results.keys():
+    #     s_qubit = f"Results for qubit {q}: "
+    #     s_freq = f"\tQubit frequency: {1e-9 * fit_results[q]['frequency']:.3f} GHz | "
+    #     s_fwhm = f"FWHM: {1e-3 * fit_results[q]['fwhm']:.1f} kHz | "
+    #     s_angle = (
+    #         f"The integration weight angle: {fit_results[q]['iw_angle']:.3f} rad\n "
+    #     )
+    #     s_saturation = f"To get the desired FWHM, the saturation amplitude is updated to: {1e3 * fit_results[q]['saturation_amp']:.1f} mV | "
+    #     s_x180 = f"To get the desired x180 gate, the x180 amplitude is updated to: {1e3 * fit_results[q]['x180_amp']:.1f} mV\n "
+    #     if fit_results[q]["success"]:
+    #         s_qubit += " SUCCESS!\n"
+    #     else:
+    #         s_qubit += " FAIL!\n"
+    #     logger.info(
+    #         s_qubit + s_freq + s_fwhm + s_freq + s_angle + s_saturation + s_x180
+    #     )
+    pass
+
+
+def process_raw_dataset(ds: xr.Dataset, node: QualibrationNode):
+    # ds = convert_IQ_to_V(ds, node.namespace["qubits"])
+    # ds = add_amplitude_and_phase(ds, "detuning", subtract_slope_flag=True)
+    # full_freq = np.array(
+    #     [ds.detuning + q.xy.RF_frequency for q in node.namespace["qubits"]]
+    # )
+    # ds = ds.assign_coords(full_freq=(["qubit", "detuning"], full_freq))
+    # ds.full_freq.attrs = {"long_name": "RF frequency", "units": "Hz"}
+    pass
+    return ds
+
+
+def fit_raw_data(
+    ds: xr.Dataset, node: QualibrationNode
+) -> Tuple[xr.Dataset, dict[str, FitParameters]]:
+    """
+    Fit the qubit frequency and FWHM for each qubit in the dataset.
+
+    Parameters:
+    -----------
+    ds : xr.Dataset
+        Dataset containing the raw data.
+    node_parameters : Parameters
+        Parameters related to the node, including whether state discrimination is used.
+
+    Returns:
+    --------
+    xr.Dataset
+        Dataset containing the fit results.
+    """
+    ds_fit = ds
+    # # Fit the exponential decay
+    # if node.parameters.use_state_discrimination:
+    #     fit_data = fit_decay_exp(ds.state, "idle_time")
+    # else:
+    #     fit_data = fit_decay_exp(ds.I, "idle_time")
+    # fit_data.attrs = {"long_name": "time", "units": "µs"}
+    # # Fitted decay
+    # fitted = decay_exp(
+    #     ds.idle_time,
+    #     fit_data.sel(fit_vals="a"),
+    #     fit_data.sel(fit_vals="offset"),
+    #     fit_data.sel(fit_vals="decay"),
+    # )
+    # # Decay rate and its uncertainty
+    # decay = fit_data.sel(fit_vals="decay")
+    # decay.attrs = {"long_name": "decay", "units": "ns"}
+    # decay_res = fit_data.sel(fit_vals="decay_decay")
+    # decay_res.attrs = {"long_name": "decay", "units": "ns"}
+    # # T1 and its uncertainty
+    # tau = -1 / fit_data.sel(fit_vals="decay")
+    # tau.attrs = {"long_name": "T1", "units": "µs"}
+    # tau_error = -tau * (np.sqrt(decay_res) / decay)
+    # tau_error.attrs = {"long_name": "T1 error", "units": "µs"}
+    # return fit_data, fit_results
+    fit_results = {
+        q: FitParameters(
+            success=False,
+        )
+        for q in ds_fit.qubit.values
+    }
+    return ds_fit, fit_results
+
+
+def _extract_relevant_fit_parameters(fit: xr.Dataset, node: QualibrationNode):
+    """Add metadata to the dataset and fit results."""
+    pass
+    # return fit, fit_results
