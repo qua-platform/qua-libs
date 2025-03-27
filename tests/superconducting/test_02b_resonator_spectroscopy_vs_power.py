859c61059543292c33c19e911f1a823d698d800c
Serwan Asaad <serwan.asaad@quantum-machines.co>
2025-03-27 14:24:12 +0100

    first 3 tests working

--- tests/superconducting/test_02b_resonator_spectroscopy_vs_power.py
+++ tests/superconducting/test_02b_resonator_spectroscopy_vs_power.py
@@ -3,9 +3,9 @@ from qualibrate import QualibrationLibrary
 
 
 def test_02b_resonator_spectroscopy_vs_power(library: QualibrationLibrary):
-    node = library.nodes["02b_resonator_spectroscopy_vs_power"]
+    node_template = library.nodes["02b_resonator_spectroscopy_vs_power"]
 
-    executed_node, run_summary = node.run(load_data_id=1582)
+    executed_node, run_summary = node_template.run(load_data_id=1582)
 
     fit_results = executed_node.results["fit_results"]
     # TODO Why is this unsuccessful?
@@ -13,4 +13,3 @@ def test_02b_resonator_spectroscopy_vs_power(library: QualibrationLibrary):
     # assert np.isnan(fit_results["q1"]["resonator_frequency"])
     assert fit_results["q3"]["success"]
     assert np.isclose(fit_results["q3"]["resonator_frequency"], 9951200000.0)
-    print("finished")
