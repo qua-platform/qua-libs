f716a9a1e933f253a0871c6446e6043b93db01fc
paulQM <paul.jamet@quantum-machines.co>
2025-03-28 11:24:00 +0100

    add cloud infrastructure addons

--- Quantum-Control-Applications-QuAM/Superconducting/quam_config/my_quam.py
+++ Quantum-Control-Applications-QuAM/Superconducting/quam_config/my_quam.py
@@ -1,8 +1,11 @@
 # from quam_builder.architecture.superconducting.qpu import BaseQuAM
+# from quam_builder.architecture.superconducting.qpu import FixedFrequencyQuAM
+from typing import ClassVar, Optional
+
+from qm import QuantumMachinesManager
 from quam_builder.architecture.superconducting.qpu import FluxTunableQuAM
 
-# from quam_builder.architecture.superconducting.qpu import FixedFrequencyQuAM
-
+from .cloud_infrastructure import CloudQuantumMachinesManager
 
 BaseQuAM = FluxTunableQuAM
 # BaseQuAM = FixedFrequencyQuAM
@@ -11,4 +14,26 @@ BaseQuAM = FluxTunableQuAM
 
 
 class QuAM(BaseQuAM):
-    pass
+
+    qmm: ClassVar[Optional[QuantumMachinesManager]] = None
+
+    def connect(self) -> QuantumMachinesManager:
+        """Open a Quantum Machine Manager with the credentials ("host" and "cluster_name") as defined in the network file.
+
+        Returns: the opened Quantum Machine Manager.
+        """
+        if self.network.get("cloud", False):
+            self.qmm = CloudQuantumMachinesManager(self.network["quantum_computer_backend"])
+        else:
+            settings = dict(
+                host=self.network["host"],
+                cluster_name=self.network["cluster_name"],
+                octave=self.get_octave_config(),
+            )
+
+            if "port" in self.network:
+                settings["port"] = self.network["port"]
+
+            self.qmm = QuantumMachinesManager(**settings)
+
+        return self.qmm
