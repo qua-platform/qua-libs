6c4fafed400020dd3f0e701c1beaf8c162d5ac3c
TheoQM <theo@quantum-machines.co>
2025-04-08 10:17:39 +0200

    close all qms

--- Quantum-Control-Applications-QUAM/Superconducting/calibration_graph/00_close_other_qms.py
+++ Quantum-Control-Applications-QUAM/Superconducting/calibration_graph/00_close_other_qms.py
@@ -7,25 +7,18 @@ description = """
         CLOSE ALL OTHER QMs.
 """
 
-
 node = QualibrationNode[NodeParameters, Quam](
     name="00_close_other_qms", description=description, parameters=NodeParameters()
 )
 
 
-# Any parameters that should change for debugging purposes only should go in here
-# These parameters are ignored when run through the GUI or as part of a graph
-@node.run_action(skip_if=node.modes.external)
-def custom_param(node: QualibrationNode[NodeParameters, Quam]):
-    # You can get type hinting in your IDE by typing node.parameters.
-    pass
-
-
 # Instantiate the QUAM class from the state file
 node.machine = Quam.load()
 
-# Generate the OPX and Octave configurations
-config = node.machine.generate_config()
-# Open Communication with the QOP
-qmm = node.machine.connect()
-qmm.close_all_qms()
+
+# %% {Close_all_quantum_machines}
+@node.run_action()
+def close_all_quantum_machines(node: QualibrationNode[NodeParameters, Quam]):
+    """Closes all the opened quantum machines."""
+    qmm = node.machine.connect()
+    qmm.close_all_qms()
