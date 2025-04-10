d8dc4922a2f328ef756e3c6b807501eb2ac137a5
TheoQM <theo@quantum-machines.co>
2025-04-10 14:48:48 +0200

    change default qubits for testing

--- Quantum-Control-Applications-QUAM/Superconducting/calibration_graph/02a_resonator_spectroscopy.py
+++ Quantum-Control-Applications-QUAM/Superconducting/calibration_graph/02a_resonator_spectroscopy.py
@@ -58,7 +58,7 @@ node = QualibrationNode[Parameters, Quam](
 def custom_param(node: QualibrationNode[Parameters, Quam]):
     """Allow the user to locally set the node parameters for debugging purposes, or execution in the Python IDE."""
     # You can get type hinting in your IDE by typing node.parameters.
-    node.parameters.qubits = ["q1", "q3"]
+    node.parameters.qubits = ["q1", "q2"]
     pass
 
 
