d6fb60b69df1461bfc41155fb9c6608c89918648
omrieoqm <omrie.ovdat@quantum-machines.co>
2025-04-15 14:28:49 +0300

    raw adc + IQ

--- Quantum-Control-Applications-QuAM/Superconducting/random_exp/000_raw_adc_readout_01.py
+++ Quantum-Control-Applications-QuAM/Superconducting/random_exp/000_raw_adc_readout_01.py
@@ -54,26 +54,41 @@ qmm = machine.connect()
 # %% {QUA_program}
 with program() as raw_trace_prog:
     n = declare(int)  # QUA variable for the averaging loop
+    
+    I = [declare(fixed) for _ in range(num_qubits)]
+    Q = [declare(fixed) for _ in range(num_qubits)]
+    I_st = [declare_stream() for _ in range(num_qubits)]
+    Q_st = [declare_stream() for _ in range(num_qubits)]
+    
+    
     adc_st = [declare_stream(adc_trace=True) for _ in range(len(resonators))]  # The stream to store the raw ADC trace
 
     for i, qubit in enumerate(qubits):
         # Wait for the qubits to decay to the ground state
         with for_(n, 0, n < node.parameters.num_averages, n + 1):
+            
+            qubit.wait(4* qubit.thermalization_time * u.ns)
+            qubit.align()
             # Reset the phase of the digital oscillator associated to the resonator element. Needed to average the cosine signal.
             reset_phase(qubit.resonator.name)
             # Measure the resonator (send a readout pulse and record the raw ADC trace)
-            qubit.resonator.measure("readout", stream=adc_st[i])
-            # Wait for the resonator to deplete
-            qubit.resonator.wait(qubit.thermalization_time * u.ns)
+            qubit.resonator.measure("readout", qua_vars=(I[i], Q[i]), stream=adc_st[i])
+            # save data
+            save(I[i], I_st[i])
+            save(Q[i], Q_st[i])
             # |0> -> |1>
+            qubit.align()
+            qubit.wait(qubit.thermalization_time * u.ns)
+            qubit.align()
             qubit.xy.play("x180")
             qubit.align()
             # Reset the phase of the digital oscillator associated to the resonator element. Needed to average the cosine signal.
             reset_phase(qubit.resonator.name)
             # Measure the resonator (send a readout pulse and record the raw ADC trace)
-            qubit.resonator.measure("readout", stream=adc_st[i])
-            # Wait for the resonator to deplete
-            qubit.resonator.wait(qubit.thermalization_time * u.ns)
+            qubit.resonator.measure("readout", qua_vars=(I[i], Q[i]), stream=adc_st[i])
+            # save data
+            save(I[i], I_st[i])
+            save(Q[i], Q_st[i])
         # Measure sequentially
         align(*[qubit.resonator.name for qubit in qubits])
         
@@ -83,6 +98,8 @@ with program() as raw_trace_prog:
             # Will save average:
             adc_st[i].input1().real().buffer(2).buffer(node.parameters.num_averages).save(f"adcI{i + 1}")
             adc_st[i].input1().image().buffer(2).buffer(node.parameters.num_averages).save(f"adcQ{i + 1}")
+            I_st[i].buffer(2).buffer(node.parameters.num_averages).save(f"I{i + 1}")
+            Q_st[i].buffer(2).buffer(node.parameters.num_averages).save(f"Q{i + 1}")
 
 
 # %% {Simulate_or_execute}
@@ -116,19 +133,28 @@ else:
     # Fetch the data from the OPX and convert it into a xarray with corresponding axes (from most inner to outer loop)
     ts = 1
     time_axis = ts * np.arange(0,resonators[0].operations["readout"].length)
-    ds = fetch_results_as_xarray(job.result_handles, qubits, {"time": time_axis, "qubit_init_state": range(2), "shot_index": range(node.parameters.num_averages)})
+    ds_adc = fetch_results_as_xarray(job.result_handles, qubits, {"time": time_axis, "qubit_init_state": range(2), "shot_index": range(node.parameters.num_averages)}, 
+                                 keys = ["adcI1", "adcQ1", "adcI2", "adcQ2"])
     # Convert raw ADC traces into volts
-    ds = ds.assign({key: -ds[key] / 2**12 for key in ("adcI", "adcQ")})
-    ds = ds.assign({"IQ_abs": np.sqrt(ds["adcI"] ** 2 + ds["adcQ"] ** 2)})
+    ds_adc = ds_adc.assign({key: -ds_adc[key] / 2**12 for key in ("adcI", "adcQ")})
+    ds_adc = ds_adc.assign({"IQ_abs": np.sqrt(ds_adc["adcI"] ** 2 + ds_adc["adcQ"] ** 2)})
     # Add the dataset to the node
-    node.results = {"ds": ds}
+    node.results = {"ds_adc": ds_adc}
+    # %%
+    # Fetch the data from the OPX and convert it into a xarray with corresponding axes (from most inner to outer loop)
+    ds_IQ = fetch_results_as_xarray(job.result_handles, qubits, {"qubit_init_state": range(2), "shot_index": range(node.parameters.num_averages)}, 
+                                 keys = ["I1", "Q1", "I2", "Q2"])
+    # Add the dataset to the node
+    node.results = {"ds_IQ": ds_IQ}
 
     # %% {Data_analysis}
 
 # %% {Save_results}
     # Save the xarray dataset to a pickle file
     with open(f"raw_adc_readout_01_{date_time.replace(":","_")}.pkl", 'wb') as f:
-        pickle.dump(ds, f)
+        pickle.dump(ds_adc, f)
+    with open(f"readout_01_IQ_{date_time.replace(":","_")}.pkl", 'wb') as f:
+        pickle.dump(ds_IQ, f)
 
     # # %% {Save_results}
     
