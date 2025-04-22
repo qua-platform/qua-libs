aa5fb02f46cc53fc0b5838e9c31b055c7cd7e428
paulQM <paul.jamet@quantum-machines.co>
2025-04-22 11:55:13 +0200

    ramsey vs flux working

--- Quantum-Control-Applications-QUAM/superconducting/quam_experiments/experiments/ramsey_versus_flux_calibration/parameters.py
+++ Quantum-Control-Applications-QUAM/superconducting/quam_experiments/experiments/ramsey_versus_flux_calibration/parameters.py
@@ -9,18 +9,18 @@ from quam_experiments.parameters import (
 class NodeSpecificParameters(RunnableParameters):
     num_shots: int = 100
     """Number of averages to perform. Default is 100."""
-    frequency_detuning_in_mhz: float = 4.0
-    """Frequency detuning in MHz. Default is 4.0 MHz."""
+    frequency_detuning_in_mhz: float = 1.0
+    """Frequency detuning in MHz. Default is 1.0 MHz."""
     min_wait_time_in_ns: int = 16
     """Minimum wait time in nanoseconds. Default is 16."""
-    max_wait_time_in_ns: int = 2000
-    """Maximum wait time in nanoseconds. Default is 2000."""
-    wait_time_step_in_ns: int = 20
-    """Step size for the wait time scan in nanoseconds. Default is 20."""
-    flux_span: float = 0.02
-    """Span of flux values to sweep in volts. Default is 0.02 V."""
-    flux_step: float = 0.002
-    """Step size for the flux scan in volts. Default is 0.002 V."""
+    max_wait_time_in_ns: int = 5000
+    """Maximum wait time in nanoseconds. Default is 5000."""
+    wait_time_step_in_ns: int = 60
+    """Step size for the wait time scan in nanoseconds. Default is 60."""
+    flux_span: float = 0.01
+    """Span of flux values to sweep in volts. Default is 0.01 V."""
+    flux_num: int = 21
+    """Number of flux points to sample. Default is 21."""
 
 
 class Parameters(
--- Quantum-Control-Applications-QUAM/superconducting/quam_experiments/experiments/ramsey_versus_flux_calibration/plotting.py
+++ Quantum-Control-Applications-QUAM/superconducting/quam_experiments/experiments/ramsey_versus_flux_calibration/plotting.py
@@ -1,12 +1,13 @@
 from typing import List
+
+import matplotlib.pyplot as plt
 import xarray as xr
 from matplotlib.axes import Axes
 from matplotlib.figure import Figure
-
 from qualang_tools.units import unit
 from qualibration_libs.plot_utils import QubitGrid, grid_iter
-from quam_experiments.analysis.fit import lorentzian_peak
 from quam_builder.architecture.superconducting.qubit import AnyTransmon
+from quam_experiments.analysis.fit import lorentzian_peak
 
 u = unit(coerce_to_integer=True)
 
@@ -38,7 +39,7 @@ def plot_raw_data_with_fit(ds: xr.Dataset, qubits: List[AnyTransmon], fits: xr.D
     for ax, qubit in grid_iter(grid):
         plot_individual_data_with_fit(ax, ds, qubit, fits.sel(qubit=qubit["qubit"]))
 
-    grid.fig.suptitle("Qubit spectroscopy (rotated 'I' quadrature + fit)")
+    grid.fig.suptitle("Ramsey vs flux")
     grid.fig.set_size_inches(15, 9)
     grid.fig.tight_layout()
     return grid.fig
@@ -63,41 +64,12 @@ def plot_individual_data_with_fit(ax: Axes, ds: xr.Dataset, qubit: dict[str, str
     -----
     - If the fit dataset is provided, the fitted curve is plotted along with the raw data.
     """
-    pass
-    # grid_names = [q.grid_location for q in qubits]
-    # grid = QubitGrid(ds, grid_names)
-    # for ax, qubit in grid_iter(grid):
-    #     ds.sel(qubit=qubit["qubit"]).state.plot(ax=ax)
-    #     ax.set_title(qubit["qubit"])
-    #     ax.set_xlabel("Idle_time (uS)")
-    #     ax.set_ylabel(" Flux (V)")
-    # grid.fig.suptitle("Ramsey freq. Vs. flux")
-    # plt.tight_layout()
-    # plt.show()
-    # node.results["figure_raw"] = grid.fig
-    #
-    # grid = QubitGrid(ds, grid_names)
-    # for ax, qubit in grid_iter(grid):
-    #     fitted_freq = (
-    #             fitvals.sel(qubit=qubit["qubit"], degree=2).polyfit_coefficients * flux ** 2
-    #             + fitvals.sel(qubit=qubit["qubit"], degree=1).polyfit_coefficients * flux
-    #             + fitvals.sel(qubit=qubit["qubit"], degree=0).polyfit_coefficients
-    #     )
-    #     frequency.sel(qubit=qubit["qubit"]).plot(marker=".", linewidth=0, ax=ax)
-    #     ax.plot(flux, fitted_freq)
-    #     ax.set_title(qubit["qubit"])
-    #     ax.set_xlabel(" Flux (V)")
-    #     print(
-    #         f"The quad term for {qubit['qubit']} is {a[qubit['qubit']] / 1e9:.3f} GHz/V^2"
-    #     )
-    #     print(
-    #         f"Flux offset for {qubit['qubit']} is {flux_offset[qubit['qubit']] * 1e3:.1f} mV"
-    #     )
-    #     print(
-    #         f"Freq offset for {qubit['qubit']} is {freq_offset[qubit['qubit']] / 1e6:.3f} MHz"
-    #     )
-    #     print()
-    # grid.fig.suptitle("Ramsey freq. Vs. flux")
-    # plt.tight_layout()
-    # plt.show()
-    # node.results["figure"] = grid.fig
+
+    ds.sel(qubit=qubit["qubit"]).state.plot(ax=ax)
+    ax.set_title(qubit["qubit"])
+    ax.set_xlabel("Idle_time (uS)")
+    ax.set_ylabel(" Flux (V)")
+
+    flux_offset = fit.flux_offset
+
+    ax.axhline(flux_offset, color="red", linestyle="--", label="Flux offset")
