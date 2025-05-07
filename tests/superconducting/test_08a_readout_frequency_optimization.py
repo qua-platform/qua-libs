2db14eae99e62a653ea931d0dc0e3b76d9a14383
TheoQM <theo@quantum-machines.co>
2025-05-07 11:39:49 +0200

    remove the tests

--- tests/superconducting/test_08a_readout_frequency_optimization.py
+++
@@ -1,7 +0,0 @@
-from qualibrate import QualibrationLibrary
-
-
-def test_08a_readout_frequency_optimization(library: QualibrationLibrary):
-    node = library.nodes["08a_readout_frequency_optimization"]
-
-    run_summary = node.run(load_data_id=1563, skip_actions=["save_results"])
