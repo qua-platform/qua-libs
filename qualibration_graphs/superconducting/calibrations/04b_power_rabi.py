a935f33a0fe3ebcec5e15aa1678dbe9f2b019855
shanto268 <shanto@usc.edu>
2025-06-09 11:30:06 -0700

    feat: interactive plots for node 04b (updated 04b node)

--- qualibration_graphs/superconducting/calibrations/04b_power_rabi.py
+++ qualibration_graphs/superconducting/calibrations/04b_power_rabi.py
@@ -1,30 +1,26 @@
 # %% {Imports}
+from dataclasses import asdict
+
 import matplotlib.pyplot as plt
 import numpy as np
 import xarray as xr
-from dataclasses import asdict
-
+from calibration_utils.power_rabi import (Parameters, fit_raw_data,
+                                          get_number_of_pulses,
+                                          log_fitted_results,
+                                          plot_raw_data_with_fit,
+                                          plotly_plot_raw_data_with_fit,
+                                          process_raw_dataset)
 from qm.qua import *
-
 from qualang_tools.loops import from_array
 from qualang_tools.multi_user import qm_session
 from qualang_tools.results import progress_counter
 from qualang_tools.units import unit
-
-from qualibrate import QualibrationNode
-from quam_config import Quam
-from calibration_utils.power_rabi import (
-    Parameters,
-    get_number_of_pulses,
-    process_raw_dataset,
-    fit_raw_data,
-    log_fitted_results,
-    plot_raw_data_with_fit,
-)
+from qualibration_libs.data import XarrayDataFetcher
 from qualibration_libs.parameters import get_qubits
 from qualibration_libs.runtime import simulate_and_plot
-from qualibration_libs.data import XarrayDataFetcher
+from quam_config import Quam
 
+from qualibrate import QualibrationNode
 
 # %% {Description}
 description = """
@@ -237,9 +233,12 @@ def plot_data(node: QualibrationNode[Parameters, Quam]):
     """Plot the raw and fitted data in specific figures whose shape is given by qubit.grid_location."""
     fig_raw_fit = plot_raw_data_with_fit(node.results["ds_raw"], node.namespace["qubits"], node.results["ds_fit"])
     plt.show()
+    plot_raw_fit = plotly_plot_raw_data_with_fit(node.results["ds_raw"], node.namespace["qubits"], node.results["ds_fit"])
+    plot_raw_fit.show()
     # Store the generated figures
     node.results["figures"] = {
         "amplitude": fig_raw_fit,
+        "amplitude_plotly": plot_raw_fit,
     }
 
 
