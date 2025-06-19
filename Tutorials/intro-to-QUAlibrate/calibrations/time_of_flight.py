b6e397fe1d751854590c3780797822adca1f7531
Michal Goldenshtein <michal@quantum-machines.co>
2025-06-19 15:42:33 -0700

    starting to modify node atanomy

--- Tutorials/intro-to-QUAlibrate/calibrations/time_of_flight.py
+++ Tutorials/intro-to-QUAlibrate/calibrations/time_of_flight.py
@@ -1,6 +1,8 @@
 # %% {Imports}
 from qm import QuantumMachinesManager
 from qm.qua import *
+from configuration.configuration_with_lf_fem_and_mw_fem import *
+
 from qualang_tools.results import progress_counter, fetching_tool
 from qualibrate import QualibrationNode
 from calibration_utils.time_of_flight import (
@@ -11,7 +13,6 @@ from calibration_utils.time_of_flight import (
     plot_averaged_run_with_fit,
 )
 from qualibration_libs.runtime import simulate_and_plot
-from configuration.configuration_with_lf_fem_and_mw_fem import *
 
 
 description = """
@@ -32,8 +33,6 @@ The data undergoes post-processing to calibrate three distinct parameters:
 
 
 node = QualibrationNode[Parameters, None](name="time_of_flight", description=description, parameters=Parameters())
-
-
 
 # Any parameters that should change for debugging purposes only should go in here
 # These parameters are ignored when run through the GUI or as part of a graph
