ee7bcab3520640bc601f94b59665f3c9f860a038
paulQM <paul.jamet@quantum-machines.co>
2025-07-16 13:13:55 +0200

    initial analysis

--- qualibration_graphs/superconducting/calibrations/12a_pi_vs_flux.py
+++ qualibration_graphs/superconducting/calibrations/12a_pi_vs_flux.py
@@ -32,7 +32,7 @@ from quam.components.pulses import DragGaussianPulse, GaussianPulse
 
 # %% {Node_parameters}
 description = """
-        ....
+Pi pulse vs flux calibration experiment.
 """
 
 # Be sure to include [Parameters, Quam] so the node has proper type hinting
@@ -49,10 +49,9 @@ node = QualibrationNode[Parameters, Quam](
 def custom_param(node: QualibrationNode[Parameters, Quam]):
     """Allow the user to locally set the node parameters for debugging purposes, or execution in the Python IDE."""
     # You can get type hinting in your IDE by typing node.parameters.
-    node.parameters.qubits = ["qD2", "qD1"]
-    node.parameters.num_shots = 100
+    node.parameters.qubits = ["qD4", "qD3"]
+    node.parameters.num_shots = 400
     node.parameters.update_lo = False
-    # node.parameters.update_lo = False
     node.parameters.frequency_span_in_mhz = 200
     node.parameters.frequency_step_in_mhz = 2
     node.parameters.operation_amplitude_factor = 1.0
@@ -61,8 +60,6 @@ def custom_param(node: QualibrationNode[Parameters, Quam]):
     node.parameters.multiplexed = True
     node.parameters.qubit_detuning_in_mhz = 300
     # node.parameters.load_data_id = 1875
-
-    pass
 
 
 # Instantiate the QUAM class from the state file
@@ -117,7 +114,7 @@ def create_qua_program(node: QualibrationNode[Parameters, Quam]):
     span = node.parameters.frequency_span_in_mhz * u.MHz
     step = node.parameters.frequency_step_in_mhz * u.MHz
     dfs = np.arange(-span // 2, span // 2, step, dtype=np.int32)
-    times = np.arange(4, node.parameters.duration_in_ns // 4, 40, dtype=np.int32)
+    times = np.arange(4, node.parameters.duration_in_ns // 4, 60, dtype=np.int32)
 
     detunings = [node.parameters.qubit_detuning_in_mhz * u.MHz for q in qubits]
 
@@ -139,7 +136,7 @@ def create_qua_program(node: QualibrationNode[Parameters, Quam]):
             state_st = [declare_stream() for _ in range(num_qubits)]
         df = declare(int)  # QUA variable for the qubit frequency
         t_delay = declare(int)
-        # duration = node.parameters.duration_in_ns * u.ns
+
 
         for multiplexed_qubits in qubits.batch():
             # Initialize the QPU in terms of flux points (flux tunable transmons and/or tunable couplers)
@@ -150,8 +147,6 @@ def create_qua_program(node: QualibrationNode[Parameters, Quam]):
             with for_(n, 0, n < n_avg, n + 1):
                 save(n, n_st)
                 with for_(*from_array(df, dfs)):
-                    # Update the qubit frequency
-                    # with for_(*from_array(t_delay, times)):
                     with for_each_(t_delay, times):
                         # Qubit initialization
                         for i, qubit in multiplexed_qubits.items():
@@ -166,8 +161,6 @@ def create_qua_program(node: QualibrationNode[Parameters, Quam]):
                                 amplitude_scale=flux_amplitudes[i] / qubit.z.operations["const"].amplitude,
                                 duration=t_delay + 200,
                             )
-                            # Apply saturation pulse to all qubits
-                            # qubit.xy.wait(qubit.z.settle_time * u.ns)
                             qubit.xy.wait(t_delay)
                             qubit.xy.play(operation, amplitude_scale=operation_amp)
                             qubit.xy.update_frequency(qubit.xy.intermediate_frequency)
@@ -248,7 +241,7 @@ def load_data(node: QualibrationNode[Parameters, Quam]):
 # %% {Analyse_data}
 @node.run_action(skip_if=node.parameters.simulate)
 def analyse_data(node: QualibrationNode[Parameters, Quam]):
-    """Analyse the raw data and store the fitted data in another xarray dataset "ds_fit" and the fitted results in the "fit_results" dictionary."""
+    """Analyse the raw data and store the fitted data and results."""
     node.results["ds_raw"] = process_raw_dataset(node.results["ds_raw"], node)
     node.results["ds_fit"], fit_results = fit_raw_data(node.results["ds_raw"], node)
     node.results["fit_results"] = {k: asdict(v) for k, v in fit_results.items()}
@@ -256,8 +249,7 @@ def analyse_data(node: QualibrationNode[Parameters, Quam]):
     # Log the relevant information extracted from the data analysis
     log_fitted_results(fit_results, log_callable=node.log)
     node.outcomes = {
-        qubit_name: ("successful" if fit_result.success else "failed")
-        for qubit_name, fit_result in fit_results.items()
+        qubit_name: ("successful" if fit_result.success else "failed") for qubit_name, fit_result in fit_results.items()
     }
 
 
@@ -295,221 +287,4 @@ def save_results(node: QualibrationNode[Parameters, Quam]):
         qubit.revert_changes()
     node.save()
 
-
 # %%
-######################################
-# Helper functions for data analysis #
-######################################
-
-
-# # Define the Gaussian
-# def gaussian(x, a, x0, sigma, offset):
-#     return a * np.exp(-((x - x0) ** 2) / (2 * sigma**2)) + offset
-
-
-# # Fit function for one time point
-# def fit_gaussian(freqs, states):
-#     p0 = [
-#         np.max(states) - np.min(states),  # amplitude
-#         freqs[np.argmax(states)],  # center
-#         (freqs[-1] - freqs[0]) / 10,  # width
-#         np.min(states),  # offset
-#     ]
-#     try:
-#         popt, _ = curve_fit(gaussian, freqs, states, p0=p0)
-#         return popt[1]  # center frequency
-#     except RuntimeError:
-#         return np.nan
-
-
-# def model_1exp(t, a0, a1, t1):
-#     return a0 * (1 + a1 * np.exp(-t / t1))
-
-
-# def model_2exp(t, a0, a1, a2, t1, t2):
-#     return a0 * (1 + a1 * np.exp(-t / t1) + a2 * np.exp(-t / t2))
-
-
-# # %% {Data_fetching_and_dataset_creation}
-# if not node.parameters.simulate:
-#     if node.parameters.load_data_id is not None:
-#         node = node.load_from_id(node.parameters.load_data_id)
-#         machine = node.machine
-#         ds = xr.Dataset({"state": node.results["ds"].state})
-#         times = ds.time.values
-#         qubits = [machine.qubits[q] for q in ds.qubit.values]
-#     else:
-#         # Fetch the data from the OPX and convert it into a xarray with corresponding axes (from most inner to outer loop)
-#         ds = fetch_results_as_xarray(job.result_handles, qubits, {"time": times * 4, "freq": dfs})
-#         ds = ds.assign_coords(
-#             {
-#                 "freq_full": (
-#                     ["qubit", "freq"],
-#                     np.array(
-#                         [
-#                             dfs + q.xy.RF_frequency + q.freq_vs_flux_01_quad_term * node.parameters.flux_amp**2
-#                             for q in qubits
-#                         ]
-#                     ),
-#                 ),
-#                 "detuning": (
-#                     ["qubit", "freq"],
-#                     np.array([dfs + q.freq_vs_flux_01_quad_term * node.parameters.flux_amp**2 for q in qubits]),
-#                 ),
-#                 "flux": (
-#                     ["qubit", "freq"],
-#                     np.array(
-#                         [np.sqrt(dfs / q.freq_vs_flux_01_quad_term + node.parameters.flux_amp**2) for q in qubits]
-#                     ),
-#                 ),
-#             }
-#         )
-#         ds.freq_full.attrs["long_name"] = "Frequency"
-#         ds.freq_full.attrs["units"] = "GHz"
-#     # Add the dataset to the node
-#     node.results = {"ds": ds}
-#     end = time.time()
-#     print(f"Script runtime: {end - start:.2f} seconds")
-
-#     # %%  {Data_analysis}
-
-#     freqs = ds["freq"].values
-
-#     # Transpose to ensure ('qubit', 'time', 'freq') order
-#     stacked = ds.transpose("qubit", "time", "freq")
-
-#     # Now apply along 'freq' per (qubit, time)
-#     center_freqs = xr.apply_ufunc(
-#         lambda states: fit_gaussian(freqs, states),
-#         stacked,
-#         input_core_dims=[["freq"]],
-#         output_core_dims=[[]],  # no dimensions left after fitting
-#         vectorize=True,
-#         dask="parallelized",
-#         output_dtypes=[float],
-#     ).rename({"state": "center_frequency"})
-
-#     # center_freqs now has dims ('qubit', 'time')
-#     center_freqs = center_freqs.center_frequency + np.array(
-#         [q.freq_vs_flux_01_quad_term * node.parameters.flux_amp**2 * np.ones_like(times) for q in qubits]
-#     )
-
-#     flux_response = np.sqrt(
-#         center_freqs
-#         / xr.DataArray(
-#             [q.freq_vs_flux_01_quad_term for q in qubits], coords={"qubit": center_freqs.qubit}, dims=["qubit"]
-#         )
-#     )
-
-#     ds["center_freqs"] = center_freqs
-#     ds["flux_response"] = flux_response
-
-# # %% {Plotting}
-# grid = QubitGrid(ds, [q.grid_location for q in qubits])
-
-# for ax, qubit in grid_iter(grid):
-#     # freq_ref = (ds.freq_full-ds.freq).sel(qubit=qubit["qubit"]).values[0]
-#     im = (
-#         ds.assign_coords(freq_GHz=ds.freq_full / 1e9)
-#         .loc[qubit]
-#         .state.plot(ax=ax, add_colorbar=False, x="time", y="freq_GHz")
-#     )
-#     ax.set_ylabel("Freq (GHz)")
-#     ax.set_xlabel("Time (ns)")
-#     ax.set_title(qubit["qubit"])
-#     cbar = grid.fig.colorbar(im, ax=ax)
-#     cbar.set_label("Qubit State")
-# grid.fig.suptitle(f"Qubit spectroscopy vs time after flux pulse \n {date_time} #{node_id}")
-
-# plt.tight_layout()
-# plt.show()
-# node.results["figure_raw"] = grid.fig
-
-
-# grid = QubitGrid(ds, [q.grid_location for q in qubits])
-
-# for ax, qubit in grid_iter(grid):
-#     # added_freq = machine.qubits[qubit['qubit']].xy.RF_frequency*0 + machine.qubits[qubit['qubit']].freq_vs_flux_01_quad_term * node.parameters.flux_amp**2
-#     # (-(center_freqs.sel(qubit=qubit["qubit"]) + added_freq)/1e9).plot()
-#     # (center_freqs.sel(qubit=qubit["qubit"]) / 1e9).plot(ax=ax)
-#     (ds.loc[qubit].center_freqs / 1e9).plot(ax=ax)
-#     ax.set_ylabel("Freq (GHz)")
-#     ax.set_xlabel("Time (ns)")
-#     ax.set_title(qubit["qubit"])
-# grid.fig.suptitle(f"Qubit frequency shift vs time after flux pulse \n {date_time} #{node_id}")
-
-# plt.tight_layout()
-# plt.show()
-# node.results["figure_freqs_shift"] = grid.fig
-
-
-# grid = QubitGrid(ds, [q.grid_location for q in qubits])
-
-# for ax, qubit in grid_iter(grid):
-#     # flux_response.sel(qubit=qubit["qubit"]).plot(ax=ax)
-#     ds.loc[qubit].flux_response.plot(ax=ax)
-#     ax.set_ylabel("Flux (V)")
-#     ax.set_xlabel("Time (ns)")
-#     ax.set_title(qubit["qubit"])
-# grid.fig.suptitle(f"Flux response vs time \n {date_time} #{node_id}")
-
-# plt.tight_layout()
-# plt.show()
-# node.results["figure_flux_response"] = grid.fig
-
-# # %% {Update_state}
-# for qubit in tracked_qubits:
-#     qubit.revert_changes()
-
-# # %% {Save_results}
-# node.results["ds"] = ds
-# node.outcomes = {q.name: "successful" for q in qubits}
-# node.results["initial_parameters"] = node.parameters.model_dump()
-# node.machine = machine
-# save_node(node)
-
-# # %% Fitting multi-exponential
-# # TODO: Make it to work on multiple qubits
-
-
-# # Define your model function
-# def model_1exp(t, a0, a1, t1):
-#     return a0 * (1 + a1 * np.exp(-t / t1))
-
-
-# def model_2exp(t, a0, a1, a2, t1, t2):
-#     return a0 * (1 + a1 * np.exp(-t / t1) + a2 * np.exp(-t / t2))
-
-
-# t_data = flux_response.time.values
-# y_data = flux_response.isel(qubit=0).values
-
-# # Fit the data
-# popt, pcov = curve_fit(model_1exp, t_data, y_data, p0=[np.max(y_data), np.min(y_data) / np.max(y_data) - 1, 1000])
-
-# a0_0 = popt[0]
-# a1_0 = a2_0 = popt[1] / 2
-# t1_0 = popt[-1]
-# t2_0 = 100
-
-# initial_guess = [a0_0, a1_0, a2_0, t1_0, t2_0]
-
-# # Perform nonlinear curve fitting
-# popt, pcov = curve_fit(model_2exp, t_data, y_data, p0=initial_guess)
-
-# y_fit = model_2exp(t_data, *popt)
-# # y_fit = model(t_data, -0.0015, -1 / 200, popt[2], popt[3], popt[4])
-
-# # Plot
-# plt.figure()
-# plt.scatter(t_data, y_data, label="Data")
-# plt.plot(
-#     t_data,
-#     y_fit,
-#     "r-",
-#     label=f"Fit: a0={popt[0]:.3f}, a1={popt[1]:.3f}, a2={popt[2]:.3f}, t1={popt[3]:.0f}, t2={popt[4]:.0f}",
-# )
-# plt.xlabel("Time")
-# plt.ylabel("Value")
-# plt.legend()
-# plt.show()
