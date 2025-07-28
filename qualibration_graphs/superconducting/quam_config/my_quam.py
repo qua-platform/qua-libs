44bbcbdd19c7ec40342d750b2c63427b4ec20a9d
Serwan Asaad <serwan.asaad@quantum-machines.co>
2025-07-28 09:53:54 +0200

    Fix: Add @quam_dataclass to root Quam component

--- qualibration_graphs/superconducting/quam_config/my_quam.py
+++ qualibration_graphs/superconducting/quam_config/my_quam.py
@@ -1,7 +1,9 @@
+from quam.core import quam_dataclass
 from quam_builder.architecture.superconducting.qpu import FixedFrequencyQuam, FluxTunableQuam
 
 
 # Define the QUAM class that will be used in all calibration nodes
 # Should inherit from either FixedFrequencyQuam or FluxTunableQuam
+@quam_dataclass
 class Quam(FluxTunableQuam):
     pass
