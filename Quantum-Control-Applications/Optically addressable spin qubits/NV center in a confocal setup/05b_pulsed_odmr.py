19cfb661cfcb3bef373ed0bd3e657e96663d5578
NicolasStaudenmaier <nicolas.staudenmaier@quantum-machines.co>
2025-10-21 15:06:13 +0200

    new readout method

--- Quantum-Control-Applications/Optically addressable spin qubits/NV center in a confocal setup/05b_pulsed_odmr.py
+++ Quantum-Control-Applications/Optically addressable spin qubits/NV center in a confocal setup/05b_pulsed_odmr.py
@@ -30,6 +30,10 @@ from qualang_tools.results.data_handler import DataHandler
 f_vec = np.arange(-30 * u.MHz, 70 * u.MHz, 2 * u.MHz)  # Frequency vector
 n_avg = 1_000_000  # number of averages
 
+# Determine reference readout during single laser pulse
+reference_wait = initialization_len_1 // 4 - 2 * meas_len_1 // 4 - 25  # in clock cycles
+reference_readout = reference_wait >= 4
+
 # Data to save
 save_data_dict = {
     "n_avg": n_avg,
@@ -42,11 +46,9 @@ save_data_dict = {
 ###################
 with program() as pulsed_odmr:
     times = declare(int, size=100)  # QUA vector for storing the time-tags
-    times_dark = declare(int, size=100)  # QUA vector for storing the time-tags
     counts = declare(int)  # variable for number of counts
-    counts_dark = declare(int)  # variable for number of counts
     counts_st = declare_stream()  # stream for counts
-    counts_dark_st = declare_stream()  # stream for counts
+    counts_ref_st = declare_stream()  # stream for counts
     f = declare(int)  # frequencies
     n = declare(int)  # number of iterations
     n_st = declare_stream()  # stream for number of iterations
@@ -60,20 +62,15 @@ with program() as pulsed_odmr:
             # Align for laser readout after the MW pulse
             align()
             play("laser_ON", "AOM1")
-            measure("readout", "SPCM1", None, time_tagging.analog(times, meas_len_1, counts))
-
+            measure("readout", "SPCM1", time_tagging.analog(times, meas_len_1, counts))
             save(counts, counts_st)  # save counts on stream
-            wait(wait_between_runs * u.ns)
-
-            align()
-            # Repeat with zero amplitude for the dark counts
-            play("x180" * amp(0), "NV")
-            # Align for laser readout after the MW pulse
-            align()
-            play("laser_ON", "AOM1")
-            measure("readout", "SPCM1", None, time_tagging.analog(times_dark, meas_len_1, counts_dark))
-
-            save(counts_dark, counts_dark_st)  # save counts on stream
+            # Measure reference photon counts at end of laser pulse
+            if reference_readout:
+                wait(reference_wait, "SPCM1")
+                measure("readout", "SPCM1", time_tagging.analog(times, meas_len_1, counts))
+            else:
+                assign(counts, 1)
+            save(counts, counts_ref_st)
             wait(wait_between_runs * u.ns)
 
         save(n, n_st)  # save number of iterations inside for_loop
@@ -81,7 +78,7 @@ with program() as pulsed_odmr:
     with stream_processing():
         # Cast the data into a 1D vector, average the 1D vectors together and store the results on the OPX processor
         counts_st.buffer(len(f_vec)).average().save("counts")
-        counts_dark_st.buffer(len(f_vec)).average().save("counts_dark")
+        counts_ref_st.buffer(len(f_vec)).average().save("counts_ref")
         n_st.save("iteration")
 
 #####################################
@@ -115,22 +112,21 @@ else:
     # Send the QUA program to the OPX, which compiles and executes it
     job = qm.execute(pulsed_odmr)
     # Get results from QUA program
-    results = fetching_tool(job, data_list=["counts", "counts_dark", "iteration"], mode="live")
+    results = fetching_tool(job, data_list=["counts", "counts_ref", "iteration"], mode="live")
     # Live plotting
     fig = plt.figure()
     interrupt_on_close(fig, job)  # Interrupts the job when closing the figure
 
     while results.is_processing():
         # Fetch results
-        counts, counts_dark, iteration = results.fetch_all()
+        counts, counts_ref, iteration = results.fetch_all()
         # Progress bar
         progress_counter(iteration, n_avg, start_time=results.get_start_time())
         # Plot data
         plt.cla()
-        plt.plot((NV_LO_freq * 0 + f_vec) / u.MHz, counts / 1000 / (meas_len_1 * 1e-9), label="photon counts")
-        plt.plot((NV_LO_freq * 0 + f_vec) / u.MHz, counts_dark / 1000 / (meas_len_1 * 1e-9), label="dark counts")
+        plt.plot((NV_LO_freq * 0 + f_vec) / u.MHz, counts / counts_ref, label="norm. photon counts")
         plt.xlabel("MW frequency [MHz]")
-        plt.ylabel("Intensity [kcps]")
+        plt.ylabel("Norm. Signal")
         plt.title("pulsed ODMR")
         plt.legend()
         plt.pause(0.1)
@@ -138,7 +134,8 @@ else:
     script_name = Path(__file__).name
     data_handler = DataHandler(root_data_folder=save_dir)
     save_data_dict.update({"counts_data": counts})
-    save_data_dict.update({"counts_dark_data": counts_dark})
+    save_data_dict.update({"counts_dark_data": counts_ref})
+    save_data_dict.update({"normalized_data": counts / counts_ref})
     save_data_dict.update({"fig_live": fig})
     data_handler.additional_files = {script_name: script_name, **default_additional_files}
     data_handler.save_data(data=save_data_dict, name="_".join(script_name.split("_")[1:]).split(".")[0])
