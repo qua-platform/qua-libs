4cac496ad50f3a354244282e1943b49dc6b3a650
TheoQM <theo@quantum-machines.co>
2025-11-28 11:10:42 +0100

    black formatting

--- Quantum-Control-Applications/Superconducting/Two-Fixed-Coupled-Transmons/octave_calibration.py
+++ Quantum-Control-Applications/Superconducting/Two-Fixed-Coupled-Transmons/octave_calibration.py
@@ -9,15 +9,15 @@ qmm = QuantumMachinesManager(host=qop_ip, cluster_name=cluster_name, log_level="
 qm = qmm.open_qm(config)
 
 elements = [
-        "rr1",
-        "rr2",
-        "q1_xy",
-        "q2_xy",
-        "cr_drive_c1t2",
-        "cr_drive_c2t1",
-        "cr_cancel_c1t2",
-        "cr_cancel_c2t1",
-    ]
+    "rr1",
+    "rr2",
+    "q1_xy",
+    "q2_xy",
+    "cr_drive_c1t2",
+    "cr_drive_c2t1",
+    "cr_cancel_c1t2",
+    "cr_cancel_c2t1",
+]
 for element in elements:
     print("-" * 37 + f" Calibrates {element}")
     qm.calibrate_element(element)  # can provide many IFs for specific LO
