986efee5e4ce1e5f582484736560ea3c12d097c6
TomerFiron <tomer.firon@quantum-machines.co>
2026-03-29 09:47:06 +0300

    Fixed wrong units in plotting. Nanoseconds instead of microseconds.

--- qualibration_graphs/superconducting/calibration_utils/T2echo/plotting.py
+++ qualibration_graphs/superconducting/calibration_utils/T2echo/plotting.py
@@ -84,11 +84,11 @@ def plot_individual_data_with_fit(ax: Axes, ds: xr.Dataset, qubit: dict[str, str
         raise RuntimeError("The dataset must contain either 'I' or 'state' for the plotting function to work.")
 
     ax.set_title(qubit["qubit"])
-    ax.set_xlabel("Idle_time (µs)")
+    ax.set_xlabel("Idle_time (ns)")
     ax.text(
         0.1,
         0.9,
-        f'T2e = {fit["T2_echo"].values*1e-3:.1f} ± {fit["T2_echo_error"].values*1e-3:.1f} µs',
+        f'T2e = {fit["T2_echo"].values*1e-3:.1f} ± {fit["T2_echo_error"].values*1e-3:.1f} ns',
         transform=ax.transAxes,
         fontsize=10,
         verticalalignment="top",
