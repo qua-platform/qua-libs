9a116e2c0284aeb4c00796739ea4971102a38d59
KU-QM <kalid.ulas@quantum-machines.co>
2026-07-21 18:22:57 +0100

    Add files from pr/qd-ld-10-t1-ramsey-rename

---
+++ qualibration_graphs/quantum_dots/calibration_utils/ramsey/parameters.py
@@ -0,0 +1,71 @@
+from qualibrate.core import NodeParameters
+from qualibrate.core.parameters import RunnableParameters
+from qualibration_libs.parameters import (
+    CommonNodeParameters,
+    QubitsExperimentNodeParameters,
+    IdleTimeNodeParameters,
+)
+
+from calibration_utils.common_utils.experiment import (
+    HeraldedInitializeParameters,
+    ParityDiffAnalysisParameters,
+)
+
+
+class NodeSpecificParameters(RunnableParameters):
+    """Parameters for Ramsey 11a."""
+
+    num_shots: int = 300
+    """Number of averages to perform. Default is 100."""
+
+
+class RamseyParameters(
+    NodeParameters,
+    CommonNodeParameters,
+    IdleTimeNodeParameters,
+    NodeSpecificParameters,
+    HeraldedInitializeParameters,
+    QubitsExperimentNodeParameters,
+    ParityDiffAnalysisParameters,
+):
+    """Parameter set for 11a_ramsey."""
+
+    frequency_detuning_in_mhz: float = 0.25
+    """Frequency detuning in MHz. Default is 1.0 MHz."""
+
+
+class RamseyDetuningParameters(
+    NodeParameters,
+    CommonNodeParameters,
+    NodeSpecificParameters,
+    HeraldedInitializeParameters,
+    QubitsExperimentNodeParameters,
+    ParityDiffAnalysisParameters,
+):
+    """Parameter set for 11b_ramsey_detuning."""
+
+    detuning_span_in_mhz: float = 5.0
+    """Frequency detuning span. Default 5MHz."""
+    detuning_step_in_mhz: float = 0.1
+    """Frequency detuning step. Default 0.1MHz"""
+    idle_time_ns: int = 100
+    """Short idle time in ns (gives wide fringes for coarse localisation)."""
+    idle_time_long_ns: int = 400
+    """Long idle time in ns (gives narrow fringes for precision + T2* via amplitude ratio)."""
+
+
+class RamseyChevronParameters(
+    NodeParameters,
+    CommonNodeParameters,
+    IdleTimeNodeParameters,
+    NodeSpecificParameters,
+    HeraldedInitializeParameters,
+    QubitsExperimentNodeParameters,
+    ParityDiffAnalysisParameters,
+):
+    """Parameter set for 11c_ramsey_chevron (and related Ramsey chevron nodes)."""
+
+    detuning_span_in_mhz: float = 5.0
+    """Frequency detuning span. Default 5MHz."""
+    detuning_step_in_mhz: float = 0.1
+    """Frequency detuning step. Default 0.1MHz"""
---
+++ qualibration_graphs/quantum_dots/calibration_utils/ramsey_parity_diff/__init__.py
@@ -0,0 +1,13 @@
+from calibration_utils.ramsey.parameters import RamseyParameters as Parameters
+from calibration_utils.ramsey_parity_diff.analysis import (
+    fit_raw_data,
+    log_fitted_results,
+)
+from calibration_utils.ramsey_parity_diff.plotting import plot_raw_data_with_fit
+
+__all__ = [
+    "Parameters",
+    "fit_raw_data",
+    "log_fitted_results",
+    "plot_raw_data_with_fit",
+]
