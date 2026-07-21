c1a494715e1f0cd2b04e3816e29dfb7113861818
KU-QM <kalid.ulas@quantum-machines.co>
2026-07-21 17:25:55 +0100

    calibration_utils

---
+++ qualibration_graphs/quantum_dots/calibration_utils/time_of_flight/parameters.py
@@ -0,0 +1,29 @@
+from typing import Optional
+from qualibrate.core import NodeParameters
+from qualibrate.core.parameters import RunnableParameters
+from qualibration_libs.parameters import CommonNodeParameters
+from calibration_utils.common_utils.experiment import BaseExperimentNodeParameters
+
+
+class NodeSpecificParameters(RunnableParameters):
+    num_shots: int = 100
+    """Number of averages to perform. Default is 100."""
+    time_of_flight_in_ns: Optional[int] = 28
+    """Time of flight in nanoseconds. Default is 28 ns."""
+    readout_amplitude_in_v: Optional[float] = 0.1
+    """Readout amplitude in volts. Default is 0.1 V."""
+    readout_length_in_ns: Optional[int] = 1000
+    """Readout length in nanoseconds. Default is 1µs."""
+    sensor_names: Optional[list[str]] = None
+    """List of sensor names. Default is None."""
+    use_simulated_data: bool = False
+    """Whether to generate simulated data instead of measuring via the OPX. Default False."""
+
+
+class Parameters(
+    NodeParameters,
+    CommonNodeParameters,
+    NodeSpecificParameters,
+    BaseExperimentNodeParameters,
+):
+    pass
