98e9ecc5250988b6bf9725b59cb01f6678877705
KU-QM <kalid.ulas@quantum-machines.co>
2026-07-24 13:33:30 +0100

    sensor gate refactor & analysis

--- qualibration_graphs/quantum_dots/calibration_utils/sensor_dot/__init__.py
+++ qualibration_graphs/quantum_dots/calibration_utils/sensor_dot/__init__.py
@@ -1,5 +1,5 @@
 from .parameters import Parameters, DacSweepParameters
-from .analysis import process_raw_dataset, fit_raw_data, log_fitted_results
+from .analysis import process_raw_dataset, fit_raw_data, log_fitted_results, lorentzian, LorentzianFitResult, fit_lorentzian, optimal_operating_point
 from .plotting import plot_raw_amplitude, plot_raw_phase, plot_amplitude_with_fit
 from .simulated_data_generator import generate_simulated_dataset
 
@@ -13,4 +13,8 @@ __all__ = [
     "plot_raw_phase",
     "plot_amplitude_with_fit",
     "generate_simulated_dataset",
+    "lorentzian", 
+    "LorentzianFitResult", 
+    "fit_lorentzian", 
+    "optimal_operating_point",
 ]
