630a9473b97f7b7acdf91a15bffa3fb2ccc6ca18
ChloeBO-QM <Chloe.Bureau-Oxton@quantum-machines.co>
2026-07-30 14:42:10 +0200

    removed video-mode specific parameters from nodes where they weren't used.

--- qualibration_graphs/quantum_dots/calibration_utils/charge_stability/parameters.py
+++ qualibration_graphs/quantum_dots/calibration_utils/charge_stability/parameters.py
@@ -2,9 +2,8 @@ from qua_dashboards.virtual_gates import virtual_layer_adder
 from qualibrate.core import NodeParameters
 from qualibrate.core.parameters import RunnableParameters
 from qualibration_libs.parameters import CommonNodeParameters
-from calibration_utils.run_video_mode.video_mode_specific_parameters import (
-    VideoModeCommonParameters,
-)
+from calibration_utils.common_utils.dc_control_parameters import DCControlParameters
+from calibration_utils.run_video_mode.video_mode_parameters import VideoModeParameters
 
 from typing import List, Literal, Dict, Union, Callable, Optional
 
@@ -57,7 +56,8 @@ class NodeSpecificParameters(RunnableParameters):
 class Parameters(
     NodeParameters,
     CommonNodeParameters,
-    VideoModeCommonParameters,
+    DCControlParameters,
+    VideoModeParameters,
     NodeSpecificParameters,
 ):
     pass
@@ -65,8 +65,8 @@ class Parameters(
 
 class OPXQDACParameters(
     NodeParameters,
-    VideoModeCommonParameters,
     CommonNodeParameters,
+    DCControlParameters,
     NodeSpecificParameters,
 ):
     x_from_qdac: bool = False
@@ -81,7 +81,6 @@ class OPXQDACParameters(
 
 class SimulationParameters(
     NodeParameters,
-    VideoModeCommonParameters,
     CommonNodeParameters,
     NodeSpecificParameters,
 ):
