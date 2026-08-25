adb4da333d3e9e9521333b0d3c1f5829642dde80
KU-QM <kalid.ulas@quantum-machines.co>
2026-08-25 11:17:29 +0100

    hello_qua changes

--- qualibration_graphs/quantum_dots/calibration_utils/hello_qua/__init__.py
+++ qualibration_graphs/quantum_dots/calibration_utils/hello_qua/__init__.py
@@ -1,15 +1,5 @@
 from .parameters import Parameters
-from .helper_utils import extract_longest_readout_time, extract_vgs_id
-from .analysis import process_raw_dataset, log_processed_summary
-from .plotting import plot_all, plot_dot_iq, plot_individual_iq
 
 __all__ = [
     "Parameters",
-    "extract_longest_readout_time",
-    "extract_vgs_id",
-    "process_raw_dataset",
-    "log_processed_summary",
-    "plot_all",
-    "plot_dot_iq",
-    "plot_individual_iq",
 ]
--- qualibration_graphs/quantum_dots/calibration_utils/hello_qua/parameters.py
+++ qualibration_graphs/quantum_dots/calibration_utils/hello_qua/parameters.py
@@ -3,26 +3,9 @@ from qualibrate.core.parameters import RunnableParameters
 from qualibration_libs.parameters import CommonNodeParameters
 from calibration_utils.common_utils.experiment import QuantumDotExperimentNodeParameters
 
-from typing import List, Optional
-
-
 class NodeSpecificParameters(RunnableParameters):
     num_shots: int = 100
     """Number of averages to perform. Default is 100."""
-    sensor_names: Optional[List[str]] = None
-    """List of sensor names to include in the measurement. If None, all sensors are used."""
-    v_center: Optional[float] = None
-    """Center of the voltage sweep in volts. Default 0.0V."""
-    v_span: float = 0.01
-    """Span of the voltage sweep in volts. Default 10mV."""
-    n_points: int = 101
-    """Number of points in the voltage sweep. Default is 101."""
-    dwell_time: int = 500
-    """Dwell time at each voltage in nanoseconds. Default is 500ns."""
-    ramp_duration: int = 100
-    """The ramp duration to each voltage point. Default is 100ns."""
-    max_compensation_voltage: float = 0.01
-    """The maximum height of the compensation pulse. The duration will be calculated using this height."""
 
 
 class Parameters(
