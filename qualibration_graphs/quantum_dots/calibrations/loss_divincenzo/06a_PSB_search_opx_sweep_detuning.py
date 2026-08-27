430b510836f0a0cef675fccc554a50a6a2fce2b5
KU-QM <kalid.ulas@quantum-machines.co>
2026-08-27 09:31:13 +0100

    power rabi fix

--- qualibration_graphs/quantum_dots/calibrations/loss_divincenzo/06a_PSB_search_opx_sweep_detuning.py
+++ qualibration_graphs/quantum_dots/calibrations/loss_divincenzo/06a_PSB_search_opx_sweep_detuning.py
@@ -78,6 +78,7 @@ node = QualibrationNode[Parameters, Quam](
 @node.run_action(skip_if=node.modes.external)
 def custom_param(node: QualibrationNode[Parameters, Quam]):
     # You can get type hinting in your IDE by typing node.parameters.
+    # node.parameters.use_simulated_data = True
     pass
 
 
