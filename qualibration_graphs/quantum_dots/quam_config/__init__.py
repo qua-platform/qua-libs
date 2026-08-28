c1efdd446f777464caaecd6b76c171de81ac15a2
KU-QM <kalid.ulas@quantum-machines.co>
2026-08-28 10:24:26 +0100

    initial custom macros implementation

--- qualibration_graphs/quantum_dots/quam_config/__init__.py
+++ qualibration_graphs/quantum_dots/quam_config/__init__.py
@@ -1,3 +1,5 @@
 from .my_quam import Quam, QubitQuam
+from .my_macros import InitializeMacro, MeasureMacro
+from .my_macro_parameters import MacroParameters
 
-__all__ = ["Quam", "QubitQuam"]
+__all__ = ["Quam", "QubitQuam", "InitializeMacro", "MeasureMacro", "MacroParameters"]
---
+++ qualibration_graphs/quantum_dots/quam_config/my_macro_parameters.py
@@ -0,0 +1,31 @@
+"""
+This script is made in order to create Node Parameters based on any custom macros that you have built. 
+
+This example populates the InitializeMacroParameters to be HeraldedBalancedInitializeMacro parameters. 
+"""
+
+from typing import Optional, Literal
+
+from qualibrate.core.parameters import RunnableParameters
+
+__all__ = ["MacroParameters"]
+
+class InitializeMacroParameters(RunnableParameters):
+    target_state: Optional[int] = None
+    """The state you want to initialize into for heralded initialization."""
+    max_loops: int = 100
+    """Maximum number of initialization loops for heralded initialization."""
+    return_n_loops: bool = False
+    """Whether to return the number of times it has looped over the initialise sequence to achieve the desired result."""
+    qubit_role: Literal["target", "control"] = "control"
+    """Specify which qubit, related to the qubit_pair, to pulse. """
+
+class MeasureMacroParameters(RunnableParameters): 
+    pass
+
+class MacroParameters(
+    InitializeMacroParameters, 
+    MeasureMacroParameters
+): 
+    """Batch all the macro related parameters to export in a single class"""
+    pass
