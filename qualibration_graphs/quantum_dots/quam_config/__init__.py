e03bbc7c1d8f5215c47260a7ac0932d0b4349c70
KU-QM <kalid.ulas@quantum-machines.co>
2026-08-28 10:24:26 +0100

    initial custom macros implementation

--- qualibration_graphs/quantum_dots/quam_config/__init__.py
+++ qualibration_graphs/quantum_dots/quam_config/__init__.py
@@ -1,3 +1,5 @@
 from .my_quam import Quam
+from .my_macros import InitializeMacro, MeasureMacro
+from .my_macro_parameters import MacroParameters
 
-__all__ = ["Quam"]
+__all__ = ["Quam", "InitializeMacro", "MeasureMacro", "MacroParameters"]
