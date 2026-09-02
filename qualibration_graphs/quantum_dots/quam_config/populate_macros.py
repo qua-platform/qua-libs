794659ed763d9a4b2ce21b889342cbda0f746a2d
KU-QM <kalid.ulas@quantum-machines.co>
2026-09-02 22:14:32 +0100

    my_macros changes

--- qualibration_graphs/quantum_dots/quam_config/populate_macros.py
+++ qualibration_graphs/quantum_dots/quam_config/populate_macros.py
@@ -1,6 +1,7 @@
 """
 Use this script to populate QuantumDotPair components with custom macros, as created in my_macros.py
 """
+
 from typing import List
 from quam_builder.architecture.quantum_dots.macro_engine import wire_machine_macros
 from quam_builder.architecture.quantum_dots.operations.names import SingleQubitMacroName
@@ -10,24 +11,23 @@ from quam_config import InitializeMacro, MeasureMacro
 
 # ### Helper function which allows you to choose the dot pairs to update
 
-def get_dot_pairs(
-    machine: Quam, 
-    dot_pairs: List[str] | None = None
-) -> List[str]: 
-    """
-    Given a spin qubit Quam machine, extracts a list of QuantumDotPair names as a list. 
 
-    Args: 
-        - machine: Quam - The Spin Quam Machine to extract the dot pairs from
-        - dot_pairs: List[str] - The list of quantum dot pair string names. If None, then 
-            this function will return all the dot pairs associated with the machine. 
+def get_dot_pairs(machine: Quam, dot_pairs: List[str] | None = None) -> List[str]:
     """
-    if dot_pairs is None: 
-        # No list supplied, default to all existing dot pairs. 
+    Given a spin qubit Quam machine, extracts a list of QuantumDotPair names as a list.
+
+    Args:
+        - machine: Quam - The Spin Quam Machine to extract the dot pairs from
+        - dot_pairs: List[str] - The list of quantum dot pair string names. If None, then
+            this function will return all the dot pairs associated with the machine.
+    """
+    if dot_pairs is None:
+        # No list supplied, default to all existing dot pairs.
         return list(machine.quantum_dot_pairs.keys())
-    else: 
-        # List supplied. Means that the user wants to update only a subset of the dot pairs. 
+    else:
+        # List supplied. Means that the user wants to update only a subset of the dot pairs.
         return dot_pairs
+
 
 ##############################
 ######## Load machine ########
@@ -40,26 +40,24 @@ machine = Quam.load()
 ######## Specify the dot pairs to update ########
 #################################################
 
-# Create a list of dot pair names here if you would like to update only a subset of dot pair macros
+# Create a list of dot pair names here if you would like to update only a subset of dot pair macros
 dot_pairs = None
 
-dot_pairs = get_dot_pairs(
-    machine, 
-    dot_pairs
-)
+dot_pairs = get_dot_pairs(machine, dot_pairs)
 
 #########################################################
 ######## Wire the custom macros into the machine ########
 #########################################################
 
 wire_machine_macros(
-    machine = machine, 
-    instance_overrides = {
-        f"quantum_dot_pairs.{qdp}" : {
-            SingleQubitMacroName.INITIALIZE : InitializeMacro,
-            SingleQubitMacroName.MEASURE : MeasureMacro,
-        } for qdp in dot_pairs
-    }
+    machine=machine,
+    instance_overrides={
+        f"quantum_dot_pairs.{qdp}": {
+            SingleQubitMacroName.INITIALIZE: InitializeMacro,
+            SingleQubitMacroName.MEASURE: MeasureMacro,
+        }
+        for qdp in dot_pairs
+    },
 )
 
 ######################################
