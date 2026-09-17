c301d7e757d6eb3cea57e7e3d8cbe0ccd1ad2b9f
KU-QM <kalid.ulas@quantum-machines.co>
2026-09-17 14:45:57 +0100

    Generality fix

--- qualibration_graphs/quantum_dots/calibrations/loss_divincenzo/08a_qubit_spectroscopy_chirp.py
+++ qualibration_graphs/quantum_dots/calibrations/loss_divincenzo/08a_qubit_spectroscopy_chirp.py
@@ -221,7 +221,6 @@ def execute_qua_program(node: QualibrationNode[Parameters, Quam]):
     qmm = node.machine.connect()
     # Get the config from the machine
     config = node.machine.generate_config()
-    # config["octaves"]["oct1"]["connectivity"] = "con1"
     # Execute the QUA program only if the quantum machine is available (this is to avoid interrupting running jobs).
     with qm_session(qmm, config, timeout=node.parameters.timeout) as qm:
         # The job is stored in the node namespace to be reused in the fetching_data run_action
