77111ed0b21f4f2c1c41d7d05ff6b76b1b2d1275
KU-QM <kalid.ulas@quantum-machines.co>
2026-09-18 13:07:06 +0100

    remove unused imports

--- qualibration_graphs/quantum_dots/calibrations/loss_divincenzo/07b_init_2d_calibration.py
+++ qualibration_graphs/quantum_dots/calibrations/loss_divincenzo/07b_init_2d_calibration.py
@@ -12,7 +12,6 @@ from qualang_tools.results import progress_counter
 
 from qualibrate.core import QualibrationNode
 from qualibration_libs.parameters.experiment import get_qubit_pairs
-from calibration_utils.common_utils.macro_updates import change_macro_tracked, revert_tracked_macros
 from quam_config import Quam
 
 from calibration_utils.init_2d import (
